7e1ca3a11cd131338d20ad8d0b284d197a6521c9
Humberto Bravo <humbandroid@gmail.com>
2022-04-01 18:10:39 -0300

    HN Wertsfy v1.0 - Continuando o projeto [IV].

--- api/imports/rcalcApp/components/funcoes-conversoras.ts
+++ api/imports/rcalcApp/components/funcoes-conversoras.ts
@@ -1,3 +1,19 @@
-// # Algoritmo ainda "confuso" e em análise
 import { CalculatorManifests } from "./@defines/calculator-manifests";
+
+const ConversoresAnguloPlano: CalculatorManifests.Arquitetura = { chaves: [], formulas: [] };
+const ConversoresAnguloSolido: CalculatorManifests.Arquitetura = { chaves: [], formulas: [] };
+const ConversoresComprimento: CalculatorManifests.Arquitetura = { chaves: [], formulas: [] };
+const ConversoresArea: CalculatorManifests.Arquitetura = { chaves: [], formulas: [] };
+const ConversoresVolume: CalculatorManifests.Arquitetura = { chaves: [], formulas: [] };
+const ConversoresMassa: CalculatorManifests.Arquitetura = { chaves: [], formulas: [] };
+const ConversoresTemperatura: CalculatorManifests.Arquitetura = { chaves: [], formulas: [] };
+const ConversoresVelocidade: CalculatorManifests.Arquitetura = { chaves: [], formulas: [] };
+const ConversoresForca: CalculatorManifests.Arquitetura = { chaves: [], formulas: [] };
+const ConversoresTempo: CalculatorManifests.Arquitetura = { chaves: [], formulas: [] };
+const ConversoresQuantidade: CalculatorManifests.Arquitetura = { chaves: [], formulas: [] };
+const ConversoresPressao: CalculatorManifests.Arquitetura = { chaves: [], formulas: [] };
+const ConversoresSom: CalculatorManifests.Arquitetura = { chaves: [], formulas: [] };
+const ConversoresLuz: CalculatorManifests.Arquitetura = { chaves: [], formulas: [] };
+const ConversoresEnergia: CalculatorManifests.Arquitetura = { chaves: [], formulas: [] };
+const ConversoresTech: CalculatorManifests.Arquitetura = { chaves: [], formulas: [] };
 

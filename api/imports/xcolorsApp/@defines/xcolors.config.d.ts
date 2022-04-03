9bf2c4075bde8098f1567c74481cf3b069f63616
Humberto Bravo <humbandroid@gmail.com>
2022-04-03 17:08:43 -0300

    HN Wertsfy v1.0 - Atualizando o arquivo 'xcolors.config.d.ts'.

--- api/imports/xcolorsApp/@defines/xcolors.config.d.ts
+++ api/imports/xcolorsApp/@defines/xcolors.config.d.ts
@@ -2,7 +2,5 @@ export declare namespace Espectros {
 
   export interface espectroDeUmaCor { cor: any[string][string] }
 
-  export interface referenciamentoDeUmEspectro { ref: any[string][string] }
-
 }
 

520a37b13e0cbd694697505d80c09a27398ffd3c
Humberto Bravo <humbandroid@gmail.com>
2022-04-03 20:45:46 -0300

    HN Wertsfy v1.0 - Atualizando o arquivo 'calculator-manifests.d.ts'.

--- api/imports/rcalcApp/components/@defines/calculator-manifests.d.ts
+++ api/imports/rcalcApp/components/@defines/calculator-manifests.d.ts
@@ -1,14 +1,9 @@
 export declare namespace CalculatorManifests {
-<<<<<<< HEAD
   export interface Arquitetura {
     
   
     chaves: any[string | number][string | number][string | number][string | number][string | number][string | number] 
   
   }
-=======
-
-  export interface Arquitetura { chaves: string | any[][], formulas: any[][][][][][] }
->>>>>>> ec17983e73420285f85b02eca30cd81dc4b21616
 
 }

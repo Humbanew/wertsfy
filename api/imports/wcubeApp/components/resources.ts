87b148e25a9e861485de5ae2572a4f2023f15699
Humberto Bravo <humbandroid@gmail.com>
2022-04-20 00:47:53 -0300

    HN Wertsfy v1.0 - {WCube Module Core - v2.0} - Implementação Base.

--- api/imports/wcubeApp/components/resources.ts
+++ api/imports/wcubeApp/components/resources.ts
@@ -4,12 +4,158 @@ export const $resources = {
 
     classico: {
 
+      tamanhoDasPartidas: {
 
-      
+        "03x03": { model: [[],[],[]] },
+
+        "04x04": { model: [[],[],[],[]] },
+
+        "05x05": { model: [[],[],[],[],[]] },
+
+        "06x06": { model: [[],[],[],[],[],[]] },
+
+        "07x07": { model: [[],[],[],[],[],[],[]] },
+
+        "08x08": { model: [[],[],[],[],[],[],[],[]] },
+
+        "09x09": { model: [[],[],[],[],[],[],[],[],[]] },
+
+        "10x10": { model: [[],[],[],[],[],[],[],[],[],[]] },
+
+        "11x11": { model: [[],[],[],[],[],[],[],[],[],[],[]] },
+
+        "12x12": { model: [[],[],[],[],[],[],[],[],[],[],[],[]] },
+
+      },
+
+      pontuacoes: {
+
+        "bloco-valor-2": { 
+          pontuacao: 2, 
+          corBackground: "#0022ff" 
+        },
+
+        "bloco-valor-4": {
+          pontuacao: 4,
+          corBackground: "#220000"
+        },
+
+        "bloco-valor-8": {
+          pontuacao: 8,
+          corBackground: "#005500"
+        },
+
+        "bloco-valor-16": {
+          pontuacao: 16,
+          corBackground: "#4f88f5"
+        },
+
+        "bloco-valor-32": {
+          pontuacao: 32,
+          corBackground: "#ff0000"
+        },
+
+        "bloco-valor-64": {
+          pontuacao: 64,
+          corBackground: "#ff00ff"
+        },
+
+        "bloco-valor-128": {
+          pontuacao: 128,
+          corBackground: "#ff8800"
+        },
+
+        "bloco-valor-256": {
+          pontuacao: 256,
+          corBackground: "#ff6a88"
+        },
+
+        "bloco-valor-512": {
+          pontuacao: 512,
+          corBackground: "#025d88"
+        },
+
+        "bloco-valor-1024": {
+          pontuacao: 1024,
+          corBackground: "#f5d500"
+        },
+
+        "bloco-valor-2048": {
+          pontuacao: 2048,
+          corBackground: "#00aa00"
+        },
+
+        "bloco-valor-4096": {
+          pontuacao: 4096,
+          corBackground: "#4f010f"
+        },
+
+        "bloco-valor-8192": {
+          pontuacao: 8192,
+          corBackground: "#ff00ff"
+        },
+
+        "bloco-valor-16384": {
+          pontuacao: 16384,
+          corBackground: "#ff8877"
+        },
+
+        "bloco-valor-32768": {
+          pontuacao: 32768,
+          corBackground: "#115533"
+        },
+
+        "bloco-valor-65536": {
+          pontuacao: 65536,
+          corBackground: "#777777"
+        },
+
+        "bloco-valor-131072": {
+          pontuacao: 131072,
+          corBackground: "#775500"
+        },
+
+        "bloco-valor-262144": {
+          pontuacao: 262144,
+          corBackground: "#00aa00"
+        },
+
+        "bloco-valor-524288": {
+          pontuacao: 524288,
+          corBackground: "#4f010f"
+        },
+
+        "bloco-valor-1048576": {
+          pontuacao: 1048576,
+          corBackground: "#000000"
+        }
+
+      },
+
+      ranks: {
+
+        "rank-e": 'E',
+
+        "rank-d": 'D',
+
+        "rank-c": 'C',
+
+        "rank-b": 'B',
+
+        "rank-a": 'A',
+
+        "rank-s": 'S',
+
+      }
+
 
     }
 
+
+
   }
+
+
 
 
 }

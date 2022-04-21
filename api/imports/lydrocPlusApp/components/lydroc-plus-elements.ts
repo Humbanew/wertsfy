0c1c982d4306761c4966e13e350b39740939b4bc
Humberto Bravo <humbandroid@gmail.com>
2022-04-21 03:09:41 -0300

    HN Wertsfy v1.0 - {Lydroc Plus Module Core v0.1} - Projetando Arquitetura [6].

--- api/imports/lydrocPlusApp/components/lydroc-plus-elements.ts
+++ api/imports/lydrocPlusApp/components/lydroc-plus-elements.ts
@@ -1,7 +1,5 @@
 //z let element = window.customElements.define('<element>', class extends HTMLElement {} ); return element;
 
 export function lydrocBasicCorebase() { }
-
 export function lydrocAdvancedCorebase() { }
-
 export function lydrocUltimateCorebase() { }

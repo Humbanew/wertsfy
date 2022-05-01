56e2c02dc6dbd9dcccfd5fd8f8172588e8fa0222
Humberto Bravo <humbandroid@gmail.com>
2022-05-01 02:22:59 -0300

    Wertsfy API - Continuando progressão do projeto 2.

--- api/imports/lydrocPlusApp/components/lydroc-plus-elements.ts
+++ api/imports/lydrocPlusApp/components/lydroc-plus-elements.ts
@@ -1,3 +1,3 @@
-//z let element = window.customElements.define('<element>', class extends HTMLElement {} ); return element;
+// let element = window.customElements.define('<element>', class extends HTMLElement {} ); return element;
 
 namespace LydrocPlusElements { };

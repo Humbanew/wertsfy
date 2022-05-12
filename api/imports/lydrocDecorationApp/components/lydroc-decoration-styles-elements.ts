d043f9c93df70f513d0e53d69b3f33cc200079d3
Humberto Bravo <humbandroid@gmail.com>
2022-05-12 02:07:09 -0300

    Wertsfy API v1.0 - Resolving Lydroc Decoration Files

--- api/imports/lydrocDecorationApp/components/lydroc-decoration-styles-elements.ts
+++ api/imports/lydrocDecorationApp/components/lydroc-decoration-styles-elements.ts
@@ -1,2 +1,2 @@
-export const $displayNone = window.customElements.define('display-none', class LyDecDisplayNone extends HTMLStyleElement {});
-export const $displayBlock = window.customElements.define('display-block', class LyDecDisplayBlock extends HTMLStyleElement {});
+
+

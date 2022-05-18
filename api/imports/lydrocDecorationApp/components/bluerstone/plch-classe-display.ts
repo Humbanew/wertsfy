ae1497a2ae53c26404baf8b90fdd7b2d4d35ae0a
Humberto Bravo <humbandroid@gmail.com>
2022-05-18 02:37:21 -0300

    Wertsfy API v1.0 - Resolvendo Módulos da API

--- api/imports/lydrocDecorationApp/components/bluerstone/plch-classe-display.ts
+++ api/imports/lydrocDecorationApp/components/bluerstone/plch-classe-display.ts
@@ -13,3 +13,167 @@ export const $dplInherit$ = class DisplayInherit extends HTMLStyleElement {
 export const $dplInitial$ = class DisplayInitial extends HTMLStyleElement {
   constructor() { super(); this.textContent = ".ly-display-initial { display: initial; }"; }
 };
+
+export const $dplInline$ = class DisplayInline extends HTMLStyleElement {
+  constructor() { super(); this.textContent = ".ly-display-inline { display: inline; }"; }
+}
+
+export const $dplInlineBlock$ = class DisplayInlineBlock extends HTMLStyleElement {
+  constructor() { super(); this.textContent = ".ly-display-inline-block { display: inline - block; }"; }
+}
+
+export const $dplInlineFlex$ = class DisplayInlineFlex extends HTMLStyleElement {
+  constructor() { super(); this.textContent = ".ly-display-inline-flex { display: inline - flex; }"; }
+}
+
+export const $dplInlineGrid$ = class DisplayInlineGrid extends HTMLStyleElement {
+  constructor() { super(); this.textContent = ".ly-display-inline-grid { display: inline - grid; }"; }
+}
+
+export const $dplFlex$ = class DisplayFlex extends HTMLStyleElement {
+  constructor() { super(); this.textContent = ".ly-display-flex { display: flex; }"; }
+}
+
+export const $dplFlowRoot$ = class DisplayFlowRoot extends HTMLStyleElement {
+  constructor() { super(); this.textContent = ".ly-display-flow-root { display: flow-root; }"; }
+}
+
+export const $dplGrid$ = class DisplayGrid extends HTMLStyleElement {
+  constructor() { super(); this.textContent = ".ly-display-grid { display: grid; }"; }
+}
+
+export const $dplNone$ = class DisplayNone extends HTMLStyleElement {
+  constructor() { super(); this.textContent = ".ly-display-none { display: none; }"; }
+}
+
+export const $dplRunIn$ = class DisplayRunIn extends HTMLStyleElement {
+  constructor() { super(); this.textContent = ".ly-display-run-in { display: run-in; }"; }
+}
+
+export const $dplTable$ = class DisplayTable extends HTMLStyleElement {
+  constructor() { super(); this.textContent = ".ly-display-table { display: table; }"; }
+}
+
+export const $dplTableRow$ = class DisplayTableRow extends HTMLStyleElement {
+  constructor() { super(); this.textContent = ".ly-display-table-row { display: table-row; }"; }
+}
+
+export const $dplTableCell$ = class DisplayTableCell extends HTMLStyleElement {
+  constructor() { super(); this.textContent = ".ly-display-table-cell { display: table-cell; }"; }
+}
+
+export const $dplTableCaption$ = class DisplayTableCaption extends HTMLStyleElement {
+  constructor() { super(); this.textContent = ".ly-display-table-caption { display: table-caption; }"; }
+}
+
+export const $dplTableRowGroup$ = class DisplayTableRowGroup extends HTMLStyleElement {
+  constructor() { super(); this.textContent = ".ly-display-table-row-group { display: table-row-group; }"; }
+}
+
+export const $dplTableHeaderGroup$ = class DisplayTableHeaderGroup extends HTMLStyleElement {
+  constructor() { super(); this.textContent = ".ly-display-table-header-group { display: table-header-group; }"; }
+}
+
+export const $dplTableFooterGroup$ = class DisplayTableFooterGroup extends HTMLStyleElement {
+  constructor() { super(); this.textContent = ".ly-display-table-footer-group { display: table-footer-group; }"; }
+}
+
+export const $dplTableColumnGroup$ = class DisplayTableColumnGroup extends HTMLStyleElement {
+  constructor() { super(); this.textContent = ".ly-display-table-column-group { display: table-column-group; }"; }
+}
+
+export const $dplTableColumn$ = class DisplayTableColumn extends HTMLStyleElement {
+  constructor() { super(); this.textContent = ".ly-display-table-column { display: table-column; }"; }
+}
+
+export const $dplUnset$ = class DisplayUnset extends HTMLStyleElement {
+  constructor() { super(); this.textContent = ".ly-display-unset { display: unset; }"; }
+}
+
+export const $dplRuby$ = class DisplayRuby extends HTMLStyleElement {
+  constructor() { super(); this.textContent = ".ly-display-ruby { display: ruby; }"; }
+}
+
+export const $dplRubyBase$ = class DisplayRubyBase extends HTMLStyleElement {
+  constructor() { super(); this.textContent = ".ly-display-ruby-base { display: ruby-base; }"; }
+}
+
+export const $dplRubyText$ = class DisplayRubyText extends HTMLStyleElement {
+  constructor() { super(); this.textContent = ".ly-display-ruby-text { display: ruby-text; }"; }
+}
+
+export const $dplRubyBaseContainer$ = class DisplayRubyBaseContainer extends HTMLStyleElement {
+  constructor() { super(); this.textContent = ".ly-display-ruby-base-container { display: ruby-base-container; }"; }
+}
+
+export const $dplRubyTextContainer$ = class DisplayRubyTextContainer extends HTMLStyleElement {
+  constructor() { super(); this.textContent = ".ly-display-ruby-text-container { display: ruby-text-container; }"; }
+}
+
+export const $dplMozBox$ = class DisplayMozBox extends HTMLStyleElement {
+  constructor() { super(); this.textContent = ".ly-display-moz-box { display: -moz-box; }"; }
+}
+
+export const $dplMozDeck$ = class DisplayMozDeck extends HTMLStyleElement {
+  constructor() { super(); this.textContent = ".ly-display-moz-deck { display: -moz-deck; }"; }
+}
+
+export const $dplMozGrid$ = class DisplayMozGrid extends HTMLStyleElement {
+  constructor() { super(); this.textContent = ".ly-display-moz-grid { display: -moz-grid; }"; }
+}
+
+export const $dplMozGridGroup$ = class DisplayMozGridGroup extends HTMLStyleElement {
+  constructor() { super(); this.textContent = ".ly-display-moz-grid-group { display: -moz-grid-group; }"; }
+}
+
+export const $dplMozGridLine$ = class DisplayMozGridLine extends HTMLStyleElement {
+  constructor() { super(); this.textContent = ".ly-display-moz-grid-line { display: -moz-grid-line; }"; }
+}
+
+export const $dplMozGroupbox$ = class DisplayMozGroupbox extends HTMLStyleElement {
+  constructor() { super(); this.textContent = ".ly-display-moz-groupbox { display: -moz-groupbox; }"; }
+}
+
+export const $dplMozInlineBox$ = class DisplayMozInlineBox extends HTMLStyleElement {
+  constructor() { super(); this.textContent = ".ly-display-moz-inline-box { display: -moz-inline-box; }"; }
+}
+
+export const $dplMozInlineGrid$ = class DisplayMozInlineGrid extends HTMLStyleElement {
+  constructor() { super(); this.textContent = ".ly-display-moz-inline-grid { display: -moz-inline-grid; }"; }
+}
+
+export const $dplMozInlineStack$ = class DisplayMozInlineStack extends HTMLStyleElement {
+  constructor() { super(); this.textContent = ".ly-display-moz-inline-stack { display: -moz-inline-stack; }"; }
+}
+
+export const $dplMozInlineTable$ = class DisplayMozInlineTable extends HTMLStyleElement {
+  constructor() { super(); this.textContent = ".ly-display-moz-inline-table { display: -moz-inline-table; }"; }
+}
+
+export const $dplMozMarker$ = class DisplayMozMarker extends HTMLStyleElement {
+  constructor() { super(); this.textContent = ".ly-display-moz-marker { display: -moz-marker; }"; }
+}
+
+export const $dplMozPopup$ = class DisplayMozPopup extends HTMLStyleElement {
+  constructor() { super(); this.textContent = ".ly-display-moz-popup { display: -moz-popup; }"; }
+}
+
+export const $dplMozStack$ = class DisplayMozStack extends HTMLStyleElement {
+  constructor() { super(); this.textContent = ".ly-display-moz-stack { display: -moz-stack; }"; }
+}
+
+export const $dplWebkitBox$ = class DisplayWebkitBox extends HTMLStyleElement {
+  constructor() { super(); this.textContent = ".ly-display-webkit-box { display: -webkit-box; }"; }
+}
+
+export const $dplWebkitInlineBox$ = class DisplayWebkitInlineBox extends HTMLStyleElement {
+  constructor() { super(); this.textContent = ".ly-display-webkit-inline-box { display: -webkit-inline-box; }"; }
+}
+
+export const $dplWebkitFlex$ = class DisplayWebkitFlex extends HTMLStyleElement {
+  constructor() { super(); this.textContent = ".ly-display-webkit-flex { display: -webkit-flex; }"; }
+}
+
+export const $dplWebkitInlineFlex$ = class DisplayWebkitInlineFlex extends HTMLStyleElement {
+  constructor() { super(); this.textContent = ".ly-display-webkit-inline-flex { display: -webkit-inline-flex; }"; }
+}
--- api/imports/lydrocDecorationApp/components/lydroc-decoration-styles-elements.ts
+++ api/imports/lydrocDecorationApp/components/lydroc-decoration-styles-elements.ts
@@ -1,1 +1,5 @@
-export const LyDecElements = { };
+export const LyDecElements = {
+
+  display: { }
+
+};

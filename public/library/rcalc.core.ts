24c5d13734f26ded0b1cf8b6ff347d89b12a6aa4
Humberto Bravo <humbandroid@gmail.com>
2022-05-26 02:10:03 -0300

    Wertsfy API v1.0 Beta - Criando as demais bibliotecas.

--- public/library/rcalc.core.ts
+++
@@ -1,5 +0,0 @@
-import { kMath } from "./kmath.lib";
-import { terminalPencils } from "./terminal.lib";
-
-console.log(terminalPencils.adicionaCorAzulNormal(`${kMath.abs(-1)}`));
-console.log(terminalPencils.adicionaCorAmareloNegrito(`${kMath.abs(-1)}`));

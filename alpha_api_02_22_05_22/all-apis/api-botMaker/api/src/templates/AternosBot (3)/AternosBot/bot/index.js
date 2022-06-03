370a903898bc7558a945bc0f8c493b8cf381f0cf
Humberto Bravo <humbandroid@gmail.com>
2022-06-03 02:37:24 -0300

    Wertsfy v1.0 Beta - Arrumando WTerminal Lib \\2.

--- alpha_api_02_22_05_22/all-apis/api-botMaker/api/src/templates/AternosBot (3)/AternosBot/bot/index.js
+++ alpha_api_02_22_05_22/all-apis/api-botMaker/api/src/templates/AternosBot (3)/AternosBot/bot/index.js
@@ -14,7 +14,7 @@ const client = new Client({
   ]
 });
 
-client.login("ODk3OTY5NDIzNjExMjAzNjM2.YWdZMg.7Aw33k7dtI_tgu_x3h2ZNVULGKs");
+client.login("<token>");
 
 client.on("ready", () => { console.log("O bot está pronto para ser executado!"); });
 

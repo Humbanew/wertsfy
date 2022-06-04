de38b96ea64caa4ddfdc882fad8d409529edd322
Humberto Bravo <humbandroid@gmail.com>
2022-06-04 03:01:43 -0300

    Wertsfy v1.0 Beta - Adicionando outras bibliotecas \\2.

--- public/library/wcharacter.lib.ts
+++ public/library/wcharacter.lib.ts
@@ -1,3 +1,7 @@
 // Lembrete: Adicionar os caracteres unicode e ascii na classe.
 
-class WCharacter { } export var wCharacter = new WCharacter();
+class WCharacter {
+
+
+  
+} export var wCharacter = new WCharacter();

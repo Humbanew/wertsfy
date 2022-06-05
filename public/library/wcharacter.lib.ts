be380622b2c584d203cec3ab563790bd73ae8ddc
Humberto Bravo <humbandroid@gmail.com>
2022-06-05 01:17:51 -0300

    Wertsfy v1.0 Beta - Adicionando outras bibliotecas \\8.

--- public/library/wcharacter.lib.ts
+++ public/library/wcharacter.lib.ts
@@ -2,5 +2,40 @@
 
 class WCharacter {
 
+  /**
+   * @description Retorna o caractere unicode 2190. ``←``
+   */
+  public static arrowU2190: string = '\u2190';
+
+  /**
+   * @description Retorna o caractere unicode 2191. ``↑``
+   */
+  public static arrowU2191: string = '\u2191';
+
+  /**
+   * @description Retorna o caractere unicode 2192. ``→``
+   */
+  public static arrowU2192: string = '\u2192';
+
+  /**
+   * @description Retorna o caractere unicode 2193. ``↓``
+   */
+  public static arrowU2193: string = '\u2193';
+
+  /**
+   * @description Retorna o caractere unicode 2194. ``↔``
+   */
+  public static arrowU2194: string = '\u2194';
+
+  /**
+   * @description Retorna o caractere unicode 2195. ``↕``
+   */
+  public static arrowU2195: string = '\u2195';
+
+  /**
+   * @description Retorna o caractere unicode 2196. ``↖``
+   */
+  public static arrowU2196: string = '\u2196';
+
 
 } export var wCharacter = new WCharacter();

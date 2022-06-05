1cc901a61f0ad7c1ae6e702024c206ec3673a48f
Humberto Bravo <humbandroid@gmail.com>
2022-06-05 02:38:06 -0300

    Wertsfy v1.0 Beta - Desenvolvendo wcharracter.lis.ts

--- public/library/wcharacter.lib.ts
+++ public/library/wcharacter.lib.ts
@@ -37,5 +37,622 @@ class WCharacter {
    */
   public ARROW_U2196: string = '\u2196';
 
+  /**
+   * @description Retorna o caractere unicode 2197. ``↗``
+   */
+  public ARROW_U2197: string = '\u2197';
+
+  /**
+   * @description Retorna o caractere unicode 2198. ``↘``
+   */
+  public ARROW_U2198: string = '\u2198';
+
+  /**
+   * @description Retorna o caractere unicode 2199. ``↙``
+   */
+  public ARROW_U2199: string = '\u2199';
+
+  /**
+   * @description Retorna o caractere unicode 219A. ``↚``
+   */
+  public ARROW_U219A: string = '\u219A';
+
+  /**
+   * @description Retorna o caractere unicode 219B. ``↛``
+   */
+  public ARROW_U219B: string = '\u219B';
+
+  /**
+   * @description Retorna o caractere unicode 219C. ``↜``
+   */
+  public ARROW_U219C: string = '\u219C';
+
+  /**
+   * @description Retorna o caractere unicode 219D. ``↝``
+   */
+  public ARROW_U219D: string = '\u219D';
+
+  /**
+   * @description Retorna o caractere unicode 219E. ``↞``
+   */
+  public ARROW_U219E: string = '\u219E';
+
+  /**
+   * @description Retorna o caractere unicode 219F. ``↟``
+   */
+  public ARROW_U219F: string = '\u219F';
+
+  /**
+   * @description Retorna o caractere unicode 21A0. ``↠``
+   */
+  public ARROW_U21A0: string = '\u21A0';
+
+  /**
+   * @description Retorna o caractere unicode 21A1. ``↡``
+   */
+  public ARROW_U21A1: string = '\u21A1';
+
+  /**
+   * @description Retorna o caractere unicode 21A2. ``↢``
+   */
+  public ARROW_U21A2: string = '\u21A2';
+
+  /**
+   * @description Retorna o caractere unicode 21A3. ``↣``
+   */
+  public ARROW_U21A3: string = '\u21A3';
+
+  /**
+   * @description Retorna o caractere unicode 21A4. ``↤``
+   */
+  public ARROW_U21A4: string = '\u21A4';
+
+  /**
+   * @description Retorna o caractere unicode 21A5. ``↥``
+   */
+  public ARROW_U21A5: string = '\u21A5';
+
+  /**
+   * @description Retorna o caractere unicode 21A6. ``↦``
+   */
+  public ARROW_U21A6: string = '\u21A6';
+
+  /**
+   * @description Retorna o caractere unicode 21A7. ``↧``
+   */
+  public ARROW_U21A7: string = '\u21A7';
+
+  /**
+   * @description Retorna o caractere unicode 21A8. ``↨``
+   */
+  public ARROW_U21A8: string = '\u21A8';
+
+  /**
+   * @description Retorna o caractere unicode 21A9. ``↩``
+   */
+  public ARROW_U21A9: string = '\u21A9';
+
+  /**
+   * @description Retorna o caractere unicode 21AA. ``↪``
+   */
+  public ARROW_U21AA: string = '\u21AA';
+
+  /**
+   * @description Retorna o caractere unicode 21AB. ``↫``
+   */
+  public ARROW_U21AB: string = '\u21AB';
+
+  /**
+   * @description Retorna o caractere unicode 21AC. ``↬``
+   */
+  public ARROW_U21AC: string = '\u21AC';
+
+  /**
+   * @description Retorna o caractere unicode 21AD. ``↭``
+   */
+  public ARROW_U21AD: string = '\u21AD';
+
+  /**
+   * @description Retorna o caractere unicode 21AE. ``↮``
+   */
+  public ARROW_U21AE: string = '\u21AE';
+
+  /**
+   * @description Retorna o caractere unicode 21AF. ``↯``
+   */
+  public ARROW_U21AF: string = '\u21AF';
+
+  /**
+   * @description Retorna o caractere unicode 21B0. ``↰``
+   */
+  public ARROW_U21B0: string = '\u21B0';
+
+  /**
+   * @description Retorna o caractere unicode 21B1. ``↱``
+   */
+  public ARROW_U21B1: string = '\u21B1';
+
+  /**
+   * @description Retorna o caractere unicode 21B2. ``↲``
+   */
+  public ARROW_U21B2: string = '\u21B2';
+
+  /**
+   * @description Retorna o caractere unicode 21B3. ``↳``
+   */
+  public ARROW_U21B3: string = '\u21B3';
+
+  /**
+   * @description Retorna o caractere unicode 21B4. ``↴``
+   */
+  public ARROW_U21B4: string = '\u21B4';
+
+  /**
+   * @description Retorna o caractere unicode 21B5. ``↵``
+   */
+  public ARROW_U21B5: string = '\u21B5';
+
+  /**
+   * @description Retorna o caractere unicode 21B6. ``↶``
+   */
+  public ARROW_U21B6: string = '\u21B6';
+
+  /**
+   * @description Retorna o caractere unicode 21B7. ``↷``
+   */
+  public ARROW_U21B7: string = '\u21B7';
+
+  /**
+   * @description Retorna o caractere unicode 21B8. ``↸``
+   */
+  public ARROW_U21B8: string = '\u21B8';
+
+  /**
+   * @description Retorna o caractere unicode 21B9. ``↹``
+   */
+  public ARROW_U21B9: string = '\u21B9';
+
+  /**
+   * @description Retorna o caractere unicode 21BA. ``↺``
+   */
+  public ARROW_U21BA: string = '\u21BA';
+
+  /**
+   * @description Retorna o caractere unicode 21BB. ``↻``
+   */
+  public ARROW_U21BB: string = '\u21BB';
+
+  /**
+   * @description Retorna o caractere unicode 21BC. ``↼``
+   */
+  public ARROW_U21BC: string = '\u21BC';
+
+  /**
+   * @description Retorna o caractere unicode 21BD. ``↽``
+   */
+  public ARROW_U21BD: string = '\u21BD';
+
+  /**
+   * @description Retorna o caractere unicode 21BE. ``↾``
+   */
+  public ARROW_U21BE: string = '\u21BE';
+
+  /**
+   * @description Retorna o caractere unicode 21BF. ``↿``
+   */
+  public ARROW_U21BF: string = '\u21BF';
+
+  /**
+   * @description Retorna o caractere unicode 21C0. ``⇀``
+   */
+  public ARROW_U21C0: string = '\u21C0';
+
+  /**
+   * @description Retorna o caractere unicode 21C1. ``⇁``
+   */
+  public ARROW_U21C1: string = '\u21C1';
+
+  /**
+   * @description Retorna o caractere unicode 21C2. ``⇂``
+   */
+  public ARROW_U21C2: string = '\u21C2';
+
+  /**
+   * @description Retorna o caractere unicode 21C3. ``⇃``
+   */
+  public ARROW_U21C3: string = '\u21C3';
+
+  /**
+   * @description Retorna o caractere unicode 21C4. ``⇄``
+   */
+  public ARROW_U21C4: string = '\u21C4';
+
+  /**
+   * @description Retorna o caractere unicode 21C5. ``⇅``
+   */
+  public ARROW_U21C5: string = '\u21C5';
+
+  /**
+   * @description Retorna o caractere unicode 21C6. ``⇆``
+   */
+  public ARROW_U21C6: string = '\u21C6';
+
+  /**
+   * @description Retorna o caractere unicode 21C7. ``⇇``
+   */
+  public ARROW_U21C7: string = '\u21C7';
+
+  /**
+   * @description Retorna o caractere unicode 21C8. ``⇈``
+   */
+  public ARROW_U21C8: string = '\u21C8';
+
+  /**
+   * @description Retorna o caractere unicode 21C9. ``⇉``
+   */
+  public ARROW_U21C9: string = '\u21C9';
+
+  /**
+   * @description Retorna o caractere unicode 21CA. ``⇊``
+   */
+  public ARROW_U21CA: string = '\u21CA';
+
+  /**
+   * @description Retorna o caractere unicode 21CB. ``⇋``
+   */
+  public ARROW_U21CB: string = '\u21CB';
+  
+  /**
+   * @description Retorna o caractere unicode 21CC. ``⇌``
+   */
+  public ARROW_U21CC: string = '\u21CC';
+
+  /**
+   * @description Retorna o caractere unicode 21CD. ``⇍``
+   */
+  public ARROW_U21CD: string = '\u21CD';
+
+  /**
+   * @description Retorna o caractere unicode 21CE. ``⇎``
+   */
+  public ARROW_U21CE: string = '\u21CE';
+
+  /**
+   * @description Retorna o caractere unicode 21CF. ``⇏``
+   */
+  public ARROW_U21CF: string = '\u21CF';
+
+  /**
+   * @description Retorna o caractere unicode 21D0. ``⇐``
+   */
+  public ARROW_U21D0: string = '\u21D0';
+
+  /**
+   * @description Retorna o caractere unicode 21D1. ``⇑``
+   */
+  public ARROW_U21D1: string = '\u21D1';
+
+  /**
+   * @description Retorna o caractere unicode 21D2. ``⇒``
+   */
+  public ARROW_U21D2: string = '\u21D2';
+
+  /**
+   * @description Retorna o caractere unicode 21D3. ``⇓``
+   */
+  public ARROW_U21D3: string = '\u21D3';
+
+  /**
+   * @description Retorna o caractere unicode 21D4. ``⇔``
+   */
+  public ARROW_U21D4: string = '\u21D4';
+
+  /**
+   * @description Retorna o caractere unicode 21D5. ``⇕``
+   */
+  public ARROW_U21D5: string = '\u21D5';
+
+  /**
+   * @description Retorna o caractere unicode 21D6. ``⇖``
+   */
+  public ARROW_U21D6: string = '\u21D6';
+
+  /**
+   * @description Retorna o caractere unicode 21D7. ``⇗``
+   */
+  public ARROW_U21D7: string = '\u21D7';
+
+  /**
+   * @description Retorna o caractere unicode 21D8. ``⇘``
+   */
+  public ARROW_U21D8: string = '\u21D8';
+
+  /**
+   * @description Retorna o caractere unicode 21D9. ``⇙``
+   */
+  public ARROW_U21D9: string = '\u21D9';
+
+  /**
+   * @description Retorna o caractere unicode 21DA. ``⇚``
+   */
+  public ARROW_U21DA: string = '\u21DA';
+
+  /**
+   * @description Retorna o caractere unicode 21DB. ``⇛``
+   */
+  public ARROW_U21DB: string = '\u21DB';
+
+  /**
+   * @description Retorna o caractere unicode 21DC. ``⇜``
+   */
+  public ARROW_U21DC: string = '\u21DC';
+
+  /**
+   * @description Retorna o caractere unicode 21DD. ``⇝``
+   */
+  public ARROW_U21DD: string = '\u21DD';
+
+  /**
+   * @description Retorna o caractere unicode 21DE. ``⇞``
+   */
+  public ARROW_U21DE: string = '\u21DE';
+
+  /**
+   * @description Retorna o caractere unicode 21DF. ``⇟``
+   */
+  public ARROW_U21DF: string = '\u21DF';
+
+  /**
+   * @description Retorna o caractere unicode 21E0. ``⇠``
+   */
+  public ARROW_U21E0: string = '\u21E0';
+
+  /**
+   * @description Retorna o caractere unicode 21E1. ``⇡``
+   */
+  public ARROW_U21E1: string = '\u21E1';
+
+  /**
+   * @description Retorna o caractere unicode 21E2. ``⇢``
+   */
+  public ARROW_U21E2: string = '\u21E2';
+
+  /**
+   * @description Retorna o caractere unicode 21E3. ``⇣``
+   */
+  public ARROW_U21E3: string = '\u21E3';
+
+  /**
+   * @description Retorna o caractere unicode 21E4. ``⇤``
+   */
+  public ARROW_U21E4: string = '\u21E4';
+
+  /**
+   * @description Retorna o caractere unicode 21E5. ``⇥``
+   */
+  public ARROW_U21E5: string = '\u21E5';
+
+  /**
+   * @description Retorna o caractere unicode 21E6. ``⇦``
+   */
+  public ARROW_U21E6: string = '\u21E6';
+
+  /**
+   * @description Retorna o caractere unicode 21E7. ``⇧``
+   */
+  public ARROW_U21E7: string = '\u21E7';
+
+  /**
+   * @description Retorna o caractere unicode 21E8. ``⇨``
+   */
+  public ARROW_U21E8: string = '\u21E8';
+
+  /**
+   * @description Retorna o caractere unicode 21E9. ``⇩``
+   */
+  public ARROW_U21E9: string = '\u21E9';
+
+  /**
+   * @description Retorna o caractere unicode 21EA. ``⇪``
+   */
+  public ARROW_U21EA: string = '\u21EA';
+
+  /**
+   * @description Retorna o caractere unicode 21EB. ``⇫``
+   */
+  public ARROW_U21EB: string = '\u21EB';
+
+  /**
+   * @description Retorna o caractere unicode 21EC. ``⇬``
+   */
+  public ARROW_U21EC: string = '\u21EC';
+
+  /**
+   * @description Retorna o caractere unicode 21ED. ``⇭``
+   */
+  public ARROW_U21ED: string = '\u21ED';
+
+  /**
+   * @description Retorna o caractere unicode 21EE. ``⇮``
+   */
+  public ARROW_U21EE: string = '\u21EE';
+
+  /**
+   * @description Retorna o caractere unicode 21EF. ``⇯``
+   */
+  public ARROW_U21EF: string = '\u21EF';
+
+  /**
+   * @description Retorna o caractere unicode 21F0. ``⇰``
+   */
+  public ARROW_U21F0: string = '\u21F0';
+
+  /**
+   * @description Retorna o caractere unicode 21F1. ``⇱``
+   */
+  public ARROW_U21F1: string = '\u21F1';
+
+  /**
+   * @description Retorna o caractere unicode 21F2. ``⇲``
+   */
+  public ARROW_U21F2: string = '\u21F2';
+
+  /**
+   * @description Retorna o caractere unicode 21F3. ``⇳``
+   */
+  public ARROW_U21F3: string = '\u21F3';
+
+  /**
+   * @description Retorna o caractere unicode 21F4. ``⇴``
+   */
+  public ARROW_U21F4: string = '\u21F4';
+
+  /**
+   * @description Retorna o caractere unicode 21F5. ``⇵``
+   */
+  public ARROW_U21F5: string = '\u21F5';
+
+  /**
+   * @description Retorna o caractere unicode 21F6. ``⇶``
+   */
+  public ARROW_U21F6: string = '\u21F6';
+
+  /**
+   * @description Retorna o caractere unicode 21F7. ``⇷``
+   */
+  public ARROW_U21F7: string = '\u21F7';
+
+  /**
+   * @description Retorna o caractere unicode 21F8. ``⇸``
+   */
+  public ARROW_U21F8: string = '\u21F8';
+
+  /**
+   * @description Retorna o caractere unicode 21F9. ``⇹``
+   */
+  public ARROW_U21F9: string = '\u21F9';
+
+  /**
+   * @description Retorna o caractere unicode 21FA. ``⇺``
+   */
+  public ARROW_U21FA: string = '\u21FA';
+
+  /**
+   * @description Retorna o caractere unicode 21FB. ``⇻``
+   */
+  public ARROW_U21FB: string = '\u21FB';
+
+  /**
+   * @description Retorna o caractere unicode 21FC. ``⇼``
+   */
+  public ARROW_U21FC: string = '\u21FC';
+
+  /**
+   * @description Retorna o caractere unicode 21FD. ``⇽``
+   */
+  public ARROW_U21FD: string = '\u21FD';
+
+  /**
+   * @description Retorna o caractere unicode 21FE. ``⇾``
+   */
+  public ARROW_U21FE: string = '\u21FE';
+
+  /**
+   * @description Retorna o caractere unicode 21FF. ``⇿``
+   */
+  public ARROW_U21FF: string = '\u21FF';
+
+  /**
+   * @description Retorna o caractere unicode 2B00. ``⬀``
+   */
+  public MISC_SYMB_ARROW_U2B00: string = '\u2B00';
+
+  /**
+   * @description Retorna o caractere unicode 2B01. ``⬁``
+   */
+  public MISC_SYMB_ARROW_U2B01: string = '\u2B01';
+
+  /**
+   * @description Retorna o caractere unicode 2B02. ``⬂``
+   */
+  public MISC_SYMB_ARROW_U2B02: string = '\u2B02';
+
+  /**
+   * @description Retorna o caractere unicode 2B03. ``⬃``
+   */
+  public MISC_SYMB_ARROW_U2B03: string = '\u2B03';
+
+  /**
+   * @description Retorna o caractere unicode 2B04. ``⬄``
+   */
+  public MISC_SYMB_ARROW_U2B04: string = '\u2B04';
+
+  /**
+   * @description Retorna o caractere unicode 2B05. ``⬅``
+   */
+  public MISC_SYMB_ARROW_U2B05: string = '\u2B05';
+
+  /**
+   * @description Retorna o caractere unicode 2B06. ``⬆``
+   */
+  public MISC_SYMB_ARROW_U2B06: string = '\u2B06';
+
+  /**
+   * @description Retorna o caractere unicode 2B07. ``⬇``
+   */
+  public MISC_SYMB_ARROW_U2B07: string = '\u2B07';
+
+  /**
+   * @description Retorna o caractere unicode 2B08. ``⬈``
+   */
+  public MISC_SYMB_ARROW_U2B08: string = '\u2B08';
+
+  /**
+   * @description Retorna o caractere unicode 2B09. ``⬉``
+   */
+  public MISC_SYMB_ARROW_U2B09: string = '\u2B09';
+
+  /**
+   * @description Retorna o caractere unicode 2B0A. ``⬊``
+   */
+  public MISC_SYMB_ARROW_U2B0A: string = '\u2B0A';
+
+  /**
+   * @description Retorna o caractere unicode 2B0B. ``⬋``
+   */
+  public MISC_SYMB_ARROW_U2B0B: string = '\u2B0B';
+
+  /**
+   * @description Retorna o caractere unicode 2B0C. ``⬌``
+   */
+  public MISC_SYMB_ARROW_U2B0C: string = '\u2B0C';
+
+  /**
+   * @description Retorna o caractere unicode 2B0D. ``⬍``
+   */
+  public MISC_SYMB_ARROW_U2B0D: string = '\u2B0D';
+
+  /**
+   * @description Retorna o caractere unicode 2B0E. ``⬎``
+   */
+  public MISC_SYMB_ARROW_U2B0E: string = '\u2B0E';
+
+  /**
+   * @description Retorna o caractere unicode 2B0F. ``⬏``
+   */
+  public MISC_SYMB_ARROW_U2B0F: string = '\u2B0F';
+
+  // MISC SYMBOLS AND ARROWS: U2B00 - U2BF0;
+  // Supplemental Arrows-A: U27F0 - U27FF;
+  // Supplemental Arrows-B: U2900 - U297F;
+  // Supplemental Arrows-C: U1F80 - U1F8F;
+  // Mathematical Unicodes;
+  // Miscellaneous Technical Unicodes;
+  // Box Drawing Unicodes;
+  // Block Elements Unicodes;
+  // Geometric Shapes Unicodes;
+  // Miscellaneous Symbols Unicodes;
+  // Enclosed Alphanumerics Unicodes;
+  // Optical Character Recognition Unicodes;
 
 } export var wCharacter = new WCharacter();

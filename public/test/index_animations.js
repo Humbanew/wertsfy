9fdadbf9438438498e769246670c772ac514b3fa
Humberto Bravo <humbandroid@gmail.com>
2022-06-06 22:54:17 -0300

    Wertsfy v1.0 Beta - Arrumando bibliotecas de caracteres e arquivos de testes.

---
+++ public/test/index_animations.js
@@ -0,0 +1,265 @@
+const animations = { };
+
+let board = document.getElementById('animation_board');
+
+let ctx = board.getContext('2d');
+
+ctx.fillStyle = '#0033ff';
+ctx.fillRect(300, 40, 50, 50);
+
+ctx.fillStyle = '#0033ff';
+ctx.fillRect(350+2, 40, 50, 50);
+
+ctx.fillStyle = '#0033ff';
+ctx.fillRect(400+4, 40, 50, 50);
+
+ctx.fillStyle = '#0033ff';
+ctx.fillRect(450+6, 40, 50, 50);
+
+ctx.fillStyle = '#0033ff';
+ctx.fillRect(500+8, 40, 50, 50);
+
+ctx.fillStyle = '#0033ff';
+ctx.fillRect(550+10, 40, 50, 50);
+
+ctx.fillStyle = '#0033ff';
+ctx.fillRect(600+12, 40, 50, 50);
+
+ctx.fillStyle = '#0033ff';
+ctx.fillRect(650+14, 40, 50, 50);
+
+ctx.fillStyle = '#0033ff';
+ctx.fillRect(700+16, 40, 50, 50);
+
+ctx.fillStyle = '#0033ff';
+ctx.fillRect(750+18, 40, 50, 50);
+
+ctx.fillStyle = '#0033ff';
+ctx.fillRect(800+20, 40, 50, 50);
+
+ctx.fillStyle = '#0033ff';
+ctx.fillRect(850+22, 40, 50, 50);
+
+setInterval(() => { 
+  anim();
+}, (12*100)+(12*200)+290);
+
+function anim() {
+
+  setTimeout(() => {
+      ctx.fillStyle = '#001155';
+      ctx.fillRect(300, 40, 50, 50);
+  
+      setTimeout(() => {
+        ctx.fillStyle = '#001155';
+        ctx.fillRect(350+2, 40, 50, 50);
+      
+        setTimeout(() => {
+          ctx.fillStyle = '#001155';
+          ctx.fillRect(400+4, 40, 50, 50);
+  
+          setTimeout(() => {
+            ctx.fillStyle = '#001155';
+            ctx.fillRect(450+6, 40, 50, 50);
+  
+            setTimeout(() => {
+              ctx.fillStyle = '#001155';
+              ctx.fillRect(500+8, 40, 50, 50);
+  
+              setTimeout(() => {
+                ctx.fillStyle = '#001155';
+                ctx.fillRect(550+10, 40, 50, 50);
+  
+                setTimeout(() => {
+                  ctx.fillStyle = '#001155';
+                  ctx.fillRect(600+12, 40, 50, 50);
+  
+                  setTimeout(() => {
+                    ctx.fillStyle = '#001155';
+                    ctx.fillRect(650+14, 40, 50, 50);
+  
+                    setTimeout(() => {
+                      ctx.fillStyle = '#001155';
+                      ctx.fillRect(700+16, 40, 50, 50);
+  
+                      setTimeout(() => {
+                        ctx.fillStyle = '#001155';
+                        ctx.fillRect(750+18, 40, 50, 50);
+  
+                        setTimeout(() => {
+                          ctx.fillStyle = '#001155';
+                          ctx.fillRect(800+20, 40, 50, 50);
+  
+                          setTimeout(() => {
+                            ctx.fillStyle = '#001155';
+                            ctx.fillRect(850+22, 40, 50, 50);
+  
+                            setTimeout(() => {
+                              ctx.fillStyle = '#0033ff';
+                              ctx.fillRect(300, 40, 50, 50);
+                            
+                              setTimeout(() => {
+                                ctx.fillStyle = '#0033ff';
+                                ctx.fillRect(350+2, 40, 50, 50);
+                            
+                                setTimeout(() => {
+                                  ctx.fillStyle = '#0033ff';
+                                  ctx.fillRect(400+4, 40, 50, 50);
+                            
+                                  setTimeout(() => {
+                                    ctx.fillStyle = '#0033ff';
+                                    ctx.fillRect(450+6, 40, 50, 50);
+                            
+                                    setTimeout(() => {
+                                      ctx.fillStyle = '#0033ff';
+                                      ctx.fillRect(500+8, 40, 50, 50);
+                            
+                                      setTimeout(() => {
+                                        ctx.fillStyle = '#0033ff';
+                                        ctx.fillRect(550+10, 40, 50, 50);
+                            
+                                        setTimeout(() => {
+                                          ctx.fillStyle = '#0033ff';
+                                          ctx.fillRect(600+12, 40, 50, 50);
+                            
+                                          setTimeout(() => {
+                                            ctx.fillStyle = '#0033ff';
+                                            ctx.fillRect(650+14, 40, 50, 50);
+                            
+                                            setTimeout(() => {
+                                              ctx.fillStyle = '#0033ff';
+                                              ctx.fillRect(700+16, 40, 50, 50);
+                            
+                                              setTimeout(() => {
+                                                ctx.fillStyle = '#0033ff';
+                                                ctx.fillRect(750+18, 40, 50, 50);
+                            
+                                                setTimeout(() => {
+                                                  ctx.fillStyle = '#0033ff';
+                                                  ctx.fillRect(800+20, 40, 50, 50);
+                            
+                                                  setTimeout(() => {
+                                                    ctx.fillStyle = '#0033ff';
+                                                    ctx.fillRect(850+22, 40, 50, 50);
+                                                  }, 200);
+                                                }, 200);
+                                              }, 200);
+                                            }, 200);
+                                          }, 200);
+                                        }, 200);
+                                      }, 200);
+                                    }, 200);
+                                  }, 200);
+                                }, 200);
+                              }, 200);
+                            
+                            }, 200);
+  
+                          }, 100);
+                        
+                        }, 100);
+  
+                      }, 100);
+                    
+                    }, 100);
+                  
+                  }, 100);
+                
+                }, 100);
+              
+              }, 100);
+            
+            }, 100);
+          
+          }, 100);
+  
+        }, 100);
+      
+      }, 100);
+  
+  }, 100);
+
+}
+
+// Animação 2
+ctx.fillStyle = '#888888';
+ctx.fillRect(300, 100, 625, 20);
+
+ctx.fillStyle = '#ee0000';
+ctx.fillRect(300, 100, 0, 20);
+
+setTimeout(() => {
+  ctx.fillStyle = '#ee0000';
+  ctx.fillRect(300, 100, 2, 20);
+
+  setTimeout(() => {
+    ctx.fillStyle = '#ee0000';
+    ctx.fillRect(300, 100, 4, 20);
+
+    setTimeout(() => {
+      ctx.fillStyle = '#ee0000';
+      ctx.fillRect(300, 100, 6, 20);
+
+      setTimeout(() => {
+        ctx.fillStyle = '#ee0000';
+        ctx.fillRect(300, 100, 8, 20);
+
+        setTimeout(() => {
+          ctx.fillStyle = '#ee0000';
+          ctx.fillRect(300, 100, 10, 20);
+
+          setTimeout(() => {
+            ctx.fillStyle = '#ee0000';
+            ctx.fillRect(300, 100, 12, 20);
+
+            setTimeout(() => {
+              ctx.fillStyle = '#ee0000';
+              ctx.fillRect(300, 100, 14, 20);
+
+              setTimeout(() => {
+                ctx.fillStyle = '#ee0000';
+                ctx.fillRect(300, 100, 16, 20);
+
+                setTimeout(() => {
+                  ctx.fillStyle = '#ee0000';
+                  ctx.fillRect(300, 100, 18, 20);
+
+                  setTimeout(() => {
+                    ctx.fillStyle = '#ee0000';
+                    ctx.fillRect(300, 100, 20, 20);
+
+                    setTimeout(() => {
+                      ctx.fillStyle = '#ee0000';
+                      ctx.fillRect(300, 100, 22, 20);
+
+                      setTimeout(() => {
+                        ctx.fillStyle = '#ee0000';
+                        ctx.fillRect(300, 100, 24, 20);
+
+                        setTimeout(() => {
+                          ctx.fillStyle = '#ee0000';
+                          ctx.fillRect(300, 100, 26, 20);
+
+                          setTimeout(() => {
+                            ctx.fillStyle = '#ee0000';
+                            ctx.fillRect(300, 100, 28, 20);
+
+                            setTimeout(() => {
+                              ctx.fillStyle = '#ee0000';
+                              ctx.fillRect(300, 100, 30, 20);
+                            }, 100);
+                          }, 100);
+                        }, 100);
+                      }, 100);
+                    }, 100);
+                  }, 100);
+                }, 100);
+              }, 100);
+            }, 100);
+          }, 100);
+        }, 100);
+      }, 100);
+    }, 100);
+  }, 100);
+}, 100);
+
---
+++ public/test/teste.lua
@@ -0,0 +1,11 @@
+global c = 1;
+
+local X = 1;
+
+for i = 1, 10 do
+  print(i);
+end
+
+function f()
+  print("f");
+end

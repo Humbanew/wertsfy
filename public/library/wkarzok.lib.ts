2b89556ca2374dfe70a23429d088b8b3ce6bc2aa
Humberto Bravo <humbandroid@gmail.com>
2022-07-04 01:58:17 -0300

    Wertsfy v1.0 Beta II - Montando outras bibliotecas.

---
+++ public/library/wkarzok.lib.ts
@@ -0,0 +1,3 @@
+class wKarzok {
+
+} var WKarzok = new wKarzok();
--- public/library/wlydroc.lib.ts
+++ public/library/wlydroc.lib.ts
@@ -1,33 +1,7 @@
+/**
+ * [HTML] =>
+ * [CSS] =>
+ */
 class wLydroc {
-
-  protected static constroeArquivoFolhaDeEstilo() { };
-
-  protected static constroeGancho() { };
-
-  protected static constroeHtmlDinamico() { };
-
-  public static adicionaClasseDisplayBlock(): string {
-    return ".ly-display-block { display: block; }";
-  }
-
-  public static addAttrClasslistDisplayBlock(elemento: string): void {
-    return document.getElementById(elemento).classList.add("ly-display-block");
-  }
-
-  public static adicionaClasseDisplayContents(): string {
-    return ".ly-display-contents { display: contents; }";
-  }
-
-  public static addAttrClasslistDisplayContents(elemento: string): void {
-    return document.getElementById(elemento).classList.add("ly-display-contents");
-  }
-
-  public static adicionaClasseDisplayInherit(): string {
-    return ".ly-display-inherit { display: inherit; }";
-  }
-
-  public static addAttrClasslistDisplayInherit(elemento: string): void {
-    return document.getElementById(elemento).classList.add("ly-display-inherit");
-  }
 
 } var WLydroc = new wLydroc();

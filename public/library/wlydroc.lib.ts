2258a989c24eb545e717ee13083dbc907cfc8a67
Humberto Bravo <humbandroid@gmail.com>
2022-07-05 01:18:32 -0300

    Wertsfy v1.0 Beta II - Montando e arrumando outras bibliotecas.

--- public/library/wlydroc.lib.ts
+++ public/library/wlydroc.lib.ts
@@ -1,7 +1,34 @@
-/**
- * [HTML] => gerenciamento de tags html via javascript/typescript
- * [CSS] => gerenciamento de arquivos e propriedades via javascript/typescript
- */
 class wLydroc {
+
+  // constantes da biblioteca
+
+  /** 
+   * @augments tag define a tag html a ser utilizada. **[ Type: ``models`` ]**.
+   * @augments idElemento define o id do elemento html a ser criado. **[ Type: ``string`` ]**.
+   * @description cria um novo elemento html.
+   */
+  public criaElemento(tag: 'abbr'|'acronym'|'address'|'anchor'|'applet'|'area'|'article'|'audio'|'b'|'base'|'basefont'|'bdi'|'bdo'|'bgsound'|'big'|'blink'|'blockquote'|'body'|'br'|'button'|'canvas'|'caption'|'center'|'cite'|'code'|'col'|'colgroup'|'content'|'data'|'datalist'|'dd'|'del'|'details'|'dfn'|'dialog'|'dir'|'div'|'dl'|'dt'|'em'|'embed'|'fieldset'|'figcaption'|'figure'|'font'|'footer'|'form'|'frameset'|'h1'|'h2'|'h3'|'h4'|'h5'|'h6'|'head'|'header'|'hgroup'|'hr'|'html'|'i'|'iframe'|'image'|'img'|'input'|'ins'|'kbd'|'keygen'|'label'|'legend'|'li'|'link'|'main'|'map'|'mark'|'marquee'|'menu'|'menuitem'|'meta'|'meter'|'nav'|'nobr'|'noembed'|'noframes'|'noscript'|'object'|'ol'|'optgroup'|'option'|'output'|'paragraph'|'param'|'picture'|'plaintext'|'portal'|'pre'|'progress'|'q'|'rb'|'rp'|'rt'|'rtc'|'ruby'|'s'|'samp'|'script'|'section'|'select'|'shadow'|'slot'|'small'|'spacer'|'span'|'strike'|'strong'|'style'|'sub'|'summary'|'sup'|'table'|'tbody'|'td'|'template'|'textarea'|'tfoot'|'th'|'thead'|'time'|'title'|'tr'|'track'|'tt'|'u'|'ul'|'var'|'video'|'wbr'|'xmp', idElemento: string): HTMLElement { 
+    const elemento = document.createElement(tag);
+    elemento.id = idElemento; 
+    return elemento; 
+  }
+
+  /**
+   * @augments id1 id do elemento base do vínculo a ser gerado. **[ Type: ``string`` ]**.
+   * @augments id2 id do elemento herdeiro do vínculo a ser gerado. **[ Type: ``string`` ]**.
+   * @description gera um vínculo entre elementos distintos.
+   */
+  public adicionaVinculoAoElemento(id1: string, id2: string): void {
+    const elemento1 = document.getElementById(id1);
+    const elemento2 = document.getElementById(id2);
+    elemento1.appendChild(elemento2);
+  }
+
+  // attrs tags html
+
+  // folhas de estilo predefinidas
+
+  // folhas de estilo customizadas
+
 
 } var WLydroc = new wLydroc();
--- public/library/wterminal.lib.ts
+++ public/library/wterminal.lib.ts
@@ -1,4 +1,6 @@
-class WTerminal {
+class wTerminal {
+
+  private static Negrito = '1m';
 
   /** 
    * @description Símbolo de entrada de dados para o terminal. 
@@ -13,7 +15,7 @@ class WTerminal {
   /**
    * @description Símbolo de divisão de partes de um programa.
    */
-  public SETUP_DIVIDER_SYMBOL: string = '<<<=====>>>';
+  public SETUP_DIVIDER_SYMBOL: string = '<|==|>';
 
   /**
    * @description Modelo de 25% da tela de carregamento de um processo no terminal.
@@ -1315,374 +1317,26 @@ class WTerminal {
    */
   public COR_TERMINAL_EEEEEE: string = '#EEEEEE';
 
-  /** 
-   * @augments texto texto a ser modificado. **[ Type: ``string`` ]**.
-   * @description Adiciona formatação em negrito no texto. 
-   */
-  public adicionaFormatacaoNegrito(texto: string): string {
-    return `\033[1m${texto}\033[0m`;
-  }
-
-  /** 
-   * @augments texto texto a ser modificado. **[ Type: ``string`` ]**.
-   * @description Adiciona formatação em faint no texto. 
-   */
-  public adicionaFormatacaoFaint(texto: string): string {
-    return `\033[2m${texto}\033[0m`;
-  }
-
-  /** 
-   * @augments texto texto a ser modificado. **[ Type: ``string`` ]**.
-   * @description Adiciona formatação em italico no texto. 
-   */
-  public adicionaFormatacaoItalico(texto: string): string {
-    return `\033[3m${texto}\033[0m`;
-  }
-
-  /** 
-   * @augments texto texto a ser modificado. **[ Type: ``string`` ]**.
-   * @description Adiciona formatação em sublinhado no texto. 
-   */
-  public adicionaFormatacaoSublinhado(texto: string): string {
-    return `\033[4m${texto}\033[0m`;
-  }
-
-  /** 
-   * @augments texto texto a ser modificado. **[ Type: ``string`` ]**.
-   * @description Adiciona formatação em slow brink no texto. 
-   */
-  public adicionaFormatacaoSlowBlink(texto: string): string {
-    return `\033[5m${texto}\033[0m`;
-  }
-
-  /** 
-   * @augments texto texto a ser modificado. **[ Type: ``string`` ]**.
-   * @description Adiciona formatação em rapid blink no texto. 
-   */
-  public adicionaFormatacaoRapidBlink(texto: string): string {
-    return `\033[6m${texto}\033[0m`;
-  }
-
-  /** 
-   * @augments texto texto a ser modificado. **[ Type: ``string`` ]**.
-   * @description Adiciona formatação em reverse no texto. 
-   */
-  public adicionaFormatacaoReverse(texto: string): string {
-    return `\033[7m${texto}\033[0m`;
-  }
-
-  /** 
-   * @augments texto texto a ser modificado. **[ Type: ``string`` ]**.
-   * @description Adiciona formatação em conceal no texto. 
-   */
-  public adicionaFormatacaoConceal(texto: string): string {
-    return `\033[8m${texto}\033[0m`;
-  }
-
-  /** 
-   * @augments texto texto a ser modificado. **[ Type: ``string`` ]**.
-   * @description Adiciona formatação em crossout no texto. 
-   */
-  public adicionaFormatacaoCrossedOut(texto: string): string {
-    return `\033[9m${texto}\033[0m`;
-  }
-
-  /** 
-   * @augments texto texto a ser modificado. **[ Type: ``string`` ]**.
-   * @description Adiciona formatação padrão no texto. 
-   */
-  public adicionaFormatacaoDefaultFont(texto: string): string {
-    return `\033[10m${texto}\033[0m`;
-  }
-
-  /** 
-   * @augments texto texto a ser modificado. **[ Type: ``string`` ]**.
-   * @description Adiciona formatação de fonte alternativa no texto. 
-   */
-  public adicionaFormatacaoAlternativeFont(texto: string): string {
-    return `\033[11m${texto}\033[0m`;
-  }
-
-  /** 
-   * @augments texto texto a ser modificado. **[ Type: ``string`` ]**.
-   * @description Adiciona formatação de fonte alternativa (dois) no texto. 
-   */
-  public adicionaFormatacaoAlternativeFont2(texto: string): string {
-    return `\033[12m${texto}\033[0m`;
-  }
-
-  /** 
-   * @augments texto texto a ser modificado. **[ Type: ``string`` ]**.
-   * @description Adiciona formatação de fonte alternativa (tres) no texto. 
-   */
-  public adicionaFormatacaoAlternativeFont3(texto: string): string {
-    return `\033[13m${texto}\033[0m`;
-  }
-
-  /** 
-   * @augments texto texto a ser modificado. **[ Type: ``string`` ]**.
-   * @description Adiciona formatação de fonte alternativa (quatro) no texto. 
-   */
-  public adicionaFormatacaoAlternativeFont4(texto: string): string {
-    return `\033[14m${texto}\033[0m`;
-  }
-
-  /** 
-   * @augments texto texto a ser modificado. **[ Type: ``string`` ]**.
-   * @description Adiciona formatação de fonte alternativa (cinco) no texto. 
-   */
-  public adicionaFormatacaoAlternativeFont5(texto: string): string {
-    return `\033[15m${texto}\033[0m`;
-  }
-
-  /** 
-   * @augments texto texto a ser modificado. **[ Type: ``string`` ]**.
-   * @description Adiciona formatação de fonte alternativa (seis) no texto. 
-   */
-  public adicionaFormatacaoAlternativeFont6(texto: string): string {
-    return `\033[16m${texto}\033[0m`;
-  }
-
-  /** 
-   * @augments texto texto a ser modificado. **[ Type: ``string`` ]**.
-   * @description Adiciona formatação de fonte alternativa (sete) no texto. 
-   */
-  public adicionaFormatacaoAlternativeFont7(texto: string): string {
-    return `\033[17m${texto}\033[0m`;
-  }
-
-  /** 
-   * @augments texto texto a ser modificado. **[ Type: ``string`` ]**.
-   * @description Adiciona formatação de fonte alternativa (oito) no texto. 
-   */
-  public adicionaFormatacaoAlternativeFont8(texto: string): string {
-    return `\033[18m${texto}\033[0m`;
-  }
-
-  /** 
-   * @augments texto texto a ser modificado. **[ Type: ``string`` ]**.
-   * @description Adiciona formatação de fonte alternativa (nove) no texto.
-   */
-  public adicionaFormatacaoAlternativeFont9(texto: string): string {
-    return `\033[19m${texto}\033[0m`;
-  }
-
-  /** 
-   * @augments texto texto a ser modificado. **[ Type: ``string`` ]**.
-   * @description Adiciona formatação de fraktur no texto. 
-   */
-  public adicionaFormatacaoFraktur(texto: string): string {
-    return `\033[20m${texto}\033[0m`;
-  }
-
-  /** 
-   * @augments texto texto a ser modificado. **[ Type: ``string`` ]**.
-   * @description Adiciona formatação de sublinhado duplo no texto. 
-   */
-  public adicionaFormatacaoDoublyUnderline(texto: string): string {
-    return `\033[21m${texto}\033[0m`;
-  }
-
-  /** 
-   * @augments texto texto a ser modificado. **[ Type: ``string`` ]**.
-   * @description Adiciona formatação de intensidade padrão no texto. 
-   */
-  public adicionaFormatacaoNormalIntensity(texto: string): string {
-    return `\033[22m${texto}\033[0m`;
-  }
-
-  /** 
-   * @augments texto texto a ser modificado. **[ Type: ``string`` ]**.
-   * @description Adiciona formatação de retirada do itálico no texto. 
-   */
-  public adicionaFormatacaoNotItalic(texto: string): string {
-    return `\033[23m${texto}\033[0m`;
-  }
-
-  /** 
-   * @augments texto texto a ser modificado. **[ Type: ``string`` ]**.
-   * @description Adiciona formatação de retirada do sublinhado no texto. 
-   */
-  public adicionaFormatacaoNotUnderline(texto: string): string {
-    return `\033[24m${texto}\033[0m`;
-  }
-
-  /** 
-   * @augments texto texto a ser modificado. **[ Type: ``string`` ]**.
-   * @description Adiciona formatação de retirada do slow blink no texto. 
-   */
-  public adicionaFormatacaoBlinkOff(texto: string): string {
-    return `\033[25m${texto}\033[0m`;
-  }
-
-  /** 
-   * @augments texto texto a ser modificado. **[ Type: ``string`` ]**.
-   * @description Adiciona formatação de espaçamento proposital no texto. 
-   */
-  public adicionaPropositalSpacing(texto: string): string {
-    return `\033[26m${texto}\033[0m`;
-  }
-
-  /** 
-   * @augments texto texto a ser modificado. **[ Type: ``string`` ]**.
-   * @description Adiciona formatação de retirada em reverse no texto. 
-   */
-  public adicionaFormatacaoReverseOff(texto: string): string {
-    return `\033[27m${texto}\033[0m`;
-  }
-
-  /** 
-   * @augments texto texto a ser modificado. **[ Type: ``string`` ]**.
-   * @description Adiciona formatação de retirada do conceal no texto. 
-   */
-  public adicionaFormatacaoConcealOff(texto: string): string {
-    return `\033[28m${texto}\033[0m`;
-  }
-
-  /** 
-   * @augments texto texto a ser modificado. **[ Type: ``string`` ]**.
-   * @description Adiciona formatação de retirada do crossout no texto. 
-   */
-  public adicionaFormatacaoNotCrossed(texto: string): string {
-    return `\033[29m${texto}\033[0m`;
-  }
-
-  /** 
-   * @augments texto texto a ser modificado. **[ Type: ``string`` ]**.
-   * @description Adiciona formatação de retirada do espaçamento proposital texto. 
-   */
-  public adicionaFormatacaoNotPropositalSpacing(texto: string): string {
-    return `\033[50m${texto}\033[0m`;
-  }
-
-  /** 
-   * @augments texto texto a ser modificado. **[ Type: ``string`` ]**.
-   * @description Adiciona formatação de framed no texto. 
-   */
-  public adicionaFormatacaoFramed(texto: string): string {
-    return `\033[51m${texto}\033[0m`;
-  }
-
-  /** 
-   * @augments texto texto a ser modificado. **[ Type: ``string`` ]**.
-   * @description Adiciona formatação de encircled no texto. 
-   */
-  public adicionaFormatacaoEncircled(texto: string): string {
-    return `\033[52m${texto}\033[0m`;
-  }
-
-  /** 
-   * @augments texto texto a ser modificado. **[ Type: ``string`` ]**.
-   * @description Adiciona formatação de overlined no texto. 
-   */
-  public adicionaFormatacaoOverlined(texto: string): string {
-    return `\033[53m${texto}\033[0m`;
-  }
-
-  /** 
-   * @augments texto texto a ser modificado. **[ Type: ``string`` ]**.
-   * @description Adiciona formatação de not framed no texto. 
-   */
-  public adicionaFormatacaoNotFramed(texto: string): string {
-    return `\033[54m${texto}\033[0m`;
-  }
-
-  /** 
-   * @augments texto texto a ser modificado. **[ Type: ``string`` ]**.
-   * @description Adiciona formatação de not encircled no texto. 
-   */
-  public adicionaFormatacaoNotEncircled(texto: string): string {
-    return `\033[55m${texto}\033[0m`;
-  }
-
-  /** 
-   * @augments texto texto a ser modificado. **[ Type: ``string`` ]**.
-   * @description Adiciona formatação de cor do sublinhado no texto. 
-   */
-  public adicionaFormatacaoCorSublinhado(texto: string): string {
-    return `\033[58m${texto}\033[0m`;
-  }
-
-  /** 
-   * @augments texto texto a ser modificado. **[ Type: ``string`` ]**.
-   * @description Adiciona formatação de sublinhado padrão no texto. 
-   */
-  public adicionaFormatacaoCorSublinhadoPadrao(texto: string): string {
-    return `\033[59m${texto}\033[0m`;
-  }
-
   /**
+   * @augments modelo tipo de formatação a ser aplicada. **[ Type: ``models`` ]**.
    * @augments texto texto a ser modificado. **[ Type: ``string`` ]**.
-   * @description Adiciona formatação de Ideograma Sublinhado no texto. 
+   * @description Adiciona formatação em um texto. 
    */
-  public adicionaFormatacaoIdeogramSublinhado(texto: string): string {
-    return `\033[60m${texto}\033[0m`;
+  public adicionaFormatacaoTexto(modelo: '01m@negrito'|'02m@faint'|'03m@italico'|'04m@sublinhado'|'05m@slowblink'|'06m@rapidblink'|'07m@reverse'|'08m@conceal'|'09m@crossedout'|'10m@defaultfont'|'11m@altfont1'|'12m@altfont2'|'13m@altfont3'|'14m@altfont4'|'15m@altfont5'|'16m@altfont6'|'17m@altfont7'|'18m@altfont8'|'19m@altfont9'|'20m@fraktur'|'21m@doublyunderline'|'22m@normalintensity'|'23m@notitalic'|'24m@notunderline'|'25m@blinkoff'|'26m@spacing'|'27m@reverseoff'|'28m@concealoff'|'29m@notcrossed'|'50m@propositalspacing'|'51m@framed'|'52m@encircled'|'53m@overlined'|'54m@notframed'|'55m@notencircled'|'58m@sublinhado'|'59m@sublinhadopadrao'|'60m@ideogramsublinhado'|'61m@ideogramsublinhadoduplo'|'62m@ideogramoverline'|'63m@ideogramoverlineduplo'|'64m@ideogramstress'|'65@noideogramattr'|'73m@superscript'|'74m@subscript'|'75m@nonsuperscriptsubscript', texto: string): string {
+    const scan: RegExp = /([0]{1})([0-9]+m)/gi;
+    const tipo = scan.exec(modelo)[1].toString();
+    return `\033[${tipo}${texto}\033[0m`;
+  };
+
+  protected adicionaCorTexto(modelo: '', texto: string): string {
+    let scan: RegExp = /([0-9]+\;)?([0-9]+\;)?([0]{1})?([0-9]+m)/gi, tipo; 
+
+    tipo = scan.exec(modelo)[0].toString();
+
+    return `\033[${tipo}${texto}\033[0m`;
   }
 
-  /** 
-   * @augments texto texto a ser modificado. **[ Type: ``string`` ]**.
-   * @description Adiciona formatação de Ideograma Sublinhado Duplo no texto. 
-   */
-  public adicionaFormatacaoIdeogramSublinhadoDuplo(texto: string): string {
-    return `\033[61m${texto}\033[0m`;
-  }
-
-  /** 
-   * @augments texto texto a ser modificado. **[ Type: ``string`` ]**.
-   * @description Adiciona formatação de Ideograma Overline no texto. 
-   */
-  public adicionaFormatacaoIdeogramOverline(texto: string): string {
-    return `\033[62m${texto}\033[0m`;
-  }
-
-  /** 
-   * @augments texto texto a ser modificado. **[ Type: ``string`` ]**.
-   * @description Adiciona formatação de Ideograma Overline Duplo no texto. 
-   */
-  public adicionaFormatacaoIdeogramOverlineDuplo(texto: string): string {
-    return `\033[63m${texto}\033[0m`;
-  }
-
-  /** 
-   * @augments texto texto a ser modificado. **[ Type: ``string`` ]**.
-   * @description Adiciona formatação de Ideograma Stress no texto. 
-   */
-  public adicionaFormatacaoIdeogramStress(texto: string): string {
-    return `\033[64m${texto}\033[0m`;
-  }
-
-  /** 
-   * @augments texto texto a ser modificado. **[ Type: ``string`` ]**.
-   * @description Adiciona formatação de retirada de Ideograma Attribute no texto. 
-   */
-  public adicionaFormatacaoNoIdeogramAttribute(texto: string): string {
-    return `\033[65m${texto}\033[0m`;
-  }
-
-  /** 
-   * @augments texto texto a ser modificado. **[ Type: ``string`` ]**.
-   * @description Adiciona formatação de Superscript no texto. 
-   */
-  public adicionaFormatacaoSuperscript(texto: string): string {
-    return `\033[73m${texto}\033[0m`;
-  }
-
-  /** 
-   * @augments texto texto a ser modificado. **[ Type: ``string`` ]**.
-   * @description Adiciona formatação de Subscript no texto. 
-   */
-  public adicionaFormatacaoSubscript(texto: string): string {
-    return `\033[74m${texto}\033[0m`;
-  }
-
-  /** 
-   * @augments texto texto a ser modificado. **[ Type: ``string`` ]**.
-   * @description Adiciona formatação de retirada de Superscript e Subscript no texto. 
-   */
-  public adicionaFormatacaoNonSuperscriptSubscript(texto: string): string {
-    return `\033[75m${texto}\033[0m`;
-  }
-
+// Parte antiga do código.
   /**
    * @augments texto texto a ser modificado. **[ Type: ``string`` ]**. 
    * @description Adiciona formatação de cor preta de texto.
@@ -5952,5 +5606,4 @@ class WTerminal {
     return `\033[48;2;${red};${green};${blue}m${texto}\033[0m`;
   }
 
-} export var wTerminal = new WTerminal();
-
+} export var WTerminal = new wTerminal();

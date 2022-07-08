9af97771cc9763f42b62d352c8c4ecfb69081fd8
Humberto Bravo <humbandroid@gmail.com>
2022-07-08 04:04:49 -0300

    Wertsfy v1.0 Beta II - Montando e arrumando outras bibliotecas. [10]

--- public/library/wxsalis.lib.ts
+++ public/library/wxsalis.lib.ts
@@ -1,9 +1,9 @@
 // Antiga HN XColors e HN Xolors
 class wXsalis {
 
-  public pegaCorTemplateTerminal(modelo: 'hex000000'|'hex800000'|'hex008000'|'hex808000'|'hex000080'|'hex800080'|'hex008080'): string {
-    const scan: RegExp = /([0-9a-f]{6})/g;
-    const formato = '#' + scan.exec(modelo)[1].toString();
+  public pegaCorTemplateTerminal(modelo: 'hex=000000'|'hex=800000'|'hex=008000'|'hex=808000'|'hex=000080'|'hex=800080'|'hex=008080'|'hex=808080'|'hex=C0C0C0'|'hex=FF0000'|'hex=00FF00'|'hex=0000FF'|'hex=FF00FF'|'hex=00FFFF'|'hex=FFFFFF'): string {
+    const scan: RegExp = /(\=)([0-9A-F]{6})/g;
+    const formato = '#' + scan.exec(modelo)[2].toString();
     return formato;
   }
 

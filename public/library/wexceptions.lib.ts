8b1ac6de3a4080bace9e85e9b60b8ada700a4ad9
Humberto Bravo <humbandroid@gmail.com>
2022-07-14 19:54:33 -0300

    Wertsfy v1.0 Beta II - Arrumando Arquivos de Biblioteca. [035]

--- public/library/wexceptions.lib.ts
+++ public/library/wexceptions.lib.ts
@@ -1,17 +1,119 @@
 class wExceptions {
 
-  protected lancaErroTerminal() { }
+  public lancaErroDeExec(erro: Error): void { 
+    throw console.error('W0101 - Error to execute same librarys.'), erro; 
+  }
 
-  protected lancaErroSpectrals() { }
+  public lancaErroDeExecDeFuncao(erro: Error): void { 
+    throw console.error('W0102 - Error to execute function.'), erro; 
+  }
 
-  protected lancaErroMath() { }
+  public lancaErroDeExecDeFuncaoComParametro(erro: Error): void { 
+    throw console.error('W0103 - Error to execute function with parameter.'), erro; 
+  }
 
-  protected lancaErroLydroc() { }
+  public lancaErroDeExecDeConexao(erro: Error): void { 
+    throw console.error('W0104 - Error to execute connection.'), erro; 
+  }
 
-  protected lancaErroKarzok() { }
+  public lancaErroDeExecDeConexaoComParametro(erro: Error): void {
+    throw console.error('W0105 - Error to execute connection with parameter.'), erro; 
+  }
 
-  protected lancaErroConnections() { }
+  public lancaErroDeExecDeBaseDeDados(erro: Error): void {
+    throw console.error('W0106 - Error to execute database.'), erro; 
+  }
 
-  protected lancaErroCloud() { }
+  public lancaErroDeExecDeBaseDeDadosComParametro(erro: Error): void {
+    throw console.error('W0107 - Error to execute database with parameter.'), erro; 
+  }
+
+  public lancaErroDeFormatoDeData(erro: Error): void {
+    throw console.error('W0108 - Error to format date.'), erro; 
+  }
+
+  public lancaErroDeFormatoDeDataComParametro(erro: Error): void { 
+    throw console.error('W0109 - Error to format date with parameter.'), erro;
+  }
+
+  public lancaErroDeFormatoDeHora(erro: Error): void {
+    throw console.error('W0110 - Error to format hour.'), erro; 
+  }
+
+  public lancaErroDeFormatoDeHoraComParametro(erro: Error): void {
+    throw console.error('W0111 - Error to format hour with parameter.'), erro; 
+  }
+
+  public lancaErroDeFormatoDeDataEHora(erro: Error): void {
+    throw console.error('W0112 - Error to format date and hour.'), erro; 
+  }
+
+  public lancaErroDeFormatoDeDataEHoraComParametro(erro: Error): void {
+    throw console.error('W0113 - Error to format date and hour with parameter.'), erro; 
+  }
+
+  public lancaErroDeFormatoDeNumero(erro: Error): void {
+    throw console.error('W0114 - Error to format number.'), erro; 
+  }
+
+  public lancaErroDeFormatoDeNumeroComParametro(erro: Error): void {
+    throw console.error('W0115 - Error to format number with parameter.'), erro; 
+  }
+
+  public lancaErroDeFormatoDeRegExp(erro: Error): void {
+    throw console.error('W0116 - Error to format regular expression.'), erro; 
+  }
+
+  public lancaErroDeFormatoDeRegExpComParametro(erro: Error): void {
+    throw console.error('W0117 - Error to format regular expression with parameter.'), erro; 
+  }
+
+  public lancaErroDeFormatoDeString(erro: Error): void {
+    throw console.error('W0118 - Error to format string.'), erro; 
+  }
+
+  public lancaErroDeFormatoDeStringComParametro(erro: Error): void {
+    throw console.error('W0119 - Error to format string with parameter.'), erro; 
+  }
+
+  public lancaErroDeFormatoDeBooleano(erro: Error): void {
+    throw console.error('W0120 - Error to format boolean.'), erro; 
+  }
+
+  public lancaErroDeFormatoDeBooleanoComParametro(erro: Error): void {
+    throw console.error('W0121 - Error to format boolean with parameter.'), erro; 
+  }
+
+  public lancaErroDeFormatoDeObjeto(erro: Error): void {
+    throw console.error('W0122 - Error to format object.'), erro; 
+  }
+
+  public lancaErroDeFormatoDeObjetoComParametro(erro: Error): void {
+    throw console.error('W0123 - Error to format object with parameter.'), erro; 
+  }
+
+  public lancaErroDeMemoria(erro: Error): void {
+    throw console.error('W0124 - Error to memory.'), erro; 
+  }
+
+  public lancaErroDeMemoriaComParametro(erro: Error): void {
+    throw console.error('W0125 - Error to memory with parameter.'), erro; 
+  }
+
+  public lancaErroDeEstouroDePilha(erro: Error): void {
+    throw console.error('W0126 - Error to stack overflow.'), erro; 
+  }
+
+  public lancaErroDeEstouroDePilhaComParametro(erro: Error): void {
+    throw console.error('W0127 - Error to stack overflow with parameter.'), erro; 
+  }
+
+  public lancaErroDeLigacaoHTTP(erro: Error): void {
+    throw console.error('W0128 - Error to HTTP connection.'), erro; 
+  }
+
+  public lancaErroDeLigacaoHTTPComParametro(erro: Error): void {
+    throw console.error('W0129 - Error to HTTP connection with parameter.'), erro; 
+  }
 
 } export var WExceptions = new wExceptions();
--- public/library/wlydroc.lib.ts
+++ public/library/wlydroc.lib.ts
@@ -22,22 +22,32 @@ class wLydroc {
     return elemento;
   }
 
-  // tags customizadas;
-  protected criaElementoCustomizado(tag: string, idElemento: string, herdeiro?: string) {
+  /**
+   * @augments tag define a tag svg a ser utilizada. **[ Type: ``models`` ]**.
+   * @augments idElemento define o id do elemento svg a ser criado. **[ Type: ``string`` ]**.
+   * @augments herdeiro define o elemento herdeiro do elemento a ser criado. **[ Type: ``string`` ]**.
+   * @augments extensao define a extensão do elemento a ser criado. **[ Type: ``HTMLElement`` ]**.
+   * @description cria um novo elemento svg.
+   */
+  public criaElementoCustomizado(tag: string, idElemento: string, herdeiro?: string, extensao?: HTMLElement): any {
+
+    if(herdeiro != 'abbr'||'acronym'||'address'||'anchor'||'applet'||'area'||'article'||'audio'||'b'||'base'||'basefont'||'bdi'||'bdo'||'bgsound'||'big'||'blink'||'blockquote'||'body'||'br'||'button'||'canvas'||'caption'||'center'||'cite'||'code'||'col'||'colgroup'||'content'||'data'||'datalist'||'dd'||'del'||'details'||'dfn'||'dialog'||'dir'||'div'||'dl'||'dt'||'em'||'embed'||'fieldset'||'figcaption'||'figure'||'font'||'footer'||'form'||'frameset'||'h1'||'h2'||'h3'||'h4'||'h5'||'h6'||'head'||'header'||'hgroup'||'hr'||'html'||'i'||'iframe'||'image'||'img'||'input'||'ins'||'kbd'||'keygen'||'label'||'legend'||'li'||'link'||'main'||'map'||'mark'||'marquee'||'menu'||'menuitem'||'meta'||'meter'||'nav'||'nobr'||'noembed'||'noframes'||'noscript'||'object'||'ol'||'optgroup'||'option'||'output'||'paragraph'||'param'||'picture'||'plaintext'||'portal'||'pre'||'progress'||'q'||'rb'||'rp'||'rt'||'rtc'||'ruby'||'s'||'samp'||'script'||'section'||'select'||'shadow'||'slot'||'small'||'spacer'||'span'||'strike'||'strong'||'style'||'sub'||'summary'||'sup'||'table'||'tbody'||'td'||'template'||'textarea'||'tfoot'||'th'||'thead'||'time'||'title'||'tr'||'track'||'tt'||'u'||'ul'||'var'||'video'||'wbr'||'xmp') return 0;
 
     const elemento = class CustomHTMLElement extends HTMLElement {
 
-      constructor() {
-        super();
-        this.id = idElemento;
-        if (herdeiro) {
-          this.setAttribute('herdeiro', herdeiro);
-        }
-        
+      constructor() { super(); this.id = idElemento; }
+
+      protected geraElemento() { 
+        const custom = window.customElements.define(tag, CustomHTMLElement, { extends: herdeiro } );
+        this.attachShadow({ mode: 'open' });
+        return custom;
       }
 
-    };
+    }
 
+    new elemento().appendChild(extensao);
+
+    return elemento;
   }
 
   /**
@@ -51,15 +61,41 @@ class wLydroc {
     elemento1.appendChild(elemento2);
   }
 
-  // attrs tags html => abbr, accept, accept-charset, accesskey, action, align, allow, allowfullscreen, allowpaymentrequest, allowusermedia, alt, as, async, autocomplete, autoplay, autosave, bgcolor, border, buffered, challenge, charset, checked, cite, class, code, codebase, color, cols, colspan, content, contenteditable, contextmenu, controls, coords, crossorigin, data, datetime, default, defer, dir, dirname, disabled, download, draggable, dropzone, enctype, for, form, formaction, formenctype, formmethod, formnovalidate, formtarget, frameborder, headers, height, hidden, high, href, hreflang, hspace, http-equiv, icon, id, integrity, ismap, itemid, itemprop, itemref, itemscope, itemtype, kind, label, lang, list, loop, low, manifest, marginheight, marginwidth, max, maxlength, media, mediagroup, method, min, minlength, multiple, muted, name, nonce, novalidate, open, optimum, pattern, placeholder, playsinline, poster, preload, profile, radiogroup, readonly, referrerpolicy, rel, required, reversed, rows, rowspan, sandbox, scope, scoped, scrolling, seamless, selected, shape, size, sizes, span, spellcheck, src, srcdoc, srclang, srcset, start, step, style, tabindex, target, title, translate, type, usemap, value, width, wrap
-  protected adicionaAtributoAoElemento(idElemento: string, atributo: string, valor: string): void { }
+  /**
+   * @augments atributo attr do elemento a ser adicionado. **[ Type: ``string`` ]**.
+   * @augments valor valor do atributo a ser adicionado. **[ Type: ``string`` ]**.
+   * @augments idElemento id do elemento a ser adicionado. **[ Type: ``string`` ]**.
+   * @description gera um vínculo entre elementos distintos.
+   */
+  public adicionaAtributoAoElemento(atributo: 'abbr'|'accept'|'accept-charset'|'accesskey'|'action'|'align'|'allow'|'allowfullscreen'|'allowpaymentrequest'|'allowusermedia'|'alt'|'as'|'async'|'autocomplete'|'autoplay'|'autosave'|'bgcolor'|'border'|'buffered'|'challenge'|'charset'|'checked'|'cite'|'class'|'code'|'codebase'|'color'|'download'|'draggable'|'dropzone'|'enctype'|'for'|'form'|'formaction'|'formenctype'|'formmethod'|'formnovalidate'|'formtarget'|'frameborder'|'headers'|'label'|'lang'|'list'|'loop'|'low'|'manifest'|'marginheight'|'marginwidth'|'max'|'maxlength'|'media'|'mediagroup'|'method'|'min'|'minlength'|'multiple'|'muted'|'name'|'nonce'|'novalidate'|'open'|'optimum'|'pattern'|'placeholder'|'playsinline'|'poster'|'preload'|'profile'|'radiogroup'|'readonly'|'referrerpolicy'|'rel'|'required'|'reversed'|'rows'|'rowspan'|'sandbox'|'scope'|'scoped'|'scrolling'|'seamless'|'selected'|'shape'|'size'|'sizes'|'span'|'spellcheck'|'src'|'srcdoc'|'srclang'|'srcset'|'start'|'step'|'style'|'tabindex'|'target'|'title'|'translate'|'type'|'usemap'|'value'|'width'|'wrap', idElemento: string, valor: string): void {
+    const elemento = document.getElementById(idElemento);
+    elemento.setAttribute(atributo, valor);
+  }
 
-  // attrs tags svg => accent-height, accumulate-ascent, accumulate-descent, additive-operator, alignment-baseline, alphabetic, amplitude, arabic-form, ascent, attribute-name, attribute-namespace, attribute-type, azimuth, baseProfile, baseline-shift, bbox, begin, bias, by, calcMode, cap-height, class, clip, clip-path, clip-rule, clipPathUnits, color, color-interpolation, color-interpolation-filters, color-profile, color-rendering, content, contentScriptType, contentStyleType, contentType, cursor, cx, cy, d, decelerate, descent, diffuse-lighting, direction, display, divisor, dominant-baseline, dur, dx, dy, edge-mode, elevation, end, exponent, externalResourcesRequired, fill, fill-opacity, fill-rule, filter, filterRes, filterUnits, flood-color, flood-opacity, focusable, font-family, font-size, font-size-adjust, font-stretch, font-style, font-variant, font-weight, format, from, fx, fy, g1, g2, glyph-name, glyph-orientation-horizontal, glyph-orientation-vertical, glyphRef, gradientTransform, gradientUnits, hanging, height, href, horiz-adv-x, horiz-origin-x, horiz-origin-y, id, ideographic, image-rendering, in, in2, intercept, k, k1, k2, k3, k4, kernelMatrix, kernelUnitLength, kerning, keyPoints, keySplines, keyTimes, lengthAdjust, letter-spacing, lighting-color, limitingConeAngle, local, marker-end, marker-mid, marker-start, marker-units, markerHeight, markerUnits, markerWidth, mask, maskContentUnits, maskUnits, mathematical, mode, name, numOctaves, offset, opacity, operator, order, orient, orientation, origin, overflow, overline-position, overline-thickness, paint-order, path, pathLength, patternContentUnits, patternTransform, patternUnits, pointer-events, points, pointsAtX, pointsAtY, pointsAtZ, preserveAlpha, preserveAspectRatio, primitiveUnits, r, radius, refX, refY, rendering-intent, repeatCount, repeatDur, requiredExtensions, requiredFeatures, restart, results, rotate, rx, ry, scale, seed, shape-renderizing, slope, spacing, specularConstant, specularExponent, specularConstantAttr, specularExponentAttr, spreadMethod, startOffset, stdDeviation, stemh, stemv, stitchTiles, stop-color, stop-opacity, strikethrough-position, strikethrough-thickness, string, stroke, stroke-dasharray, stroke-dashoffset, stroke-linecap, stroke-linejoin, stroke-miterlimit, stroke-opacity, stroke-width, surfaceScale, systemLanguage, tableValues, target, targetX, targetY, text-anchor, text-decoration, text-rendering, textLength, to, transform, type, u1, u2, underline-position, underline-thickness, unicode, unicode-bidi, unicode-range, units-per-em, values, v-alphabetic, v-hanging, v-ideographic, v-mathematical, vector-effect, version, vert-adv-y, vert-origin-x, vert-origin-y, viewBox, viewTarget, visibility, width, widths, word-spacing, writing-mode, x, x-height, x1, x2, xChannelSelector, y, y1, y2, yChannelSelector, z, zoomAndPan
-  protected adicionaAtributoAoElementoSVG(idElemento: string, atributo: string, valor: string): void { }
+  /**
+   * @augments atributo attr do elemento a ser adicionado. **[ Type: ``string`` ]**.
+   * @augments valor valor do atributo a ser adicionado. **[ Type: ``string`` ]**.
+   * @augments idElemento id do elemento a ser adicionado. **[ Type: ``string`` ]**.
+   * @description gera um vínculo entre elementos distintos.
+   */
+  public adicionaAtributoAoElementoSVG(atributo: 'accent-height'|'accumulate-ascent'|'accumulate-descent'|'additive-operator'|'alignment-baseline'|'alphabetic'|'amplitude'|'arabic-form'|'ascent'|'attribute-name'|'attribute-namespace'|'attribute-type'|'azimuth'|'baseProfile'|'baseline-shift'|'bbox'|'begin'|'bias'|'by'|'calcMode'|'cap-height'|'class'|'clip'|'clip-path'|'clip-rule'|'clipPathUnits'|'color'|'color-interpolation'|'color-interpolation-filters'|'color-profile'|'color-rendering'|'content'|'contentScriptType'|'contentStyleType'|'contentType'|'cursor'|'cx'|'cy'|'d'|'decelerate'|'descent'|'diffuse-lighting'|'direction'|'display'|'divisor'|'dominant-baseline'|'dur'|'dx'|'dy'|'edge-mode'|'elevation'|'end'|'exponent'|'externalResourcesRequired'|'fill'|'fill-opacity'|'fill-rule'|'filter'|'filterRes'|'filterUnits'|'flood-color'|'flood-opacity'|'focusable'|'font-family'|'font-size'|'font-size-adjust'|'font-stretch'|'font-style'|'font-variant'|'font-weight'|'format'|'from'|'fx'|'fy'|'g1'|'g2'|'glyph-name'|'glyph-orientation-horizontal'|'glyph-orientation-vertical'|'glyphRef'|'gradientTransform'|'gradientUnits'|'hanging'|'height'|'href'|'horiz-adv-x'|'horiz-origin-x'|'horiz-origin-y'|'id'|'ideographic'|'image-rendering'|'in'|'in2'|'intercept'|'k'|'k1'|'k2'|'k3'|'k4'|'kernelMatrix'|'kernelUnitLength'|'kerning'|'keyPoints'|'keySplines'|'keyTimes'|'lengthAdjust'|'letter-spacing'|'lighting-color'|'limitingConeAngle'|'local'|'marker-end'|'marker-mid'|'marker-start'|'marker-units'|'markerHeight'|'markerUnits'|'markerWidth'|'mask'|'maskContentUnits'|'maskUnits'|'mathematical'|'mode'|'name'|'numOctaves'|'offset'|'opacity'|'operator'|'order'|'orient'|'orientation'|'origin'|'overflow'|'overline-position'|'overline-thickness'|'paint-order'|'path'|'pathLength'|'patternContentUnits'|'patternTransform'|'patternUnits'|'pointer-events'|'points'|'pointsAtX'|'pointsAtY'|'pointsAtZ'|'preserveAlpha'|'preserveAspectRatio'|'primitiveUnits'|'r'|'radius'|'refX'|'refY'|'rendering-intent'|'repeatCount'|'repeatDur'|'requiredExtensions'|'requiredFeatures'|'restart'|'results'|'rotate'|'rx'|'ry'|'scale'|'seed'|'shape-renderizing'|'slope'|'spacing'|'specularConstant'|'specularExponent'|'specularConstantAttr'|'specularExponentAttr'|'spreadMethod'|'startOffset'|'stdDeviation'|'stemh'|'stemv'|'stitchTiles'|'stop-color'|'stop-opacity'|'strikethrough-position'|'strikethrough-thickness'|'string'|'stroke'|'stroke-dasharray'|'stroke-dashoffset'|'stroke-linecap'|'stroke-linejoin'|'stroke-miterlimit'|'stroke-opacity'|'stroke-width'|'surfaceScale'|'systemLanguage'|'tableValues'|'target'|'targetX'|'targetY'|'text-anchor'|'text-decoration'|'text-rendering'|'textLength'|'to'|'transform'|'type'|'u1'|'u2'|'underline-position'|'underline-thickness'|'unicode'|'unicode-bidi'|'unicode-range'|'units-per-em'|'values'|'v-alphabetic'|'v-hanging'|'v-ideographic'|'v-mathematical'|'vector-effect'|'version'|'vert-adv-y'|'vert-origin-x'|'vert-origin-y'|'viewBox'|'viewTarget'|'visibility'|'width'|'widths'|'word-spacing'|'writing-mode'|'x'|'x-height'|'x1'|'x2'|'xChannelSelector'|'y'|'y1'|'y2'|'yChannelSelector'|'z'|'zoomAndPan', idElemento: string, valor: string): void {
+    const elemento = document.getElementById(idElemento);
+    elemento.setAttribute(atributo, valor);
+  }
 
-  // attrs tags customizadas;
+  /**
+   * @augments atributo atributo do elemento a ser adicionado. **[ Type: ``string`` ]**.
+   * @augments idElemento id do elemento a ser adicionado. **[ Type: ``string`` ]**.
+   * @description gera um attributo customizado. 
+   */
+  public coletaAtributoDoElemento(atributo: string, idElemento: string): string {
+    const elemento = document.getElementById(idElemento);
+    return elemento.getAttribute(atributo);
+  }
 
   // folhas de estilo predefinidas (CSS)
 
+  // folhas de estilo customizadas (CSS)
+  protected defineFolhaDeEstilo(idFolhaDeEstilo: string, caminhoFolhaDeEstilo: string): void { }
 
 } export var WLydroc = new wLydroc();

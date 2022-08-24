14741d18fbf51366335522919b0ed6a1a0ecd46f
Humba01 <humbandroid@gmail.com>
2022-08-24 12:57:54 +0000

    adicionandoo uma nova infra.

--- application/dashboard.setup.ts
+++ application/dashboard.setup.ts
@@ -1,3 +1,6 @@
-const cmd = require('commander');
-const shell = require('shelljs');
+/** @copyright Copyright 2021-2022 */
+const cmd = require('commander'), shell = require('shelljs');
 
+// Interfaces para a criação de libs
+interface constant_register { name: string, value: string, description: string }
+interface function_container { conteiner_id: string|number, contentBlock: [] }

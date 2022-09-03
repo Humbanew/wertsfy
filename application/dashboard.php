10dd6bbfcaa30115473f69d6f87718bc5a7f56c3
Humba01 <humbandroid@gmail.com>
2022-09-03 20:36:36 -0300

    Novos recursos.

--- application/dashboard.php
+++ application/dashboard.php
@@ -11,10 +11,33 @@
   <body>
     <header>
 
-      <style> 
-        @font-face { font-family: Code; src: url(); }
-        .font_consola_sans { font-family: 'Consolas'; }
-        #formatter { display: inline-block; color: #ffffff; background-color: #000000; } 
+      <style>
+        html, body, head, footer, div
+        {
+          margin: 0;
+        }
+
+        @font-face 
+        { 
+          font-family: Code; src: url(); 
+        }
+        
+        .font_consola_sans 
+        { 
+          font-family: 'Consolas'; 
+        }
+        
+        #formatter 
+        { 
+          color: #ffffff; 
+          background-color: #000000; 
+          border-radius: 2.8px; 
+          display: inline-block;
+          margin-left: 0.5vw;
+          margin-top: 0.5vw;
+          padding: 0.9rem 0.8rem;
+          box-shadow: 1px 1px 6px #777777;
+        } 
       </style>
       
       <nav>
--- application/gulpfile.js
+++ application/gulpfile.js
@@ -1,1 +1,24 @@
-const gulp = require('gulp'), devServer = require('gulp-devserver'), phpConnect = require('gulp-connect-php');
+const gulp = require('gulp')
+  ,devServer = require('gulp-devserver')
+  ,phpConnect = require('gulp-connect-php')
+  ,browserSync = require('browser-sync');
+
+let connection = new phpConnect();
+
+gulp.task('phpserver', () => {
+ 
+  connection.server({
+    "port": 5500,
+    "base": "./",
+    "open": false
+  }, () => { browserSync({
+
+    proxy: "127.0.0.1:5500",
+    watch: true,
+    watchEvents: true,
+    files: ["./**"]
+
+    }) 
+  });
+
+});

45f956f04a161606bb2179a124048ebf2b10b7e5
Humba01 <humbandroid@gmail.com>
2022-09-10 00:42:30 -0300

    Criando nova arquitetura.

--- application/wboard.ts
+++ application/wboard.ts
@@ -42,7 +42,7 @@ class WBoard {
         return 1.6021766208e-19;
       }
 
-      get sqrt() 
+      get sqrtd() 
       {
         
         return (
@@ -75,7 +75,7 @@ class WBoard {
     
       }
 
-      get cbrt()
+      get cbrtd()
       {
         
         return (
@@ -138,22 +138,73 @@ class WBoard {
         return -999999999999999;
       }
 
-      abs(x: number): number 
+      abs(x: number, neg?: boolean): number 
       {
+        if(neg == true) return -1 * (x < 0 ? -1 * x : x);
         return x < 0 ? -1 * x : x;
       }
 
-      absNeg(x: number): number 
+      sqrt(x: number, neg?: boolean): number 
       {
-        return -1 * (x < 0 ? -1 * x : x);
+        if(neg == true) return -1 * (x ** (1 / 2));
+        return x ** (1 / 2);
       }
 
-    }
+      cbrt(x: number, neg?: boolean): number 
+      {
+        if(neg == true) return -1 * (x ** (1 / 3));
+        return x ** (1 / 3);
+      }
 
+      quart(x: number, neg?: boolean): number 
+      {
+        if(neg == true) return -1 * (x ** (1 / 4));
+        return x ** (1 / 4);
+      }
+
+      quint(x: number, neg?: boolean): number 
+      {
+        if(neg == true) return -1 * (x ** (1 / 5));
+        return x ** (1 / 5);
+      }
+
+      sext(x: number, neg?: boolean): number 
+      {
+        if(neg == true) return -1 * (x ** (1 / 6));
+        return x ** (1 / 6);
+      }
+
+      sept(x: number, neg?: boolean): number 
+      {
+        if(neg == true) return -1 * (x ** (1 / 7));
+        return x ** (1 / 7);
+      }
+
+      oct(x: number, neg?: boolean): number 
+      {
+        if(neg == true) return -1 * (x ** (1 / 8));
+        return x ** (1 / 8);
+      }
+
+      non(x: number, neg?: boolean): number 
+      {
+        if(neg == true) return -1 * (x ** (1 / 9));
+        return x ** (1 / 9);
+      }
+
+      dec(x: number, neg?: boolean): number 
+      {
+        if(neg == true) return -1 * (x ** (1 / 10));
+        return x ** (1 / 10);
+      }
+
+
+    }
+    
 
   }
 
-  protected base_math = new this.wtsy_lib.prototype.Math();
+  protected rdcmath = new this.wtsy_lib.prototype.Math();
 
   // Abstração de toda a Aplicação para o mundo externo
   public wertsfy= 
@@ -162,37 +213,53 @@ class WBoard {
     math:
     {
 
-      Pi: this.base_math.pi,
+      Pi: this.rdcmath.pi,
 
-      Euler: this.base_math.euler,
+      Euler: this.rdcmath.euler,
       
-      Plank: this.base_math.plank,
+      Plank: this.rdcmath.plank,
 
-      Avogadro: this.base_math.avogadro,
+      Avogadro: this.rdcmath.avogadro,
 
-      Newton: this.base_math.newton,
+      Newton: this.rdcmath.newton,
 
-      Testa: this.base_math.tesla,
+      Testa: this.rdcmath.tesla,
 
-      Sqrt: this.base_math.sqrt,
+      Sqrt: this.rdcmath.sqrtd,
 
-      Cbrt: this.base_math.cbrt,
+      Cbrt: this.rdcmath.cbrtd,
 
-      Ln2: this.base_math.ln2,
+      Ln2: this.rdcmath.ln2,
 
-      Ln10: this.base_math.ln10,
+      Ln10: this.rdcmath.ln10,
 
-      Log2e: this.base_math.log2e,
+      Log2e: this.rdcmath.log2e,
 
-      Log10e: this.base_math.log10e,
+      Log10e: this.rdcmath.log10e,
 
-      Maxvalue: this.base_math.maxvalue,
+      Maxvalue: this.rdcmath.maxvalue,
 
-      Minvalue: this.base_math.minvalue,
+      Minvalue: this.rdcmath.minvalue,
 
-      abs: (x: number) => this.base_math.abs(x),
-      
-      absNeg: (x: number) => this.base_math.absNeg(x)
+      abs: (x: number, neg?: boolean) => this.rdcmath.abs(x, neg),
+
+      sqrt: (x: number, neg?: boolean) => this.rdcmath.sqrt(x, neg),
+
+      cbrt: (x: number, neg?: boolean) => this.rdcmath.cbrt(x, neg),
+
+      quart: (x: number, neg?: boolean) => this.rdcmath.quart(x, neg),
+
+      quint: (x: number, neg?: boolean) => this.rdcmath.quint(x, neg),
+
+      sext: (x: number, neg?: boolean) => this.rdcmath.sext(x, neg),
+
+      sept: (x: number, neg?: boolean) => this.rdcmath.sept(x, neg),
+
+      oct: (x: number, neg?: boolean) => this.rdcmath.oct(x, neg),
+
+      non: (x: number, neg?: boolean) => this.rdcmath.non(x, neg),
+
+      dec: (x: number, neg?: boolean) => this.rdcmath.dec(x, neg)
 
     }
 

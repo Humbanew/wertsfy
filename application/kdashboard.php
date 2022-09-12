c3f6c63440b1d66b781d4ae6051d87f557143cba
Humba01 <humbandroid@gmail.com>
2022-09-11 23:08:07 -0300

    Criando nova arquitetura.

--- application/kdashboard.php
+++ application/kdashboard.php
@@ -1,4 +1,4 @@
-<?php header("Access-Control-Allow-Origin: https://github.com/", true) ?>
+<?php $defines = []; ?>
 
 <DOCTYPE html>
 <html>
@@ -9,6 +9,24 @@
   </head>
 
   <style>
+    .dark-theme { }
+
+    .light-theme { }
+
+    .kambler-theme { }
+
+    .winter-theme { }
+
+    @font-face {
+      font-family: Exo2;
+      src: url("./www/fnts/Exo2-Regular.ttf");
+    }
+
+    @font-face {
+      font-family: Electrolize;
+      src: url("./www/fnts/Electrolize-Regular.ttf");
+    }
+
     div, section, p, h1, h2, h3, h4, h5, h6, head, body, html
     {
       margin: 0;
@@ -52,7 +70,7 @@
     
     .container
     {
-      font-family: Consolas;
+      font-family: Exo2;
       margin-left: 1vw;
       margin-top: 1vw;
     }
@@ -63,6 +81,28 @@
       display: inline-block;
       padding: 0.4vw 0.6vw;
       box-shadow: 0px 0px 10px #666666;
+    }
+
+    #configurations-module, #wertsfy-cli-shell-module
+    {
+      background-color: #222222;
+      border: 1px solid #111111;
+      border-radius: 4px; 
+      color: #006666;
+      display: inline-block;
+      font-family: Exo2;
+      font-size: 2.2em;
+      font-weight: 800;
+      rotate: -90deg;
+      margin-top: 17vw;
+      margin-left: -14vw;
+      padding: 0.6vw 0.7vw;
+      width: 70vh;
+    }
+
+    #wertsfy-cli-shell-module
+    {
+      margin-left: -29.8vw;
     }
   </style>
 
@@ -77,9 +117,13 @@
       <h1 class="title">Wertsfy Dashboard</h1>
     </div>
 
-    <section id="configurations-module"></section>
+    <section id="configurations-module">
+      <p>Configurations</p>
+    </section>
 
-    <section id="wertsfy-cli-shell-module"></section>
+    <section id="wertsfy-cli-shell-module">
+      <p>Wertsfy CLI Shell</p>
+    </section>
   
     <section id="nikko-module"></section>
 

30eea35bb439b5c92f55b722917f000fd28e3fcd
Humberto Bravo <humbandroid@gmail.com>
2022-09-27 13:39:49 +0000

    Mexendo no código legado.

--- application/_wertsfy-v2.0.0/@legado/minecraftformats.ts
+++ application/_wertsfy-v2.0.0/@legado/minecraftformats.ts
@@ -1,4 +1,4 @@
-let structure = { 
+const structure = { 
 
   "minecraft": {
 
@@ -14,12 +14,66 @@ let structure = {
             "uvlock": {
 
               Nulled: "~none",
-              Disabled: "false",
-              Enabled: "true"
+              Disabled: "false"||false,
+              Enabled: "true"||true
 
             },
             "x": 0|90|180|270,
-            "y": 0|90|180|270
+            "y": 0|90|180|270,
+            "conditions": {
+
+              "apply": {
+
+                "model": String
+
+              },
+
+              "when": {
+
+                "east": {
+
+                  Nulled: "~none",
+                  Disabled: "false"||false,
+                  Enabled: "true"||true
+
+                },
+
+                "west": {
+
+                  Nulled: "~none",
+                  Disabled: "false"||false,
+                  Enabled: "true"||true
+
+                },
+
+                "north": {
+
+                  Nulled: "~none",
+                  Disabled: "false"||false,
+                  Enabled: "true"||true
+
+                },
+
+                "south": {
+
+                  Nulled: "~none",
+                  Disabled: "false"||false,
+                  Enabled: "true"||true
+
+                }
+
+
+              }
+            
+            }
+
+          },
+
+          "axis": {
+
+            CoordX: "x",
+            CoordY: "y",
+            CoordZ: "z"
 
           },
 
@@ -53,8 +107,8 @@ let structure = {
           "in_wall": {
 
             Nulled: "~none",
-            Disabled: "false",
-            Enabled: "true"
+            Disabled: "false"||false,
+            Enabled: "true"||true
 
           },
 
@@ -69,18 +123,67 @@ let structure = {
           "open": {
 
             Nulled: "~none",
-            Disabled: "false",
-            Enabled: "true"
+            Disabled: "false"||false,
+            Enabled: "true"||true
 
           },
 
           "powered": {
 
             Nulled: "~none",
-            Disabled: "false",
-            Enabled: "true"
+            Disabled: "false"||false,
+            Enabled: "true"||true
             
           },
+
+          "shape": {
+
+            Nulled: "~none",
+            AscendingEast: "ascending_east",
+            AscendingWest: "ascending_west",
+            AscendingNorth: "ascending_north",
+            AscendingSouth: "ascending_south",
+            EastWest: "east_west",
+            InnerLeft: "inner_left",
+            InnerRight: "inner_right",
+            NorthSouth: "north_south",
+            OuterLeft: "outer_left",
+            OuterRight: "outer_right",
+            Straight: "straight"
+
+          },
+
+          "type": {
+
+            Nulled: "~none",
+            Bottom: "bottom",
+            Double: "double",
+            Type: "type"
+
+          }
+
+
+        },
+
+        "multipart": {
+
+          "@configuration": {
+
+            "when": {
+              
+              "up": {
+              
+                Nulled: "~none",
+                Disabled: "false"||false,
+                Enabled: "true"||true
+              
+              }
+              
+
+            }
+
+
+          }
 
 
         }

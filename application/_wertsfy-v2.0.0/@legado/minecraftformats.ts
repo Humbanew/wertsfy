7e68bd9364e4087f6bbd3daf0ff094a7e0fbf849
Humberto Bravo <humbandroid@gmail.com>
2022-09-27 12:30:36 +0000

    Mexendo no código legado.

---
+++ application/_wertsfy-v2.0.0/@legado/minecraftformats.ts
@@ -0,0 +1,52 @@
+let structure = { 
+
+  "minecraft": {
+
+    "assets": {
+
+      "blockstates": {
+
+        "variants": {
+
+          "face": [
+
+            "~none",
+            "ceiling",
+            "floor",
+            "wall"
+
+          ],
+
+          "facing": [
+
+            "~none",
+            "east",
+            "west",
+            "north",
+            "south"
+
+          ],
+
+          "powered": [
+
+            "~none",
+            "false",
+            "true"
+            
+          ],
+
+          "position": ""
+
+        }
+
+
+      }
+
+
+    },
+
+    "data": { }
+
+  } 
+
+}

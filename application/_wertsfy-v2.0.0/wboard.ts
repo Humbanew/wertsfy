3dac6136ac75c4d95b4591f885963901ff34d5f2
Humba01 <humbandroid@gmail.com>
2022-10-03 22:02:18 -0300

    Arrumando tipagens

--- application/_wertsfy-v2.0.0/wboard.ts
+++ application/_wertsfy-v2.0.0/wboard.ts
@@ -1,6 +1,6 @@
 'use strict'
 
-import { WCollections } from "./wcollections";
+import { WCollections, WTrmSClr, WTrmSClrF } from "./wcollections";
 
 // Quadro base de arquitetura
 class WBoard {
@@ -561,34 +561,47 @@ class WBoard {
         return '\0';
       }
 
-      public adicionaFormatacaoTexto(modelo: WCollections, texto: string): string {
-        let formato = /[0-9]m/gi.exec(modelo.Terminal.Tipo.Formatacao.Template)?.toString();
+      public adicionaFormTexto(modelo: WCollections, texto: string): string {
+        const objeto = modelo.Terminal.Tipo.Formatacao.Template, regexp = /[0-9]m/gi;
+        let formato = regexp.exec(objeto)?.toString();
         return `\033[${formato}${texto}\033[0m`;
       };
 
-      public adicionaColorizacaoTexto(modelo: WCollections, tC: "3b4bit"|"8bit"|"24bit", eC: "C"|"F", texto: string): string {
-        let formato = null;
+      public adicionaClrTexto(modelo: WCollections, tC: WTrmSClr, eC: WTrmSClrF, texto: string): string {
+        let formato = null, objeto: any, regexp: RegExp;
         switch(tC) {
           case "3b4bit":
             switch(eC) {
               case "C":
-                formato = /[0-9]m/gi.exec(modelo.Terminal.Tipo.Coloracao.Template.Formato.f_3b4bit.CorLetra)?.toString();
+                objeto = modelo.Terminal.Tipo.Coloracao.Template.Formato.f_3b4bit.CorLetra 
+                 ,regexp = /[0-9]m/gi;
+                formato = regexp.exec(objeto)?.toString();
               case "F":
-                formato = /[0-9]m/gi.exec(modelo.Terminal.Tipo.Coloracao.Template.Formato.f_3b4bit.CorFundo)?.toString();
+                objeto = modelo.Terminal.Tipo.Coloracao.Template.Formato.f_3b4bit.CorFundo 
+                 ,regexp = /[0-9]m/gi;
+                formato = regexp.exec(objeto)?.toString();
             }
           case "8bit":
             switch(eC) {
               case "C":
-                formato = /[0-9];5;[0-9]m/gi.exec(modelo.Terminal.Tipo.Coloracao.Template.Formato.f_8bit.CorLetra)?.toString();
+                objeto = modelo.Terminal.Tipo.Coloracao.Template.Formato.f_8bit.CorLetra 
+                 ,regexp = /[0-9];5;[0-9]m/gi;
+                formato = regexp.exec(objeto)?.toString();
               case "F":
-                formato = /[0-9];5;[0-9]m/gi.exec(modelo.Terminal.Tipo.Coloracao.Template.Formato.f_8bit.CorFundo)?.toString();
+                objeto = modelo.Terminal.Tipo.Coloracao.Template.Formato.f_8bit.CorFundo
+                 ,regexp = /[0-9];5;[0-9]m/gi;
+                formato = regexp.exec(objeto)?.toString();
             }
           case "24bit":
             switch(eC) {
               case "C":
-                formato = /[0-9];2;[0-9];[0-9];[0-9]m/gi.exec(modelo.Terminal.Tipo.Coloracao.Template.Formato.f_24bit.CorLetra)?.toString();
+                objeto = modelo.Terminal.Tipo.Coloracao.Template.Formato.f_24bit.CorLetra
+                 ,regexp = /[0-9];2;[0-9];[0-9];[0-9]m/gi;
+                formato = regexp.exec(objeto)?.toString();
               case "F":
-                formato = /[0-9];2;[0-9];[0-9];[0-9]m/gi.exec(modelo.Terminal.Tipo.Coloracao.Template.Formato.f_24bit.CorFundo)?.toString();
+                objeto = modelo.Terminal.Tipo.Coloracao.Template.Formato.f_24bit.CorFundo
+                 ,regexp = /[0-9];2;[0-9];[0-9];[0-9]m/gi;
+                formato = regexp.exec(objeto)?.toString();
             }
         }
         return `\033[${formato}${texto}\033[0m`;
--- application/_wertsfy-v2.0.0/wcollections.d.ts
+++ application/_wertsfy-v2.0.0/wcollections.d.ts
@@ -1,6 +1,10 @@
 // Tipos da arquitetura
 export declare type WCollections= 
 {
-  Terminal: WCTerminal ,
-  ExpressoesRegulares: "" 
+  Terminal: WCTerminal,
+  ExpressoesRegulares: WCRegExp 
 }
+
+export declare type WTrmSClr = "3b4bit"|"8bit"|"24bit";
+
+export declare type WTrmSClrF = "C"|"F";

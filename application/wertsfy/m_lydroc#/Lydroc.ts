4eaf73376df82a561eff3e617f27a5eebb41d1cc
Humberto Bravo <humbandroid@gmail.com>
2022-10-11 22:27:55 -0300

    Arrumando tipagens de biblioteca.

--- application/wertsfy/m_lydroc#/Lydroc.ts
+++ application/wertsfy/m_lydroc#/Lydroc.ts
@@ -1,5 +1,55 @@
-class Lydroc { }
-
 /** 
- * html elements 
+ * html elements
+ * 
+ * all elements </>
+ * \_ attributes:
+ *    \\ class
+ *    \\ id
+ * 
+ * <html> 
+ * \_ attributes: 
+ *    \\ manifest, 
+ *    \\ version, 
+ *    \\ xmlns.
+ * 
+ * <base> 
+ * \_ attributes: 
+ *    \\ href, 
+ *    \\ target [_self, _blank, _parent, _top].
+ * 
+ * <head> 
+ * \_ attributes: 
+ *    \\ profile.
+ * 
+ * <link> 
+ * \_ attributes: 
+ *    \\ as [audio, document, embed, fetch, font, image, object, script, style, track, video, worker], 
+ *    \\ crossorigin [anonymous, use-credentials], 
+ *    \\ disabled, 
+ *    \\ fetchpriority [high, low, auto], 
+ *    \\ href, 
+ *    \\ hreflang, 
+ *    \\ imagesizes, 
+ *    \\ imagesrcset, 
+ *    \\ integrity, 
+ *    \\ media, 
+ *    \\ prefetch, 
+ *    \\ referrerpolicy [no-referrer, no-referrer-when-downgrade, origin, origin-when-cross-origin, unsafe-url], 
+ *    \\ rel, 
+ *    \\ sizes [any, white-space=>"<width in pixels>x<height in pixels> or <width in pixels>X<height in pixels>"], 
+ *    \\ title, 
+ *    \\ type, 
+ *    \\ methods, 
+ *    \\ target, 
+ *    \\ charset, 
+ *    \\ rev.
+ *
+ * <meta>
+ * \_ attributes: 
+ *    \\ charset, 
+ *    \\ content, 
+ *    \\ http-equiv [content-security-policy, content-type, default-style, x-ua-compatible, refresh], 
+ *    \\ name.  
  */
+
+class Lydroc { }

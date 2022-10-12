2885977fea28e3a19f8ccfc7e9080151d0179e79
Humberto Bravo <humbandroid@gmail.com>
2022-10-12 02:38:20 -0300

    Arrumando tipagens de biblioteca.

--- application/wertsfy/m_lydroc#/Lydroc.ts
+++ application/wertsfy/m_lydroc#/Lydroc.ts
@@ -1,15 +1,81 @@
 /** 
  * html elements
  * 
- * all elements </>
- * \_ attributes:
- *    \\ class
- *    \\ id
+ * \_ event handler attributes:
+ *    \\ onabort, 
+ *    \\ onautocomplete, 
+ *    \\ onautocompleteerror, 
+ *    \\ onblur, 
+ *    \\ oncancel, 
+ *    \\ oncanplay, 
+ *    \\ oncanplaythrough, 
+ *    \\ onchange,
+ *    \\ onclick,
+ *    \\ onclose,
+ *    \\ oncontextmenu,
+ *    \\ oncuechange,
+ *    \\ ondblclick,
+ *    \\ ondrag,
+ *    \\ ondragend,
+ *    \\ ondragenter,
+ *    \\ ondragleave,
+ *    \\ ondragover,
+ *    \\ ondragstart,
+ *    \\ ondrop,
+ *    \\ ondurationchange,
+ *    \\ onemptied,
+ *    \\ onended,
+ *    \\ onerror,
+ *    \\ onfocus,
+ *    \\ oninput,
+ *    \\ oninvalid,
+ *    \\ onkeydown,
+ *    \\ onkeypress,
+ *    \\ onkeyup,
+ *    \\ onload,
+ *    \\ onloadeddata,
+ *    \\ onloadedmetadata,
+ *    \\ onloadstart,
+ *    \\ onmousedown,
+ *    \\ onmouseenter,
+ *    \\ onmouseleave,
+ *    \\ onmousemove,
+ *    \\ onmouseout,
+ *    \\ onmouseover,
+ *    \\ onmouseup,
+ *    \\ onmousewheel,
+ *    \\ onpause,
+ *    \\ onplay,
+ *    \\ onplaying,
+ *    \\ onprogress,
+ *    \\ onratechange,
+ *    \\ onreset,
+ *    \\ onresize,
+ *    \\ onscroll,
+ *    \\ onseeked,
+ *    \\ onseeking,
+ *    \\ onselect,
+ *    \\ onshow,
+ *    \\ onsort,
+ *    \\ onstalled,
+ *    \\ onsubmit,
+ *    \\ onsuspend,
+ *    \\ ontimeupdate,
+ *    \\ ontoggle,
+ *    \\ onvolumechange,
+ *    \\ onwaiting.
+ * 
+ * \_ global attributes:
+ *    \\ accesskey,
+ *    \\ autocapitalize [off or none, on or sentences, words, characters],
+ * 
+ *    \\ \\ class
+ *    \\ \\ id
  * 
  * <html> 
  * \_ attributes: 
- *    \\ manifest, 
- *    \\ version, 
+ *    \\ manifest (deprecated, non-standand), 
+ *    \\ version (deprecated), 
  *    \\ xmlns.
  * 
  * <base> 
@@ -19,37 +85,80 @@
  * 
  * <head> 
  * \_ attributes: 
- *    \\ profile.
+ *    \\ profile (deprecated, non-standand).
  * 
  * <link> 
  * \_ attributes: 
  *    \\ as [audio, document, embed, fetch, font, image, object, script, style, track, video, worker], 
  *    \\ crossorigin [anonymous, use-credentials], 
- *    \\ disabled, 
- *    \\ fetchpriority [high, low, auto], 
+ *    \\ disabled (deprecated, non-standand), 
+ *    \\ fetchpriority [high, low, auto] (experimental), 
  *    \\ href, 
  *    \\ hreflang, 
  *    \\ imagesizes, 
  *    \\ imagesrcset, 
  *    \\ integrity, 
  *    \\ media, 
- *    \\ prefetch, 
+ *    \\ prefetch (experimental), 
  *    \\ referrerpolicy [no-referrer, no-referrer-when-downgrade, origin, origin-when-cross-origin, unsafe-url], 
  *    \\ rel, 
- *    \\ sizes [any, white-space=>"<width in pixels>x<height in pixels> or <width in pixels>X<height in pixels>"], 
+ *    \\ sizes [any, white-space=>"<width in pixels>x<height in pixels> or <width in pixels>X<height in pixels>"] (experimental), 
  *    \\ title, 
  *    \\ type, 
- *    \\ methods, 
- *    \\ target, 
- *    \\ charset, 
- *    \\ rev.
+ *    \\ methods (deprecated, non-standand), 
+ *    \\ target (deprecated), 
+ *    \\ charset (deprecated), 
+ *    \\ rev (deprecated).
  *
  * <meta>
  * \_ attributes: 
  *    \\ charset, 
  *    \\ content, 
  *    \\ http-equiv [content-security-policy, content-type, default-style, x-ua-compatible, refresh], 
- *    \\ name.  
+ *    \\ name.
+ * 
+ * <style>
+ * \_ attributes:
+ *    \\ media,
+ *    \\ nonce,
+ *    \\ title,
+ *    \\ type (deprecated),
+ *    
+ * 
+ * <title>
+ * \_ global attributes
+ * 
+ * <body>
+ * \_ attributes:
+ *    \\ alink (deprecated),
+ *    \\ background (deprecated),
+ *    \\ bgcolor (deprecated),
+ *    \\ bottommargin (deprecated),
+ *    \\ leftmargin (deprecated),
+ *    \\ link (deprecated),
+ *    \\ onafterprint,
+ *    \\ onbeforeprint,
+ *    \\ onbeforeunload,
+ *    \\ onblur,
+ *    \\ onerror,
+ *    \\ onfocus,
+ *    \\ onhashchange,
+ *    \\ onlanguagechange,
+ *    \\ onload,
+ *    \\ onmessage,
+ *    \\ onoffline,
+ *    \\ ononline,
+ *    \\ onpopstate,
+ *    \\ onredo,
+ *    \\ onresize,
+ *    \\ onstorage,
+ *    \\ onundo,
+ *    \\ onunload,
+ *    \\ rightmargin (deprecated),
+ *    \\ text (deprecated),
+ *    \\ topmargin (deprecated),
+ *    \\ vlink (deprecated).
+ *   
  */
 
 class Lydroc { }

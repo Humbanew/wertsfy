3bff2a9f5f04ee3fac6b80983951ef549dd54dab
Humberto Bravo <humbandroid@gmail.com>
2022-10-28 00:38:40 -0300

    Arrumando projeto

--- application/base/preview/architecture.d.ts
+++ application/base/preview/architecture.d.ts
@@ -3,16 +3,16 @@ declare interface Native {
 }
 
 // parents of Native:
-/***/  
-declare interface Struct { 
-  constructor: Object
-}
-/***/
+  /***/  
+  declare interface Struct { 
+    constructor: Object
+  }
+  /***/
 
 declare interface Ecmascript { }
 
 // parents of Ecmascript:
-/***/
+  /***/
 
 declare module "arch-native" { 
   export var Native: Native;

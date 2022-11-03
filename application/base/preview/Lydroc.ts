7dec07d4b38a88998bbba5737554cceeae709583
Humberto Bravo <humbandroid@gmail.com>
2022-11-03 01:58:33 -0300

    Arrumando problemas de funcionamento e compatibilidade

--- application/base/preview/Lydroc.ts
+++ application/base/preview/Lydroc.ts
@@ -1,11 +1,4 @@
-/// <reference path="architecture.d.ts" />
-
-import * as wbase from "archbase";
-
 class WLydroc { }
-
 function newElement(element: string, classname: string, id: string) { }
 function addAttribute(element: HTMLElement) { }
 function addEvent(element: HTMLElement) { }
-
-console.log(newElement("html", "format", "ft"));

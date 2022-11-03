964e4f3c9b4679f1328947a3c229abe335e05385
Humberto Bravo <humbandroid@gmail.com>
2022-11-03 02:37:19 -0300

    Arrumando problemas de funcionamento e compatibilidade

--- application/base/preview/Lydroc.ts
+++ application/base/preview/Lydroc.ts
@@ -1,4 +1,11 @@
 class WLydroc { }
-function newElement(element: string, classname: string, id: string) { }
+
+function newElement(element: string, classname: string, id: string) {
+  let constructor = document.createElement(element);
+  constructor.id = id;
+  constructor.className = classname;
+  return constructor;
+}
+
 function addAttribute(element: HTMLElement) { }
 function addEvent(element: HTMLElement) { }

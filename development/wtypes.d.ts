22fa695b52dbf4dc826df9cca3ae9b05fa8fc5f7
Humberto Bravo <humbandroid@gmail.com>
2022-11-28 00:59:31 -0300

    v0.0.1 - Arquitetando componentes

--- development/wtypes.d.ts
+++ development/wtypes.d.ts
@@ -7,3 +7,9 @@ declare type ColorYellowFormatter = '/[f]{2}[fdb9]{2}[0]{2}[0-9a-f]{2}/g';
 
 // "#660066", "#770077", "#880088", "#990099"
 declare type ColorPurpleFormatter = '/[6789]{2}[0]{2}[6789]{2}[0-9a-f]{2}/g';
+
+// "#ffffff", "#dddddd", "#bbbbbb", "#999999"
+declare type ColorWhiteFormatter = '/[fdb9]{6}[0-9a-f]{2}/g';
+
+// "#000000", "#111111", "#222222", "#333333"
+declare type ColorBlackFormatter = '/[0123]{6}[0-9a-f]{2}/g';

f9fb93cf7c440f5e1f676b95f87096c56b1d38f6
Humberto Bravo <humbandroid@gmail.com>
2022-11-28 21:22:30 -0300

    v0.0.1 - Arquitetando componentes

--- development/wtypes.d.ts
+++ development/wtypes.d.ts
@@ -3,16 +3,16 @@ declare module "@wtfy/typedefs" { }
 // // [Spectrals Lib] Color Types
 
 // "#ffff00", "#ffdd00", "#ffbb00", "#ff9900"
-declare type ColorYellowFormatter = '/[f]{2}[fdb9]{2}[0]{2}[0-9a-f]{2}/g';
+declare type ColorYellowFormatter = '/[f]{2}[4-9a-f]{2}[0]{2}[0-9a-f]{2}/g';
 
 // "#660066", "#770077", "#880088", "#990099"
-declare type ColorPurpleFormatter = '/[6789]{2}[0]{2}[6789]{2}[0-9a-f]{2}/g';
+declare type ColorPurpleFormatter = '/[4-9a-f]{2}[0]{2}[4-9a-f]{2}[0-9a-f]{2}/g';
 
 // "#ffffff", "#dddddd", "#bbbbbb", "#999999"
-declare type ColorWhiteFormatter = '/[fdb9]{6}[0-9a-f]{2}/g';
+declare type ColorWhiteFormatter = '/[9a-f]{6}[0-9a-f]{2}/g';
 
 // "#000000", "#111111", "#222222", "#333333"
-declare type ColorBlackFormatter = '/[0123]{6}[0-9a-f]{2}/g';
+declare type ColorBlackFormatter = '/[0-8]{6}[0-9a-f]{2}/g';
 
 
 /**

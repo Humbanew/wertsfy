a4ec7c9f7ca8dd3b1ac2a11f4161639e96b17d37
Humberto Bravo <humbandroid@gmail.com>
2022-11-29 02:26:21 -0300

    v0.0.1 - Arquitetando componentes

--- development/wtypes.d.ts
+++ development/wtypes.d.ts
@@ -555,3 +555,118 @@ declare type TerminalColorModelSchemas = "Color"|"Format";
 
   }
  */
+
+// // Funcionalidades Legado
+
+/**
+ *      // bloco de implementação da Biblioteca "Terminal"
+    wterminal: class WTerminal {
+
+      get inputSymbol() {
+        return '|>';
+      }
+    
+      get outputSymbol() {
+        return '|<';
+      }
+    
+      get divSymbol() {
+        return '=//\\\\//\\\\//\\\\='
+      }
+    
+      get loadingSymbols() {
+        
+        return (
+    
+          {
+            '025': '\\',
+            '050': '|',
+            '075': '/',
+            '100': '-'
+          }
+        
+        )
+      
+      }
+    
+      get escapeCharacterNewLine() {
+        return '\n';
+      }
+    
+      get escapeCharacterTab() {
+        return '\t';
+      }
+    
+      get escapeCharacterBackspace() {
+        return '\b';
+      }
+    
+      get escapeCharacterRestartLine() {
+        return '\r';
+      }
+    
+      get escapeCharacterVerticalTab() {
+        return '\v';
+      }
+    
+      get escapeCharacterFormFeed() {
+        return '\f';
+      }
+    
+      get escapeCharacterNull() {
+        return '\0';
+      }
+    
+      public adicionaFormTexto(modelo: WCollections, texto: string): string {
+        const objeto = modelo.Terminal.Tipo.Formatacao.Template, regexp = /[0-9]m/gi;
+        let formato = regexp.exec(objeto)?.toString();
+        return "\\033[" + `${formato}${texto}` + "\\033[0m";
+      };
+    
+      public adicionaClrTexto(modelo: WCollections, template: WTSchClr, esquema: WTSchClrFrgd, texto: string): string {
+        let formato: string, objeto: any, regexp: RegExp;
+        switch(template) {
+          case "3b4bit":
+            switch(esquema) {
+              case "C":
+                objeto = modelo.Terminal.Tipo.Coloracao.Template.Formato.f_3b4bit.CorLetra 
+                 ,regexp = /[0-9]m/gi;
+                formato = regexp.exec(objeto)?.toString();
+              case "F":
+                objeto = modelo.Terminal.Tipo.Coloracao.Template.Formato.f_3b4bit.CorFundo 
+                 ,regexp = /[0-9]m/gi;
+                formato = regexp.exec(objeto)?.toString();
+            }
+          case "8bit":
+            switch(esquema) {
+              case "C":
+                objeto = modelo.Terminal.Tipo.Coloracao.Template.Formato.f_8bit.CorLetra 
+                 ,regexp = /[0-9];5;[0-9]m/gi;
+                formato = regexp.exec(objeto)?.toString();
+              case "F":
+                objeto = modelo.Terminal.Tipo.Coloracao.Template.Formato.f_8bit.CorFundo
+                 ,regexp = /[0-9];5;[0-9]m/gi;
+                formato = regexp.exec(objeto)?.toString();
+            }
+          case "24bit":
+            switch(esquema) {
+              case "C":
+                objeto = modelo.Terminal.Tipo.Coloracao.Template.Formato.f_24bit.CorLetra
+                 ,regexp = /[0-9];2;[0-9];[0-9];[0-9]m/gi;
+                formato = regexp.exec(objeto)?.toString();
+              case "F":
+                objeto = modelo.Terminal.Tipo.Coloracao.Template.Formato.f_24bit.CorFundo
+                 ,regexp = /[0-9];2;[0-9];[0-9];[0-9]m/gi;
+                formato = regexp.exec(objeto)?.toString();
+            }
+        }
+        return "\\033[" + `${formato}${texto}` + "\\033[0m";
+      }
+    
+      public adicionaFormClrTexto(modeloFrm: WCollections, modeloClr: WCollections, templateClr: WTSchClr, esquemaClr: WTSchClrFrgd, texto: string) { 
+        let constructor = this.adicionaFormTexto(modeloFrm, this.adicionaClrTexto(modeloClr, templateClr, esquemaClr, texto));
+        return constructor;
+      }
+    
+    },
+ */

a5b1b17f2e4b8678ab12bb601dff77c839afc300
Humberto Bravo <humbandroid@gmail.com>
2022-12-16 03:19:20 -0300

    v0.0.1 => v0.1.0 - Adaptando Algoritmos e Aprimorando Conjunto

--- development/labo/tester.ts
+++ development/labo/tester.ts
@@ -1,5 +1,7 @@
 import { MathematicDefines } from "wertsfy-typedefs";
 import { wertsfy } from "../wertsfy";
+// teste 1
 wertsfy.mathematic.corebase.Pi;
 
+// teste 2
 wertsfy.mathematic.calculator.common(MathematicDefines.common="plus", 6, 6, 54);

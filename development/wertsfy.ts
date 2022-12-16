22d4874d733f4d96e75661ab29a129a314da8c10
Humberto Bravo <humbandroid@gmail.com>
2022-12-16 03:44:50 -0300

    v0.1.0 - Adaptando Algoritmos e Aprimorando Conjunto

--- development/wertsfy.ts
+++ development/wertsfy.ts
@@ -2539,6 +2539,7 @@ class WERTSFY_MATH_CALCULATOR {
 
   protected conversor() { }
 
+
 };
 
 class WERTSFY_INFORMATION_GAMES { }
@@ -2552,3 +2553,647 @@ class WERTSFY_MULTIPLATAFORM_HULIGHT { }
 class WERTSFY_WEBSERVICES { }
 class WERTSFY_SHORTCUTS_DISCORD { }
 class WERTSFY_SHORTCUTS_GRAPH { }
+
+
+// Mathematic Calculator Old SRC
+
+// // Algoritmos de Conversores
+// // // Algoritmo 1
+interface ChaveConversora { 
+  painel: [[][]] 
+};
+
+interface TamanhosSistemaInternacional { 
+  seletor: "deca"|"hecto"|"kilo"|"mega"|"giga"|"tera"|"peta"|"exa"|"zeta"|"yotta"|"deci"|"centi"|"mili"|"micro"|"nano"|"pico"|"femto"|"atto"|"zepto"|"yocto"|"graus"|"radianos" 
+};
+
+interface NotacoesGrandezas { 
+  seletor: "metros"|"litros"|"metros-quadrados"|"metros-cubicos" 
+};
+
+class Transformadores {
+
+  protected terminal: ChaveConversora;
+
+  private convBinarioDecimal(valor: string): number {
+    let valorDecimal = 0;
+    let res: number[] = [];
+    let divisor: string[]|number[] = valor.split("" , valor.length);
+    for(let y = 0; y < divisor.length; y++) {
+      res[y] = parseInt(divisor[y]);
+    }
+    for(let i = 0, u = divisor.length - 1; i < divisor.length; i++, u--) {
+      if(u < 0) { break; }
+      valorDecimal = parseInt(divisor[i]) * 2 ** u + valorDecimal;
+    }
+    return valorDecimal;
+  }
+
+  private convBinarioHexadecimal(valor: string): string {  
+    let divisor = [];
+    let verifCom5 = valor.length / 5;
+    let verifCom6 = valor.length / 6;
+    let verifCom7 = valor.length / 7;
+    if (Number.isInteger(verifCom7) == true) { valor = "0" + valor; }
+    if (Number.isInteger(verifCom6) == true) { valor = "00" + valor; }
+    if (Number.isInteger(verifCom5) == true) { valor = "000" + valor; }
+    let separador = valor.length / 4;
+    let repetidor = 1;
+    let e = 1, f = 2;
+    while (repetidor < separador + 1) {
+      let posicaoInicial = 0;
+      let incremento = 4;
+      if (repetidor == 1) {
+        divisor.push(valor.slice(posicaoInicial, incremento));
+        repetidor++;
+      }
+      if (repetidor > 1) {
+        divisor.push(valor.slice(incremento * e, incremento * f));
+        e++; f++;
+        repetidor++;
+      }
+    }
+    let numeroHexadecimal = "";
+    for (let u = 0; u < divisor.length; u++) {
+      if (divisor[u] == "0000") { numeroHexadecimal = numeroHexadecimal + "0"; }
+      if (divisor[u] == "0001") { numeroHexadecimal = numeroHexadecimal + "1"; }
+      if (divisor[u] == "0010") { numeroHexadecimal = numeroHexadecimal + "2"; }
+      if (divisor[u] == "0011") { numeroHexadecimal = numeroHexadecimal + "3"; }
+      if (divisor[u] == "0100") { numeroHexadecimal = numeroHexadecimal + "4"; }
+      if (divisor[u] == "0101") { numeroHexadecimal = numeroHexadecimal + "5"; }
+      if (divisor[u] == "0110") { numeroHexadecimal = numeroHexadecimal + "6"; }
+      if (divisor[u] == "0111") { numeroHexadecimal = numeroHexadecimal + "7"; }
+      if (divisor[u] == "1000") { numeroHexadecimal = numeroHexadecimal + "8"; }
+      if (divisor[u] == "1001") { numeroHexadecimal = numeroHexadecimal + "9"; }
+      if (divisor[u] == "1010") { numeroHexadecimal = numeroHexadecimal + "A"; }
+      if (divisor[u] == "1011") { numeroHexadecimal = numeroHexadecimal + "B"; }
+      if (divisor[u] == "1100") { numeroHexadecimal = numeroHexadecimal + "C"; }
+      if (divisor[u] == "1101") { numeroHexadecimal = numeroHexadecimal + "D"; }
+      if (divisor[u] == "1110") { numeroHexadecimal = numeroHexadecimal + "E"; }
+      if (divisor[u] == "1111") { numeroHexadecimal = numeroHexadecimal + "F"; }
+    }
+    return numeroHexadecimal;
+  }
+
+  private convBinarioOctal(valor: string): string {
+    let divisor = [];
+    let verifCom4 = valor.length / 4;
+    let verifCom5 = valor.length / 5;
+    if(Number.isInteger(verifCom5) == true) { valor = "0" + valor; }
+    if(Number.isInteger(verifCom4) == true) { valor = "00" + valor; }
+    let separador = valor.length / 3;
+    let repetidor = 1;
+    let e = 1, f = 2;
+    while(repetidor < separador+1) {
+      let posicaoInicial = 0;
+      let incremento = 3;
+      if(repetidor == 1) {
+        divisor.push(valor.slice(posicaoInicial, incremento));
+        repetidor++;
+      }
+      if(repetidor > 1) {
+        divisor.push(valor.slice(incremento*e, incremento*f));
+        e++; f++;
+        repetidor++;
+      }
+    }
+    let numeroOctal = "";
+    for(let u = 0; u < divisor.length; u++) {
+      if(divisor[u] == "000") { numeroOctal = numeroOctal + "0"; }
+      if(divisor[u] == "001") { numeroOctal = numeroOctal + "1"; }
+      if(divisor[u] == "010") { numeroOctal = numeroOctal + "2"; }
+      if(divisor[u] == "011") { numeroOctal = numeroOctal + "3"; }
+      if(divisor[u] == "100") { numeroOctal = numeroOctal + "4"; }
+      if(divisor[u] == "101") { numeroOctal = numeroOctal + "5"; }
+      if(divisor[u] == "110") { numeroOctal = numeroOctal + "6"; }
+      if(divisor[u] == "111") { numeroOctal = numeroOctal + "7"; }
+    }
+    return numeroOctal;
+  }
+
+  private convDecimalBinario(valor: number): string {
+    let numeroBinario = "";
+    if(valor == 0) { numeroBinario = "0" + numeroBinario; }
+    if(valor == 3) { numeroBinario = "1" + numeroBinario; }
+    while(valor > 2) {
+      valor = valor / 2;
+      if (Number.isInteger(valor) == true) { numeroBinario = "0" + numeroBinario; }
+      if (Number.isInteger(valor) == false) { numeroBinario = "1" + numeroBinario; }
+      if (valor == 3) { numeroBinario = "1" + numeroBinario; }
+      if (valor == 2) { numeroBinario = "10" + numeroBinario; }
+    }
+    return numeroBinario;
+  }
+
+  private convDecimalHexadecimal(valor: number): string {
+    let numeroHexadecimal = "";
+    let achaResto: any, resto: any;
+    while (valor > 1) {
+      valor = valor / 16;
+      achaResto = valor * 16;
+      resto = (valor * 16) - achaResto;
+      if (Number.isInteger(valor) == true) { numeroHexadecimal = "0" + numeroHexadecimal; }
+      if (Number.isInteger(valor) == false) {
+        resto = parseInt(resto);
+        if(resto < 9) { numeroHexadecimal = resto + numeroHexadecimal; }
+        if(resto > 9) {
+          if(resto == 10) { numeroHexadecimal = "A" + numeroHexadecimal; }
+          if(resto == 11) { numeroHexadecimal = "B" + numeroHexadecimal; }
+          if(resto == 12) { numeroHexadecimal = "C" + numeroHexadecimal; }
+          if(resto == 13) { numeroHexadecimal = "D" + numeroHexadecimal; }
+          if(resto == 14) { numeroHexadecimal = "E" + numeroHexadecimal; }
+          if(resto == 15) { numeroHexadecimal = "F" + numeroHexadecimal; }
+        }
+      }
+    }
+    return numeroHexadecimal;
+  }
+
+  private convDecimalOctal(valor: number): string {
+    let numeroOctal = "";
+    let achaResto: any, resto: any;
+    while(valor > 1) {
+      valor = valor / 8;
+      achaResto = valor * 8;
+      resto = (valor * 8) - achaResto;
+      if (Number.isInteger(valor) == true) { numeroOctal = "0" + numeroOctal; }
+      if (Number.isInteger(valor) == false) { numeroOctal = parseInt(resto) + numeroOctal; }
+    }
+    return numeroOctal;
+  }
+
+  private convOctalBinario(valor: string): string {
+    let divisor: any = valor.split("", valor.length);
+    for (let y = 0; y < divisor.length; y++) {
+      divisor[y] = parseInt(divisor[y]);
+      if(divisor[y] == 0) { divisor[y] = "000"; valor = "000" + valor; }
+      if(divisor[y] == 1) { divisor[y] = "001"; valor = "001" + valor; }
+      if(divisor[y] == 2) { divisor[y] = "010"; valor = "010" + valor; }
+      if(divisor[y] == 3) { divisor[y] = "011"; valor = "011" + valor; }
+      if(divisor[y] == 4) { divisor[y] = "100"; valor = "100" + valor; }
+      if(divisor[y] == 5) { divisor[y] = "101"; valor = "101" + valor; }
+      if(divisor[y] == 6) { divisor[y] = "110"; valor = "110" + valor; }
+      if(divisor[y] == 7) { divisor[y] = "111"; valor = "111" + valor; }
+    }
+    return valor;
+  }
+
+  private convOctalDecimal(valor: string): number {
+    let valorDecimal = 0;
+    let divisor: any = valor.split("", valor.length);
+    for (let y = 0; y < divisor.length; y++) { divisor[y] = parseInt(divisor[y]); }
+    for (let i = 0, u = divisor.length - 1; i < divisor.length; i++, u--) {
+      if (u < 0) { break; }
+      valorDecimal = parseInt(divisor[i]) * 8 ** u + valorDecimal;
+    }
+    return valorDecimal;
+  }
+
+  private convOctalHexadecimal(valor: string): string {
+    let valorHexadecimal: any = 0;
+    let divisor: any = valor.split("", valor.length);
+    for (let y = 0; y < divisor.length; y++) { divisor[y] = parseInt(divisor[y]); }
+    for (let i = 0, u = divisor.length - 1; i < divisor.length; i++, u--) {
+      if (u < 0) { break; }
+      valorHexadecimal = parseInt(divisor[i]) * 8 ** u + valorHexadecimal;
+    }
+    return valorHexadecimal;
+  }
+
+  private convHexadecimalDecimal(valor: string): number {
+    let valorDecimal = 0;
+    let divisor = valor.split("", valor.length);
+    for (let y = 0; y < divisor.length; y++) {
+      if (divisor[y] == "A") { divisor[y] = "10"; }
+      if (divisor[y] == "B") { divisor[y] = "11"; }
+      if (divisor[y] == "C") { divisor[y] = "12"; }
+      if (divisor[y] == "D") { divisor[y] = "13"; }
+      if (divisor[y] == "E") { divisor[y] = "14"; }
+      if (divisor[y] == "F") { divisor[y] = "15"; }
+    }
+    return valorDecimal;
+  }
+
+  private convHexadecimalBinario(valor: string) { };
+
+  private convHexadecimalOctal(valor: string) { };
+
+  public static controlPanel() { };
+
+} export let ModuloTransformadores = new Transformadores();
+
+// // // Algortimo 2
+
+function conversorUnidadesAnguloPlano() { }
+
+function conversorUnidadesAnguloSolido() { }
+
+function conversorUnidadesComprimento() { }
+
+function conversorUnidadesArea() { }
+
+function conversorUnidadesVolume() { }
+
+function conversorUnidades4Dimensao() { }
+
+function conversorUnidades5Dimensao() { }
+
+function conversorUnidades6Dimensao() { }
+
+function conversorUnidadesMassa() { }
+
+function conversorUnidadesForca() { }
+
+function conversorUnidadesTemperatura() { }
+
+function conversorUnidadesVelocidade() { }
+
+function conversorUnidadesTempo() { }
+
+function conversorUnidadesQuantidade() { }
+
+function conversorUnidadesPressao() { }
+
+function conversorUnidadesSom() { }
+
+function conversorUnidadesLuz() { }
+
+function conversorUnidadesEnergia() { }
+
+function conversorUnidadesComputacional() { }
+
+//& Constantes de Conversão
+
+  //Q Unidades do Sistema Internacional - Multiplos 
+  const DECA = 10 ** 1;
+
+  const HECTO = 10 ** 2;
+  
+  const KILO = 10 ** 3;
+  
+  const MEGA = 10 ** 6;
+  
+  const GIGA = 10 ** 9;
+  
+  const TERA = 10 ** 12;
+  
+  const PETA = 10 ** 15;
+  
+  const EXA = 10 ** 18;
+  
+  const ZETA = 10 ** 21;
+  
+  const YOTTA = 10 ** 24;
+  
+    //Q Unidades do Sistema Internacional - Submultiplos
+  
+  const DECI = 10 ** -1;
+  
+  const CENTI = 10 ** -2;
+  
+  const MILI = 10 ** -3;
+  
+  const MICRO = 10 ** -6;
+  
+  const NANO = 10 ** -9;
+  
+  const PICO = 10 ** -12;
+  
+  const FEMTO = 10 ** -15;
+  
+  const ATTO = 10 ** -18;
+  
+  const ZEPTO = 10 ** -21;
+  
+  const YOCTO = 10 ** -24;
+  
+    //Q Unidades Diversas Angulo
+  const GRAU = 180 / Math.PI;
+  
+    //Q Unidades Diversas Distancia & Comprimento
+  const MILHA = 1609.344;
+  
+  const MILHA_NAUTICA = 1852;
+  
+  const POLEGADA = 0.3048;
+  
+  const PE = 0.3048;
+  
+  const JARDAS = 0.9144;
+  
+  const PALMO = 0.3048 / 12;
+  
+  const BRACA = 0.3048 / 4;
+  
+  const VARA = 0.3048 / 2;
+  
+  const CORRENTE = 0.3048 / 60;
+  
+  const FURLONG = 201.168;
+  
+  const ROD = 0.3048 / 3;
+  
+  const ANOS_LUZ = 9.4607304725808e+15;
+  
+  const PARSEC = 3.08567758e+16;
+  
+  const UNIDADADE_ASTRONOMICA = 1.49597870691e+11;
+  
+  const ANGSTROM = 1e-10;
+  
+    //Q Unidades Diversas Área
+  const ACRE = 4046.86;
+  
+  const ARE = 100;
+  
+  const HECTARE = 10000;
+  
+  const ALQUEIRE = 4046.86 * 2;
+  
+  const DUNAM = 4046.86 * 3;
+  
+  const ROOD = 4046.86 * 4;
+  
+  const HOMESTEAD = 4046.86 * 5;
+  
+    //Q Unidades Diversas Volume
+  const LITRO = 0.001;
+  
+  const DECILITRO = 0.01;
+  
+  const CENTILITRO = 0.1;
+  
+  const MILILITRO = 1;
+  
+  const MICROLITRO = 10 ** 3;
+  
+  const NANOLITRO = 10 ** 6;
+  
+  const PICOLITRO = 10 ** 9;
+  
+  const FEMTOLITRO = 10 ** 12;
+  
+  const ATOLITRO = 10 ** 15;
+  
+  const ZEPTOLITRO = 10 ** 18;
+  
+  const YOTTOLITRO = 10 ** 21;
+  
+  const DECALITRO = 10 ** -1;
+  
+  const HECTOLITRO = 10 ** -2;
+  
+  const KILOLITRO = 10 ** -3;
+  
+  const MEGALITRO = 10 ** -6;
+  
+  const GIGALITRO = 10 ** -9;
+  
+  const TERALITRO = 10 ** -12;
+  
+  const PETALITRO = 10 ** -15;
+  
+  const EXALITRO = 10 ** -18;
+  
+  const ZETALITRO = 10 ** -21;
+  
+  const YOTALITRO = 10 ** -24;
+  
+  const JARDALITRO = 0.9144;
+  
+  const PINTADO = 0.473176473;
+  
+  const TONEL = 0.000001;
+  
+  const PIPA = 0.000001;
+  
+  const ALMUDE = 1;
+  
+  const ALMUDE_ARABE = 1;
+  
+  const ALMUDE_CONDADO_PORTUCALENSE = 1;
+  
+  const ALMUDE_CONDADO_DOM_AFONSO_HENRIQUES = 1;
+  
+  const ALMUDE_DOM_PEDRO_I = 1;
+  
+  const ALMUDE_DOM_PEDRO_II = 1;
+  
+  const ALMUDE_DOM_MANUEL_I = 1;
+  
+  const ALMUDE_CONSELHO_SANTO_TIRSO_RIO_AVE = 1;
+  
+  const ALMUDE_CONSELHO_SANTO_TIRSOpVALE_DO_LECA = 1;
+  
+  const ALMUDE_COIMBRA_VINHO = 1;
+  
+  const ALMUDE_COIMBRA_AZEITE = 1;
+  
+  const GALAO = 0.00378541;
+  
+  const GALAO_LIQUIDO = 0.00378541;
+  
+  const GALAO_LIQUIDO_BARRIS_ESTADUNIDENSES = 0.00378541;
+  
+  const GALAO_LIQUIDO_QUARTOS_ESTADUNIDENSES = 0.00378541;
+  
+  const GALAO_LIQUIDO_QUARTILHOS_ESTADUNIDENSES = 0.00378541;
+  
+  const GALAO_LIQUIDO_GILLS_ESTADUNIDENSES = 0.00378541;
+  
+  const GALAO_LIQUIDO_ONCAS_ESTADUNIDENSES = 0.00378541;
+  
+  const GALAO_SECO = 0.00378541;
+  
+  const GALAO_SECO_QUARTILHOS = 0.00378541;
+  
+  const GALAO_SECO_QUARTOS = 0.00378541;
+  
+  const GALAO_SECO_PECKS = 0.00378541;
+  
+  const GALAO_SECO_BUSHELS = 0.00378541;
+  
+  const GALAO_IMPERIAL = 0.00378541;
+  
+  const GALAO_IMPERIAL_BARRIS_BRITANICOS = 0.00378541;
+  
+  const GALAO_IMPERIAL_QUARTOS_BRITANICOS = 0.00378541;
+  
+  const GALAO_IMPERIAL_QUARTILHOS_BRITANICOS = 0.00378541;
+  
+  const GALAO_IMPERIAL_GILLS_BRITANICOS = 0.00378541;
+  
+  const GALAO_IMPERIAL_ONCAS_BRITANICOS = 0.00378541;
+  
+  const QUARTO_IMPERIAL_BRITANICO = 0.00378541;
+  
+  const QUARTO_IMPERIAL_BRITANICO_BARRIS_IMPERIAIS = 0.00378541;
+  
+  const QUARTO_IMPERIAL_BRITANICO_GALOES_IMPERIAIS = 0.00378541;
+  
+  const QUARTO_IMPERIAL_BRITANICO_PINTOS_IMPERIAIS = 0.00378541;
+  
+  const QUARTO_IMPERIAL_BRITANICO_GILLS_IMPERIAIS = 0.00378541;
+  
+  const QUARTO_IMPERIAL_BRITANICO_ONCAS_LIQUIDAS_IMPERIAIS = 0.00378541;
+  
+  const QUARTO_ESTADUNIDENSE = 0.00378541;
+  
+  const QUARTO_ESTADUNIDENSE_BARRIS_ESTADUNIDENSES = 0.00378541;
+  
+  const QUARTO_ESTADUNIDENSE_GALOES_ESTADUNIDENSES = 0.00378541;
+  
+  const QUARTO_ESTADUNIDENSE_PINTOS_ESTADUNIDENSES = 0.00378541;
+  
+  const QUARTO_ESTADUNIDENSE_GILLS_ESTADUNIDENSES = 0.00378541;
+  
+  const QUARTO_ESTADUNIDENSE_ONCAS_LIQUIDAS_ESTADUNIDENSES = 0.00378541;
+  
+  const PINTO_BRITANICO = 0.00378541;
+  
+  const PINTO_ESTADOS_UNIDOS = 0.00378541;
+  
+  const PINTO_AUSTRALIA = 0.00378541;
+  
+  const PINTO_CANADENSE = 0.00378541;
+  
+  const PINTO_FRANCES = 0.00378541;
+  
+  const PINTO_BELGICA = 0.00378541;
+  
+  const PINTO_BRITANICO_BARRIS_IMPERIAIS = 0.00378541;
+  
+  const PINTO_BRITANICO_GALOES_IMPERIAIS = 0.00378541;
+  
+  const PINTO_BRITANICO_GILLS_IMPERIAIS = 0.00378541;
+  
+  const PINTO_BRITANICO_ONCAS_LIQUIDAS_IMPERIAIS = 0.00378541;
+  
+  const PINTO_BRITANICO_BARRIS_ESTADUNIDENSES = 0.00378541;
+  
+  const PINTO_BRITANICO_GALOES_ESTADUNIDENSES = 0.00378541;
+  
+  const PINTO_BRITANICO_GILLS_ESTADUNIDENSES = 0.00378541;
+  
+  const PINTO_BRITANICO_ONCAS_LIQUIDAS_ESTADUNIDENSES = 0.00378541;
+  
+  const QUINTAL_PORTUGUES = 1;
+  
+  const QUINTAL_DE_PESO_GRANDE = 1;
+  
+  const QUINTAL_DE_PESO_PEQUENO = 1;
+  
+  const QUINTAL_METRICO = 1;
+  
+  const QUINTAL_METRICO_GRAMAS = 1;
+  
+  const QUINTAL_METRICO_DECAGRAMAS = 1;
+  
+  const QUINTAL_METRICO_HECTOGRAMAS = 1;
+  
+  const QUINTAL_METRICO_KILOGRAMAS = 1;
+  
+  const QUINTAL_METRICO_MINIAGRAMAS = 1;
+  
+  const QUINTAL_METRICO_TONELADAS_METRICAS = 1;
+  
+  const QUINTAL_METRICO_TONELADAS = 1;
+  
+  const QUINTAL_NORTE_AMERICANO = 1;
+  
+  const QUINTAL_NORTE_AMERICANO_GRAOS = 1;
+  
+  const QUINTAL_NORTE_AMERICANO_DACMAS_AVOIRDUPOIS = 1;
+  
+  const QUINTAL_NORTE_AMERICANO_ONCAS_AVOIRDUPOIS = 1;
+  
+  const QUINTAL_NORTE_AMERICANO_LIBRAS_AVOIRDUPOIS = 1;
+  
+  const QUINTAL_NORTE_AMERICANO_ARROBAS = 1;
+  
+  const QUINTAL_NORTE_AMERICANO_QUARTOS_CURTOS = 1;
+  
+  const QUINTAL_NORTE_AMERICANO_TONELADAS_CURTAS = 1;
+  
+  const QUINTAL_BRITANICO = 1;
+  
+  const QUINTAL_BRITANICO_GRAOS = 1;
+  
+  const QUINTAL_BRITANICO_DACMAS_AVOIRDUPOIS = 1;
+  
+  const QUINTAL_BRITANICO_ONCAS_AVOIRDUPOIS = 1;
+  
+  const QUINTAL_BRITANICO_LIBRAS_AVOIRDUPOIS = 1;
+  
+  const QUINTAL_BRITANICO_STONES = 1;
+  
+  const QUINTAL_BRITANICO_QUARTOS_CURTOS = 1;
+  
+  const QUINTAL_BRITANICO_TONELADAS_CURTAS = 1;
+  
+  const SESTEIRO = 1;
+  
+  const SESTEIRO_MOIO = 1;
+  
+  const SESTEIRO_SEC_XI = 1;
+  
+  const SESTEIRO_SEC_XIV = 1;
+  
+  const SESTEIRO_CONGIUS = 1;
+  
+  const SESTEIRO_MODIUS = 1;
+  
+  const SESTEIRO_SEXTANS = 1;
+  
+  const SESTEIRO_TRIENS = 1;
+  
+  const SESTEIRO_HEMINA = 1;
+  
+  const SESTEIRO_CHOENIX = 1;
+  
+  const ARRATEL = 1;
+  
+  const ONCA = 1;
+  
+    //Q Unidades Diversas Massa
+  const TONELADA = 1000;
+  
+    //Q Unidades Diversos Quantidades
+  const DUZIA = 12;
+  
+  const GROSA = 6;
+  
+    //Q Unidades Diversos Pressão
+  const ATM = 101325;
+  
+  const TORR = 133.322;
+  
+    //Q Unidades Diversos Som
+  const BEL = 1;
+  
+  const DECIBEL = 0.1;
+  
+    //Q Unidades Diversos Luz
+  const CANDELA = 1;
+  
+  const LUMEN_QUADRADO = 1;
+  
+  const LUMEN_SEGUNDO = 1;
+  
+  const LUX_QUADRADO = 1;
+  
+  const LUX_SEGUNDO = 1;
+  
+  const LUMEN_WATT = 1;
+  
+  const LUX_WATT = 1;
+  
+  const LUMEN_CUBICO = 1;
+  
+  const LUX_CUBICO = 1;
+  
+    //Q Unidades Diversos Energia
+  const CALORIA = 4.184;
+  
+  const KILOCALORIA = 4184;
+  

5cc7c2678cb62506ba93aadb227faca9864c2c94
Humberto Bravo <humbandroid@gmail.com>
2022-12-19 03:23:10 -0300

    v0.1.0 - Aprimorando tipagens existentes

--- development/wtypes.d.ts
+++ development/wtypes.d.ts
@@ -24,45 +24,49 @@ export declare namespace TypeDefines {
     formatation: 
     { 
       
-      Bold: "01m",
-      Faint: "02m",
-      Italic: "03m",
-      Underline: "04m",
-      SlowBlink: "05m",
-      RapidBlink: "06m",
-      Reverse: "07m",
-      Conceal: "08m",
-      CrossedOut: "09m",
-      DefaultFont: "10m",
-      AltFont01: "11m",
-      AltFont02: "12m",
-      AltFont03: "13m",
-      AltFont04: "14m",
-      AltFont05: "15m",
-      AltFont06: "16m",
-      AltFont07: "17m",
-      AltFont08: "18m",
-      AltFont09: "19m",
-      Fraktur: "20m",
-      DoublyUnderline: "21m",
-      NormalIntensity: "22m",
-      NotItalic: "23m",
-      NotUnderline: "24m",
-      BlinkOff: "25m",
-      Spacing: "26m",
-      ReverseOff: "27m",
-      ConcealOff: "28m",
-      NotCrossed: "29m",
-      PropositalSpacing: "50m",
-      Framed: "51m",
-      Encircled: "52m",
-      Overlined: "53m",
-      NotEncircled: "54m",
-      NotOverlined: "55m",
-      Underlined: "58m",
-      Superscript: "73m",
-      Subscript: "74m",
-      NonSuperscriptSubscript: "75m" 
+      bold: "01m",
+      faint: "02m",
+      italic: "03m",
+      underline: "04m",
+      slow_blink: "05m",
+      rapid_blink: "06m",
+      reverse: "07m",
+      conceal: "08m",
+      crossed_out: "09m",
+      font: {
+        
+        of00: "10m",
+        of01: "11m",
+        of02: "12m",
+        of03: "13m",
+        of04: "14m",
+        of05: "15m",
+        of06: "16m",
+        of07: "17m",
+        of08: "18m",
+        of09: "19m",
+
+      },
+      fraktur: "20m",
+      doubly_underline: "21m",
+      normal_intensity: "22m",
+      not_italic: "23m",
+      not_underline: "24m",
+      blink_off: "25m",
+      spacing: "26m",
+      reverse_off: "27m",
+      conceal_off: "28m",
+      not_crossed: "29m",
+      proposital_spacing: "50m",
+      framed: "51m",
+      encircled: "52m",
+      overlined: "53m",
+      not_encircled: "54m",
+      not_overlined: "55m",
+      underlined: "58m",
+      superscript: "73m",
+      subscript: "74m",
+      non_superscript_subscript: "75m" 
 
     }
 
@@ -90,77 +94,60 @@ export declare namespace TypeDefines {
 /**
  *
  *
-  declare type WCTerminalColoracao=
-  {
-    Template:
-    {
-      
-      Formato:
-      {
+f_3b4bit:
 
-        f_3b4bit: {
+  CorLetra:
+    "30m#corpreta"|
+    "31m#corvermelha"|
+    "32m#corverde"|
+    "33m#coramarela"|
+    "34m#corazul"|
+    "35m#corroxa"|
+    "36m#corciano"|
+    "37m#corcinza"|
+    "90m#corcinzaclaro"|
+    "91m#corvermelhoclaro"|
+    "92m#corverdeclaro"|
+    "93m#coramareloclaro"|
+    "94m#corazulclassico"|
+    "95m#corrosa"|
+    "96m#corazulclaro"|
+    "97m#corbranca",
+  
+  CorFundo:
+    "40m#fundopreto"|
+    "41m#fundovermelho"|
+    "42m#fundoverde"|
+    "43m#fundoamarelo"|
+    "44m#fundoazul"|
+    "45m#fundoroxo"|
+    "46m#fundociano"|
+    "47m#fundocinza"|
+    "100m#fundocinzaclaro"|
+    "101m#fundovermelhoclaro"|
+    "102m#fundoverdeclaro"|
+    "103m#fundoamareloclaro"|
+    "104m#fundoazulclassico"|
+    "105m#fundorosa"|
+    "106m#fundoazulclaro"|
+    "107m#fundobranco"
 
-          CorLetra:
-            "30m#corpreta"|
-            "31m#corvermelha"|
-            "32m#corverde"|
-            "33m#coramarela"|
-            "34m#corazul"|
-            "35m#corroxa"|
-            "36m#corciano"|
-            "37m#corcinza"|
-            "90m#corcinzaclaro"|
-            "91m#corvermelhoclaro"|
-            "92m#corverdeclaro"|
-            "93m#coramareloclaro"|
-            "94m#corazulclassico"|
-            "95m#corrosa"|
-            "96m#corazulclaro"|
-            "97m#corbranca",
-          
-          CorFundo:
-            "40m#fundopreto"|
-            "41m#fundovermelho"|
-            "42m#fundoverde"|
-            "43m#fundoamarelo"|
-            "44m#fundoazul"|
-            "45m#fundoroxo"|
-            "46m#fundociano"|
-            "47m#fundocinza"|
-            "100m#fundocinzaclaro"|
-            "101m#fundovermelhoclaro"|
-            "102m#fundoverdeclaro"|
-            "103m#fundoamareloclaro"|
-            "104m#fundoazulclassico"|
-            "105m#fundorosa"|
-            "106m#fundoazulclaro"|
-            "107m#fundobranco"
 
-        },
+f_8bit: 
 
-        f_8bit: {
+  CorLetra: 
+    `38;5;${TColoracao}m#cortempcustom`,
 
-          CorLetra: 
-            `38;5;${TColoracao}m#cortempcustom`,
+  CorFundo: 
+    `48;5;${TColoracao}m#fundotempcustom`
 
-          CorFundo: 
-            `48;5;${TColoracao}m#fundotempcustom`
 
-        },
-        
-        f_24bit: {
+f_24bit: 
 
-          CorLetra: 
-            `38;2;${TColoracao};${TColoracao};${TColoracao}m#corhexcustom`,
+  CorLetra: 
+    `38;2;${TColoracao};${TColoracao};${TColoracao}m#corhexcustom`,
 
-          CorFundo: 
-            `48;2;${TColoracao};${TColoracao};${TColoracao}m#fundohexcustom`
-
-        }
-
-      }
-
-    }
-
-  }
+  CorFundo: 
+    `48;2;${TColoracao};${TColoracao};${TColoracao}m#fundohexcustom`
+ *
  */

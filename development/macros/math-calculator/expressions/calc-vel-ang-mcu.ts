714a1e03272d5b1cb40fe0af0574cba3ce009adb
Humberto Bravo <humbandroid@gmail.com>
2023-01-25 04:04:34 -0300

    v0.1.0 - Arrumando macros

--- development/macros/math-calculator/expressions/calc-vel-ang-mcu.ts
+++ development/macros/math-calculator/expressions/calc-vel-ang-mcu.ts
@@ -1,5 +1,5 @@
-const formulaCalculoDoVelocidadeAngularMovimentoCircularUniforme = (anguloInicial: number, anguloFinal: number, tempoInicial: number, tempoFinal: number): number => {
-  let resultado = (anguloFinal - anguloInicial) / (tempoFinal - tempoInicial);
+const formulaCalculoDoVelocidadeAngularMovimentoCircularUniforme = (angulo: { inicial: number, final: number}, tempo: { inicial: number, final: number}): number => {
+  let resultado = (angulo.final - angulo.inicial) / (tempo.final - tempo.inicial);
   return resultado;
 }
 export { formulaCalculoDoVelocidadeAngularMovimentoCircularUniforme as Macro_math_calculator_exp_calc_velocidade_angular_movimento_circular_uniforme_method }
--- development/macros/math-calculator/expressions/calc-vel-ang-mcuv.ts
+++ development/macros/math-calculator/expressions/calc-vel-ang-mcuv.ts
@@ -1,5 +1,5 @@
-const formulaCalculoDaAceleracaoAngularMovimentoCircularUniformenteVariado = (velocidadeAngularInicial: number, velocidadeAngularFinal: number, tempoInicial: number, tempoFinal: number): number => {
-  let resultado = (velocidadeAngularFinal - velocidadeAngularInicial) / (tempoFinal - tempoInicial);
+const formulaCalculoDaAceleracaoAngularMovimentoCircularUniformenteVariado = (velocidadeAngular: { inicial: number, final: number}, tempo: { inicial: number, final: number}): number => {
+  let resultado = (velocidadeAngular.final - velocidadeAngular.inicial) / (tempo.final - tempo.inicial);
   return resultado;
 }
 export { formulaCalculoDaAceleracaoAngularMovimentoCircularUniformenteVariado as Macro_math_calculator_exp_calc_aceleracao_angular_movimento_circular_uniformente_variado_method }

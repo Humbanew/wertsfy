9faaa001f9a5020e28146f96b2894f4afe54d1d7
Humba01 <humbandroid@gmail.com>
2023-02-12 15:42:01 -0300

    v0.1.0 - Aprimorando base de código

--- public/.components/wcomponents.ts
+++ public/.components/wcomponents.ts
@@ -60,248 +60,248 @@ export namespace LibraryMath {
     src: { object: { collections: { sqrt_cbrt: WComponents.Mathematic.cubic_root_constant_collection } } }
   }; export const CubicRoot = lm_cubic_root.src.object.collections.sqrt_cbrt;
 
-  // Arrumar aqui.
-  let lm_abs: WComponentFunction = {
-    src: WComponents.Mathematic.abs_method
-  }; export const abs = lm_abs.src;
+  let lm_abs: WComponent = {
+    src: { function: WComponents.Mathematic.abs_method }
+  }; export const abs = lm_abs.src.function;
 
-  let lm_arcsin: WComponentFunction = {
-    src: WComponents.Mathematic.arcsin_method
-  }; export const arcsin = lm_arcsin.src;
+  let lm_arcsin: WComponent = {
+    src: { function: WComponents.Mathematic.arcsin_method }
+  }; export const arcsin = lm_arcsin.src.function;
 
-  let lm_arcsinh: WComponentFunction = {
-    src: WComponents.Mathematic.arcsinh_method
-  }; export const arcsinh = lm_arcsinh.src;
+  let lm_arcsinh: WComponent = {
+    src: { function: WComponents.Mathematic.arcsinh_method }
+  }; export const arcsinh = lm_arcsinh.src.function;
 
-  let lm_arccos: WComponentFunction = {
-    src: WComponents.Mathematic.arccos_method
-  }; export const arccos = lm_arccos.src;
+  let lm_arccos: WComponent = {
+    src: { function: WComponents.Mathematic.arccos_method }
+  }; export const arccos = lm_arccos.src.function;
 
-  let lm_arccosh: WComponentFunction = {
-    src: WComponents.Mathematic.arccosh_method
-  }; export const arccosh = lm_arccosh.src;
+  let lm_arccosh: WComponent = {
+    src: { function: WComponents.Mathematic.arccosh_method }
+  }; export const arccosh = lm_arccosh.src.function;
 
-  let lm_arctan: WComponentFunction = {
-    src: WComponents.Mathematic.arctan_method
-  }; export const arctan = lm_arctan.src;
+  let lm_arctan: WComponent = {
+    src: { function: WComponents.Mathematic.arctan_method }
+  }; export const arctan = lm_arctan.src.function;
 
-  let lm_arctanh: WComponentFunction = {
-    src: WComponents.Mathematic.arctanh_method
-  }; export const arctanh = lm_arctanh.src;
+  let lm_arctanh: WComponent = {
+    src: { function: WComponents.Mathematic.arctanh_method }
+  }; export const arctanh = lm_arctanh.src.function;
 
-  let lm_arcsec: WComponentFunction = {
-    src: WComponents.Mathematic.arcsec_method
-  }; export const arcsec = lm_arcsec.src;
+  let lm_arcsec: WComponent = {
+    src: { function: WComponents.Mathematic.arcsec_method }
+  }; export const arcsec = lm_arcsec.src.function;
 
-  let lm_arcsech: WComponentFunction = {
-    src: WComponents.Mathematic.arcsech_method
-  }; export const arcsech = lm_arcsech.src;
+  let lm_arcsech: WComponent = {
+    src: { function: WComponents.Mathematic.arcsech_method }
+  }; export const arcsech = lm_arcsech.src.function;
 
-  let lm_arccosec: WComponentFunction = {
-    src: WComponents.Mathematic.arccosec_method
-  }; export const arccossec = lm_arccosec.src;
+  let lm_arccosec: WComponent = {
+    src: { function: WComponents.Mathematic.arccosec_method }
+  }; export const arccossec = lm_arccosec.src.function;
 
-  let lm_arccosech: WComponentFunction = {
-    src: WComponents.Mathematic.arccosech_method
-  }; export const arccosech = lm_arccosech.src;
+  let lm_arccosech: WComponent = {
+    src: { function: WComponents.Mathematic.arccosech_method }
+  }; export const arccosech = lm_arccosech.src.function;
   
-  let lm_arccotan: WComponentFunction = {
-    src: WComponents.Mathematic.arccotan_method
-  }; export const arccotan = lm_arccotan.src;
+  let lm_arccotan: WComponent = {
+    src: { function: WComponents.Mathematic.arccotan_method }
+  }; export const arccotan = lm_arccotan.src.function;
 
-  let lm_arccotanh: WComponentFunction = {
-    src: WComponents.Mathematic.arccotanh_method
-  }; export const arccotanh = lm_arccotanh.src;
+  let lm_arccotanh: WComponent = {
+    src: { function: WComponents.Mathematic.arccotanh_method }
+  }; export const arccotanh = lm_arccotanh.src.function;
 
-  let lm_sqrt: WComponentFunction = {
-    src: WComponents.Mathematic.sqrt_method
-  }; export const sqrt = lm_sqrt.src;
+  let lm_sqrt: WComponent = {
+    src: { function: WComponents.Mathematic.sqrt_method }
+  }; export const sqrt = lm_sqrt.src.function;
 
-  let lm_cbrt: WComponentFunction = {
-    src: WComponents.Mathematic.cbrt_method
-  }; export const cbrt = lm_cbrt.src;
+  let lm_cbrt: WComponent = {
+    src: { function: WComponents.Mathematic.cbrt_method }
+  }; export const cbrt = lm_cbrt.src.function;
 
-  let lm_quart: WComponentFunction = {
-    src: WComponents.Mathematic.quart_method
-  }; export const quart = lm_quart.src;
+  let lm_quart: WComponent = {
+    src: { function: WComponents.Mathematic.quart_method }
+  }; export const quart = lm_quart.src.function;
 
-  let lm_quirt: WComponentFunction = {
-    src: WComponents.Mathematic.quirt_method
-  }; export const quirt = lm_quirt.src;
+  let lm_quirt: WComponent = {
+    src: { function: WComponents.Mathematic.quirt_method }
+  }; export const quirt = lm_quirt.src.function;
 
-  let lm_sexrt: WComponentFunction = {
-    src: WComponents.Mathematic.sexrt_method
-  }; export const sexrt = lm_sexrt.src;
+  let lm_sexrt: WComponent = {
+    src: { function: WComponents.Mathematic.sexrt_method }
+  }; export const sexrt = lm_sexrt.src.function;
 
-  let lm_seprt: WComponentFunction = {
-    src: WComponents.Mathematic.seprt_method
-  }; export const seprt = lm_seprt.src;
+  let lm_seprt: WComponent = {
+    src: { function: WComponents.Mathematic.seprt_method }
+  }; export const seprt = lm_seprt.src.function;
 
-  let lm_octrt: WComponentFunction = {
-    src: WComponents.Mathematic.octrt_method
-  }; export const octrt = lm_octrt.src;
+  let lm_octrt: WComponent = {
+    src: { function: WComponents.Mathematic.octrt_method }
+  }; export const octrt = lm_octrt.src.function;
 
-  let lm_nonrt: WComponentFunction = {
-    src: WComponents.Mathematic.nonrt_method
-  }; export const nonrt = lm_nonrt.src;
+  let lm_nonrt: WComponent = {
+    src: { function: WComponents.Mathematic.nonrt_method }
+  }; export const nonrt = lm_nonrt.src.function;
 
-  let lm_decrt: WComponentFunction = {
-    src: WComponents.Mathematic.decrt_method
-  }; export const decrt = lm_decrt.src;
+  let lm_decrt: WComponent = {
+    src: { function: WComponents.Mathematic.decrt_method }
+  }; export const decrt = lm_decrt.src.function;
 
-  let lm_sin: WComponentFunction = {
-    src: WComponents.Mathematic.sin_method
-  }; export const sin = lm_sin.src;
+  let lm_sin: WComponent = {
+    src: { function: WComponents.Mathematic.sin_method }
+  }; export const sin = lm_sin.src.function;
 
-  let lm_sinh: WComponentFunction = {
-    src: WComponents.Mathematic.sinh_method
-  }; export const sinh = lm_sinh.src;
+  let lm_sinh: WComponent = {
+    src: { function: WComponents.Mathematic.sinh_method }
+  }; export const sinh = lm_sinh.src.function;
 
-  let lm_cos: WComponentFunction = {
-    src: WComponents.Mathematic.cos_method
-  }; export const cos = lm_cos.src;
+  let lm_cos: WComponent = {
+    src: { function: WComponents.Mathematic.cos_method }
+  }; export const cos = lm_cos.src.function;
 
-  let lm_cosh: WComponentFunction = {
+  // Arrumar aqui.
+  let lm_cosh: WComponent = {
     src: WComponents.Mathematic.cosh_method
   }; export const cosh = lm_cosh.src;
 
-  let lm_tan: WComponentFunction = {
+  let lm_tan: WComponent = {
     src: WComponents.Mathematic.tan_method
   }; export const tan = lm_tan.src;
 
-  let lm_tanh: WComponentFunction = {
+  let lm_tanh: WComponent = {
     src: WComponents.Mathematic.tanh_method
   }; export const tanh = lm_tanh.src;
 
-  let lm_sec: WComponentFunction = {
+  let lm_sec: WComponent = {
     src: WComponents.Mathematic.sec_method
   }; export const sec = lm_sec.src;
 
-  let lm_sech: WComponentFunction = {
+  let lm_sech: WComponent = {
     src: WComponents.Mathematic.sech_method
   }; export const sech = lm_sech.src;
 
-  let lm_cosec: WComponentFunction = {
+  let lm_cosec: WComponent = {
     src: WComponents.Mathematic.cosec_method
   }; export const cosec = lm_cosec.src;
 
-  let lm_cosech: WComponentFunction = {
+  let lm_cosech: WComponent = {
     src: WComponents.Mathematic.cosech_method
   }; export const cosech = lm_cosech.src;
 
-  let lm_cotan: WComponentFunction = {
+  let lm_cotan: WComponent = {
     src: WComponents.Mathematic.cotan_method
   }; export const cotan = lm_cotan.src;
 
-  let lm_cotanh: WComponentFunction = {
+  let lm_cotanh: WComponent = {
     src: WComponents.Mathematic.cotanh_method
   }; export const cotanh = lm_cotanh.src;
 
-  let lm_pow: WComponentFunction = {
+  let lm_pow: WComponent = {
     src: WComponents.Mathematic.pow_method
   }; export const pow = lm_pow.src;
 
-  let lm_powofpow: WComponentFunction = {
+  let lm_powofpow: WComponent = {
     src: WComponents.Mathematic.powofpow_method
   }; export const powofpow = lm_powofpow.src;
 
-  let lm_min: WComponentFunction = {
+  let lm_min: WComponent = {
     src: WComponents.Mathematic.min_method
   }; export const min = lm_min.src;
 
-  let lm_max: WComponentFunction = {
+  let lm_max: WComponent = {
     src: WComponents.Mathematic.max_method
   }; export const max = lm_max.src;
 
-  let lm_random: WComponentFunction = {
+  let lm_random: WComponent = {
     src: WComponents.Mathematic.random_method
   }; export const random = lm_random.src;
 
-  let lm_log: WComponentFunction = {
+  let lm_log: WComponent = {
     src: WComponents.Mathematic.log_method
   }; export const log = lm_log.src;
 
-  let lm_logm1: WComponentFunction = {
+  let lm_logm1: WComponent = {
     src: WComponents.Mathematic.logm1_method
   }; export const logm1 = lm_logm1.src;
 
-  let lm_logp1: WComponentFunction = {
+  let lm_logp1: WComponent = {
     src: WComponents.Mathematic.logp1_method
   }; export const logp1 = lm_logp1.src;
 
-  let lm_log2: WComponentFunction = {
+  let lm_log2: WComponent = {
     src: WComponents.Mathematic.log2_method
   }; export const log2 = lm_log2.src;
 
-  let lm_log2m1: WComponentFunction = {
+  let lm_log2m1: WComponent = {
     src: WComponents.Mathematic.log2m1_method
   }; export const log2m1 = lm_log2m1.src;
 
-  let lm_log2p1: WComponentFunction = {
+  let lm_log2p1: WComponent = {
     src: WComponents.Mathematic.log2p1_method
   }; export const log2p1 = lm_log2p1.src;
 
-  let lm_logx: WComponentFunction = {
+  let lm_logx: WComponent = {
     src: WComponents.Mathematic.logx_method
   }; export const logx = lm_logx.src;
 
-  let lm_logxm1: WComponentFunction = {
+  let lm_logxm1: WComponent = {
     src: WComponents.Mathematic.logxm1_method
   }; export const logxm1 = lm_logxm1.src;
 
-  let lm_logxp1: WComponentFunction = {
+  let lm_logxp1: WComponent = {
     src: WComponents.Mathematic.logxp1_method
   }; export const logxp1 = lm_logxp1.src;
 
-  let lm_ln: WComponentFunction = {
+  let lm_ln: WComponent = {
     src: WComponents.Mathematic.ln_method
   }; export const ln = lm_ln.src;
 
-  let lm_lnm1: WComponentFunction = {
+  let lm_lnm1: WComponent = {
     src: WComponents.Mathematic.lnm1_method
   }; export const lnm1 = lm_lnm1.src;
 
-  let lm_lnp1: WComponentFunction = {
+  let lm_lnp1: WComponent = {
     src: WComponents.Mathematic.lnp1_method
   }; export const lnp1 = lm_lnp1.src;
 
-  let lm_exp: WComponentFunction = {
+  let lm_exp: WComponent = {
     src: WComponents.Mathematic.exp_method
   }; export const exp = lm_exp.src;
 
-  let lm_expm1: WComponentFunction = {
+  let lm_expm1: WComponent = {
     src: WComponents.Mathematic.expm1_method
   }; export const expm1 = lm_expm1.src;
 
-  let lm_expp1: WComponentFunction = {
+  let lm_expp1: WComponent = {
     src: WComponents.Mathematic.expp1_method
   }; export const expp1 = lm_expp1.src;
 
-  let lm_round: WComponentFunction = {
+  let lm_round: WComponent = {
     src: WComponents.Mathematic.round_method
   }; export const round = lm_round.src;
 
-  let lm_fround: WComponentFunction = {
+  let lm_fround: WComponent = {
     src: WComponents.Mathematic.fround_method
   }; export const fround = lm_fround.src;
 
-  let lm_floor: WComponentFunction = {
+  let lm_floor: WComponent = {
     src: WComponents.Mathematic.floor_method
   }; export const floor = lm_floor.src;
 
-  let lm_ceil: WComponentFunction = {
+  let lm_ceil: WComponent = {
     src: WComponents.Mathematic.ceil_method
   }; export const ceil = lm_ceil.src;
 
-  let lm_imul: WComponentFunction = {
+  let lm_imul: WComponent = {
     src: WComponents.Mathematic.imul_method
   }; export const imul = lm_imul.src;
 
-  let lm_sign: WComponentFunction = {
+  let lm_sign: WComponent = {
     src: WComponents.Mathematic.sign_method
   }; export const sign = lm_sign.src;
 
-  let lm_trunc: WComponentFunction = {
+  let lm_trunc: WComponent = {
     src: WComponents.Mathematic.trunc_method
   }; export const trunc = lm_trunc.src;
 

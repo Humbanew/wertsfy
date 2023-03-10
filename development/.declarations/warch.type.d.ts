7dcdb3539b6dbb87459935d57764cb7dac0aa9f7
Humba01 <humbandroid@gmail.com>
2023-03-10 04:40:09 -0300

    v0.1.0 - Desenvolvendo uma nova arquitetura de interfaces de tipagens dentro da arquitetura atual - Testando recursos experimentais

--- development/.declarations/warch.type.d.ts
+++ development/.declarations/warch.type.d.ts
@@ -1,20 +1,43 @@
 export declare type WConstSqrtCbrtCollectionObject = { 
-  Of01_02: number, Of01_04: number, Of01_06: number, Of01_08: number, Of01: number, Of02: number, Of03: number, Of04: number, Of05: number, Of06: number, Of07: number, Of09: number, Of10: number  
+  Of01_02: number; 
+  Of01_04: number; 
+  Of01_06: number; 
+  Of01_08: number; 
+  Of01: number; 
+  Of02: number; 
+  Of03: number; 
+  Of04: number; 
+  Of05: number; 
+  Of06: number; 
+  Of07: number; 
+  Of09: number; 
+  Of10: number;  
 }
 
 export declare type WConstShortcutsCollectionObject = {
-  Pi: number, Euler: number, Log2: number, Log2e: number, Log10: number, Log10e: number
+  Pi: number; 
+  Euler: number; 
+  Log2: number; 
+  Log2e: number; 
+  Log10: number; 
+  Log10e: number;
 }
 
 // possivel recurso de melhoria da arquitetura.
-import { Component_math_corebase_abs_method } from "./.components/math/method.abs";
-declare type AbsFunction = typeof Component_math_corebase_abs_method;
+import { WComponents } from "./.components/wcimport";
+declare type FuncsM = typeof WComponents.Mathematic;
+declare type FuncsC = typeof WComponents.Calculator;
 
-import { Component_math_corebase_arccos_method } from "./.components/math/method.arccos";
-declare type ArccosFunction = typeof Component_math_corebase_arccos_method;
-
-import { Component_math_corebase_exp_method } from "./.components/math/method.exp";
-declare type ExpFunction = typeof Component_math_corebase_exp_method;
 
   /** @experimental Interface de Testes */
-export declare interface WComponentExperimental { srcm1: { function?: null }, srcm2: { function?: null } }
+export declare interface WComponentExperimental { 
+  
+  src: { 
+    
+    function?: { 
+      default: Function 
+    } 
+  
+  } 
+
+}

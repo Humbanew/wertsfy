9097aa772fb8a73eadc820cb1bfea6bde4b13116
Humba01 <humbandroid@gmail.com>
2023-03-26 09:00:33 -0300

    v0.1.0 - Arrumando tipagens do projeto

--- development/.declarations/architecture.labo.d.ts
+++ development/.declarations/architecture.labo.d.ts
@@ -15,6 +15,239 @@ declare interface WKarzok {
 
 declare interface WKCubbic { }
 
+declare const cubbic_blueprint: { 
+
+  "tamanho": {
+
+    "tam02x02": {
+      
+      "estrutura": [[][]]
+      "aviso": String 
+    
+    }
+    "tam03x03": {
+      
+      "estrutura": [[][][]] 
+      "aviso": String 
+    
+    }
+    "tam04x04": {
+      
+      "estrutura": [[][][][]] 
+      "aviso": String 
+    
+    }
+    "tam05x05": {
+      
+      "estrutura": [[][][][][]] 
+      "aviso": String 
+    
+    }
+    "tam06x06": {
+      
+      "estrutura": [[][][][][][]] 
+      "aviso": String 
+    
+    }
+    "tam07x07": {
+      
+      "estrutura": [[][][][][][][]] 
+      "aviso": String 
+    
+    }
+    "tam08x08": {
+      
+      "estrutura": [[][][][][][][][]] 
+      "aviso": String 
+    
+    }
+    "tam09x09": {
+      
+      "estrutura": [[][][][][][][][][]] 
+      "aviso": String 
+    
+    }
+    "tam10x10": {
+      
+      "estrutura": [[][][][][][][][][][]] 
+      "aviso": String 
+    
+    }
+
+  }
+
+  "pontuacao": {
+
+    "valorm2": {
+
+      "nvl01": {
+        
+        "valor": 2 
+        "cor": String
+      
+      }
+      "nvl02": {
+        
+        "valor": 4 
+        "cor": String
+      
+      }
+      "nvl03": {
+        
+        "valor": 8 
+        "cor": String
+      
+      }
+      "nvl04": {
+        
+        "valor": 16 
+        "cor": String
+      
+      }
+      "nvl05": {
+        
+        "valor": 32 
+        "cor": String
+      
+      }
+      "nvl06": {
+        
+        "valor": 64 
+        "cor": String
+      
+      }
+      "nvl07": {
+        
+        "valor": 128 
+        "cor": String
+      
+      }
+      "nvl08": {
+        
+        "valor": 256 
+        "cor": String
+      
+      }
+      "nvl09": {
+        
+        "valor": 512 
+        "cor": String
+      
+      }
+      "nvl10": {
+        
+        "valor": 1024 
+        "cor": String
+      
+      }
+      "nvl11": {
+        
+        "valor": 2048 
+        "cor": String
+      
+      }
+      "nvl12": {
+        
+        "valor": 4096 
+        "cor": String
+      
+      }
+      "nvl13": {
+        
+        "valor": 8192 
+        "cor": String
+      
+      }
+      "nvl14": {
+        
+        "valor": 16384 
+        "cor": String
+      
+      }
+      "nvl15": {
+        
+        "valor": 32768 
+        "cor": String
+      
+      }
+      "nvl16": {
+        
+        "valor": 65536 
+        "cor": String
+      
+      }
+      "nvl17": {
+        
+        "valor": 131072 
+        "cor": String
+      
+      }
+      "nvl18": {
+        
+        "valor": 262144 
+        "cor": String
+      
+      }
+      "nvl19": {
+        
+        "valor": 524288 
+        "cor": String
+      
+      }
+      "nvl20": {
+        
+        "valor": 1048576 
+        "cor": String
+      
+      }
+
+    }
+
+  }
+
+  "rank": {
+
+    "rankE": {
+
+      "meta": 1500
+      "nome": "E"
+
+    }
+    "rankD": {
+
+      "meta": 8200
+      "nome": "D"
+
+    }
+    "rankC": {
+
+      "meta": 18500
+      "nome": "C"
+
+    }
+    "rankB": {
+    
+      "meta": 30020
+      "nome": "B"
+    
+    }
+    "rankA": {
+
+      "meta": 60280
+      "nome": "A"
+
+    }
+    "rankS": {
+
+      "meta": 290590
+      "nome": "S"
+
+    }
+
+  }
+
+}
+
     // Lydroc
     // Terminal-Spectrals
 

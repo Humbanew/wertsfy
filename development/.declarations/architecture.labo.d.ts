dc39a4182873862b1898d6c5f40f2593c7a3cd35
Humba01 <humbandroid@gmail.com>
2023-03-30 16:43:47 -0300

    v0.1.0 - Desenvolvendo novas estruturas de tipo

--- development/.declarations/architecture.labo.d.ts
+++ development/.declarations/architecture.labo.d.ts
@@ -45,179 +45,15 @@ declare interface WKCubbic {
 
   pontuacao: {
 
-  }
-
-}
-
-declare const cubbic_blueprint: { 
-
-  "pontuacao": {
-
-    "valorm2": {
-
-      "nvl01": {
-        
-        "valor": 2 
-        "cor": String
-      
-      }
-      "nvl02": {
-        
-        "valor": 4 
-        "cor": String
-      
-      }
-      "nvl03": {
-        
-        "valor": 8 
-        "cor": String
-      
-      }
-      "nvl04": {
-        
-        "valor": 16 
-        "cor": String
-      
-      }
-      "nvl05": {
-        
-        "valor": 32 
-        "cor": String
-      
-      }
-      "nvl06": {
-        
-        "valor": 64 
-        "cor": String
-      
-      }
-      "nvl07": {
-        
-        "valor": 128 
-        "cor": String
-      
-      }
-      "nvl08": {
-        
-        "valor": 256 
-        "cor": String
-      
-      }
-      "nvl09": {
-        
-        "valor": 512 
-        "cor": String
-      
-      }
-      "nvl10": {
-        
-        "valor": 1024 
-        "cor": String
-      
-      }
-      "nvl11": {
-        
-        "valor": 2048 
-        "cor": String
-      
-      }
-      "nvl12": {
-        
-        "valor": 4096 
-        "cor": String
-      
-      }
-      "nvl13": {
-        
-        "valor": 8192 
-        "cor": String
-      
-      }
-      "nvl14": {
-        
-        "valor": 16384 
-        "cor": String
-      
-      }
-      "nvl15": {
-        
-        "valor": 32768 
-        "cor": String
-      
-      }
-      "nvl16": {
-        
-        "valor": 65536 
-        "cor": String
-      
-      }
-      "nvl17": {
-        
-        "valor": 131072 
-        "cor": String
-      
-      }
-      "nvl18": {
-        
-        "valor": 262144 
-        "cor": String
-      
-      }
-      "nvl19": {
-        
-        "valor": 524288 
-        "cor": String
-      
-      }
-      "nvl20": {
-        
-        "valor": 1048576 
-        "cor": String
-      
-      }
-
-    }
+    valor: Number
+    cor: String
 
   }
 
-  "rank": {
+  ranking: {
 
-    "rankE": {
-
-      "meta": 1500
-      "nome": "E"
-
-    }
-    "rankD": {
-
-      "meta": 8200
-      "nome": "D"
-
-    }
-    "rankC": {
-
-      "meta": 18500
-      "nome": "C"
-
-    }
-    "rankB": {
-    
-      "meta": 30020
-      "nome": "B"
-    
-    }
-    "rankA": {
-
-      "meta": 60280
-      "nome": "A"
-
-    }
-    "rankS": {
-
-      "meta": 290590
-      "nome": "S"
-
-    }
+    meta: Number
+    nome: String
 
   }
 
@@ -228,7 +64,3 @@ declare interface WKLydroc { }
     // Terminal-Spectrals
 declare interface WKTerminalSpectrals { }
 
-  // WComponent Upgrade
-declare interface Function { }
-declare interface String { }
-declare interface Data { }

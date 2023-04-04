7564a7645d9a3fd5870b4e1c3bdaef4d9e3330fb
Humba01 <humbandroid@gmail.com>
2023-04-04 14:25:43 -0300

    v0.1.0 - Projetando novos caminhos

--- development/.declarations/architecture.labo.d.ts
+++ development/.declarations/architecture.labo.d.ts
@@ -1,9 +1,2 @@
 // possíveis recursos de aprimoramento de arquitetura
 // aqui podem ser testados diversos algoritmos
-
-// Lydroc
-declare interface WLydroc { }
-
-/** @deprecated No use this. @description Behind Module */
-declare class WMCalcConv { readonly conversores = 0; }
-

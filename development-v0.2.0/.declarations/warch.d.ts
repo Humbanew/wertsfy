dc22acaa150d7ec11caed164e7fe84ce47798c7d
Humba01 <humbandroid@gmail.com>
2023-04-04 16:26:59 -0300

    v0.2.0 - Preparando terreno & Documentando estrutura já construída

--- development-v0.2.0/.declarations/warch.d.ts
+++ development-v0.2.0/.declarations/warch.d.ts
@@ -1,5 +1,4 @@
 import { WConstSqrtCbrtCollectionObject, WConstShortcutsCollectionObject, WITFilter, WITFilterOutput } from "./warch.type";
-import { WertsfyPrototipos } from "./warch.prototype";
 
 export declare interface WComponent {
   

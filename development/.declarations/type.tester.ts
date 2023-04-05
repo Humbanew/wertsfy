7b9fd6cd5b3f67a0733aa2a07a6adaea984c9356
Humberto Bravo <humbandroid@gmail.com>
2023-04-05 15:05:58 -0300

    Revert "V0.2.0" (#88)
    * Revert "v0.2.0 - Documentando estrutura já construída"
    
    This reverts commit de0d031a44b42ae953dece9a16228c8faf3c1963.
    
    * Revert "v0.2.0 - Documentando estrutura já construída"
    
    This reverts commit 97727f6b18d183fe432d1baf772ad448d09eae16.
    
    * Revert "v0.2.0 - Documentando estrutura já construída"
    
    This reverts commit 302d1c7f4d2e8d4e624cf7373d66677edce2c51b.
    
    * Revert "v0.2.0 - Preparando terreno & Documentando estrutura já construída"
    
    This reverts commit dc22acaa150d7ec11caed164e7fe84ce47798c7d.
    
    * Revert "v0.2.0 - Preparando terreno"
    
    This reverts commit d8e0c58bc748129ab281622fd1cee07a174bd933.
    
    * Revert "v0.2.0 - Preparando terreno"
    
    This reverts commit b9c3e7ef45e7f76ccd782cc4b1a9b1c12cda2710.
    
    * Revert "v0.2.0 - Preparando terreno"
    
    This reverts commit 7daa07e46192d7ee5e453fff6857a150328536c8.
    
    * Revert "v0.2.0 - Preparando terreno"
    
    This reverts commit 7d4bef7dbaac82be91ed2b46701c73191b442139.

--- development/.declarations/type.tester.ts
+++
@@ -1,6 +0,0 @@
-import { WertsfyTypes } from "./wexport";
-
-// Testando imports de variaveis de tipagens.
-WertsfyTypes.Logicos.comum;
-WertsfyTypes.Logicos.avancados;
-WertsfyTypes.Logicos.expressoes;

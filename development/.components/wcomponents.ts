67bfc1d1135cb5cabef8f0f343447714a32aa6c0
Humba01 <humbandroid@gmail.com>
2023-04-14 16:39:43 -0300

    v0.2.0 - addp - método lnp1 - biblioteca mathematic

--- development/.components/wcomponents.ts
+++ development/.components/wcomponents.ts
@@ -913,8 +913,13 @@ export namespace LibraryMathematic {
    * 
    * **``Método [Method]``**
    * 
-   * @comentaryDescription
+   * Retorna o logarítmo natural mais 1 de um valor. [_Returns the natural logarithm plus 1 of a value._]
    * 
+   * ______________________________________
+   * 
+   * @argument x ***typeof: number*** ⁝ { value }
+   * 
+   * @argument y ***typeof: number*** ⁝ { base }
    * 
    */
   export const lnp1: WComponent["src"]["function"]=

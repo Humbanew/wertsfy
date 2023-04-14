68c3a66d238582b88728ef0b188289a36381f2df
Humba01 <humbandroid@gmail.com>
2023-04-14 12:48:15 -0300

    v0.2.0 - addp - método arccos - biblioteca mathematic

--- development/.components/wcomponents.ts
+++ development/.components/wcomponents.ts
@@ -196,8 +196,11 @@ export namespace LibraryMathematic {
    * 
    * **``Método [Method]``**
    * 
-   * @comentaryDescription
+   * Retorna o valor do arco cosseno de um número. [_Returns the cosine arc value of a number._]
    * 
+   * ______________________________________
+   * 
+   * @argument x ***[typeof: number]***
    * 
    */
   export const arccos: WComponent["src"]["function"]=

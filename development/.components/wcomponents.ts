3146afc0204193bd4bf27f603c6604029687d58d
Humba01 <humbandroid@gmail.com>
2023-04-14 12:54:10 -0300

    v0.2.0 - addp - método arcsech - biblioteca mathematic

--- development/.components/wcomponents.ts
+++ development/.components/wcomponents.ts
@@ -271,8 +271,11 @@ export namespace LibraryMathematic {
    * 
    * **``Método [Method]``**
    * 
-   * @comentaryDescription
+   * Retorna o valor do arco secante hiperbólico de um número. [_Returns the hiperbolic secant arc value of a number._]
    * 
+   * ______________________________________
+   * 
+   * @argument x ***[typeof: number]***
    * 
    */
   export const arcsech: WComponent["src"]["function"]=

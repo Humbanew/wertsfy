63f170399ed7216710813653971ddf1a39630d93
Humba01 <humbandroid@gmail.com>
2023-04-16 22:45:10 -0300

    Progredindo com o projeto

--- development/preview/architecture.labo.d.ts
+++ development/preview/architecture.labo.d.ts
@@ -2,7 +2,7 @@
 // aqui podem ser testados diversos algoritmos
 
 // Wertsfy Whispers => Lydroc + Karzok + Chaosdragon
-declare interface WWhispers { classlistLibrary: null; }
+declare interface WWhispers { cssClasslistLibrary: null; }
 
 // Wertsfy Characters => Conjunto de Caracteres Especiais ASCII e Unicode
 declare interface WCharacters { }

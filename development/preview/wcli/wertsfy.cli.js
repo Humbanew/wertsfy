58c295c54679df5a68e0081abe3a125d4b33962c
Humba01 <humbandroid@gmail.com>
2023-04-23 02:05:59 -0300

    v0.2.0 - Wertsfy Command Line Interface - Primeiros Testes

---
+++ development/preview/wcli/wertsfy.cli.js
@@ -0,0 +1,7 @@
+#! /usr/bin/env node
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var process_1 = require("process");
+if (process_1.argv[2] == "--labo") {
+    console.warn("Labo = " + process_1.argv[3] + process_1.argv[4]);
+}

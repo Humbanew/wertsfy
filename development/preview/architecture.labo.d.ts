79e3d08c8a989e24205935573e8eea17c1f3a02f
Humba01 <humbandroid@gmail.com>
2023-04-28 01:26:58 -0300

    v0.2.0 - Wertsfy Recursos de Prévia

--- development/preview/architecture.labo.d.ts
+++ development/preview/architecture.labo.d.ts
@@ -1,14 +1,8 @@
 // possíveis recursos de aprimoramento de arquitetura
 // aqui podem ser testados diversos algoritmos
 
-import { wwlydroc } from "./architecture.md.labo";
-
 // Wertsfy Whispers => Lydroc + Karzok + Chaosdragon + SpectralsTerminal
-declare interface WWhispers { 
-
-  lydroc: typeof wwlydroc;
-
-} export declare var wtfy_whispers: WWhispers;
+declare interface WWhispers { } export declare var wtfy_whispers: WWhispers;
 
 // Wertsfy Characters => Conjunto de Caracteres Especiais ASCII e Unicode
 declare interface WCharacters { }

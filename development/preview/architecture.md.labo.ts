e9e76ad0cd9b177fcf44ebf05559f5625691bbe6
Humba01 <humbandroid@gmail.com>
2023-04-30 02:34:35 -0300

    v0.2.0 - Wertsfy Recursos de Prévia

--- development/preview/architecture.md.labo.ts
+++ development/preview/architecture.md.labo.ts
@@ -1,9 +1,80 @@
 namespace WertsfyWhispers {
 
-  export let lydroc = {
+  let lydroc = {
 
-    HTMLConstructor: { }
+    HTMLAttrsConstructor: class constructor { 
+
+      protected classList: string[];
+      protected id: string;
+
+      set attrClassList(classlist: string[]) {
+        this.classList = classlist;
+      }
+
+      set attrId(id: string) {
+        this.id = id;
+      }
+
+    },
+
+    HTMLElementsConstructor: {
+
+      StaticForm: null,
+
+      DynamicForm: null
+
+    },
+
+    HTMLStcModeElementsConstructor: class constructor {
+
+      defineDoctypeTag() {
+        return '<!DOCTYPE html>';
+      }
+
+      defineHTMLTag() {
+        return '<html></html>';
+      }
+
+      defineHeadTag() {
+        return '<head></head>';
+      }
+
+      defineBodyTag() {
+        return '<body></body>';
+      }
+
+      defineTitleTag() {
+        return '<title></title>';
+      }
+
+    },
+
+    HTMLDnyModeElementsConstructor: class constructor {
+
+      defineDoctypeTag() {
+        return "<!DOCTYPE html>";
+      }
+
+      defineHTMLTag() {
+        return document.createElement("html");
+      }
+
+      defineHeadTag() {
+        return document.createElement("head");
+      }
+
+      defineBodyTag() {
+        return document.createElement("body");
+      }
+
+      defineTitleTag() {
+        return document.createElement("title");
+      }
+
+    }
 
   }
+
+  export var lyAttrConstructor = new lydroc.HTMLAttrsConstructor();
 
 }

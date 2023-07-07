7b6e521c4c28c0896fa1a3a0fc22d91d1684b25c
Humba01 <humbandroid@gmail.com>
2023-07-07 20:29:32 -0300

    v1.0.0 - Continuando `arithmetic.lib.ts`

--- api/wertsfy.api.ts
+++ api/wertsfy.api.ts
@@ -4,10 +4,6 @@ import { Aritmeticos } from "./syntax/arithmetic.lib";
  *  
  * ![Wertsfy Logo Gen 6 JSDocs](https://github.com/Humbanew/wertsfy/assets/59739253/e8e71afd-190e-437e-800e-efd353f91e61)
  * 
- * ___
- * 
- * ![Root Badge](https://github.com/Humbanew/wertsfy/assets/59739253/0342bd7c-de0b-4b40-a8e2-c6e83733e296)
- * 
  * **``Interface de Programação de Aplicativos [Application Programming Interface]``**
  * 
  * Contém todos os recursos disponíveis pelo kit de desenvolvimento. _[Contains all resources available by the Development Kit.]_
@@ -20,11 +16,6 @@ export const wertsfy=
    *  
    * ![Wertsfy Logo Gen 6 JSDocs](https://github.com/Humbanew/wertsfy/assets/59739253/e8e71afd-190e-437e-800e-efd353f91e61)
    * 
-   * ___
-   * 
-   * ![Root Badge](https://github.com/Humbanew/wertsfy/assets/59739253/0342bd7c-de0b-4b40-a8e2-c6e83733e296)
-   * ![Library Badge](https://github.com/Humbanew/wertsfy/assets/59739253/a13efd6e-eacb-4d85-a0a0-dd7329a6def6)
-   * 
    * **``Bibliotecas da API [API Librarys]``**
    * 
    * Contém todas as bibliotecas disponíveis. _[Contains all available libraries.]_
@@ -36,11 +27,6 @@ export const wertsfy=
      *  
      * ![Wertsfy Logo Gen 6 JSDocs](https://github.com/Humbanew/wertsfy/assets/59739253/e8e71afd-190e-437e-800e-efd353f91e61)
      * 
-     * ___
-     * 
-     * ![Root Badge](https://github.com/Humbanew/wertsfy/assets/59739253/0342bd7c-de0b-4b40-a8e2-c6e83733e296)
-     * ![Library Badge](https://github.com/Humbanew/wertsfy/assets/59739253/a13efd6e-eacb-4d85-a0a0-dd7329a6def6)
-     * ![Library Badge - Arithmetic](https://github.com/Humbanew/wertsfy/assets/59739253/600a6626-109a-41ed-bb3f-7d651b81d3aa)
      * 
      * **``Biblioteca [Library] - Aritmeticos``**
      * 
--- test/api.test.ts
+++ test/api.test.ts
@@ -3,3 +3,7 @@ import { wertsfy } from "../api/wertsfy.api";
 // Biblioteca Aritmeticas
 wertsfy.Bibliotecas.arithm;
 
+wertsfy.Bibliotecas.arithm.Consts;
+
+
+

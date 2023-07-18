c1987da2e66d2956b34251589be8a6672645bb3f
Humba01 <humbandroid@gmail.com>
2023-07-18 03:18:52 -0300

    v1.0.0 - Arrumando comentários das funcionalidades do projeto

--- api/syntax/interfaces/wtsfy.int.d.ts
+++ api/syntax/interfaces/wtsfy.int.d.ts
@@ -9,6 +9,10 @@
  * 
  * Define uma exceção com um modelo global. _[Defines an exception with a global model.]_
  *
+ * ___
+ * ![1 0 0 Badge](https://github.com/Humbanew/wertsfy/assets/59739253/a0c8edb1-da2c-431b-b450-fe6f3642bc18)
+ * ![Release Badge](https://github.com/Humbanew/wertsfy/assets/59739253/2a9a3224-2818-49ff-8f39-1caa7d9b99d1)
+ * 
  */
 declare interface IGException {
 

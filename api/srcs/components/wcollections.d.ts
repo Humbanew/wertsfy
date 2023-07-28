e94d68894fdabf009d7e4c7745c0d5166d7d84a4
Humba01 <humbandroid@gmail.com>
2023-07-28 03:14:07 -0300

    Coletando dados para as próximas bibliotecas

---
+++ api/srcs/components/wcollections.d.ts
@@ -0,0 +1,9 @@
+// Tipos da arquitetura
+export declare type WCollections= 
+{
+  Terminal: WCTerminal,
+  ExpressoesRegulares: WCRegExp 
+}
+
+export declare type WTrmSClr = "3b4bit"|"8bit"|"24bit";
+export declare type WTrmSClrF = "C"|"F";

4b34ffa77acea68766a4201b121383bc1f8e8f64
Humba01 <humbandroid@gmail.com>
2024-02-25 20:49:11 -0300

    **Arrumando biblioteca Colorama - III**

---
+++ api/libs/colorama/componentes/html/pink/pink.hpp
@@ -0,0 +1,16 @@
+/**
+ * @author Humbanew Project ©️ 2024. Todos os direitos reservados.
+ */  
+
+#ifndef PINK_HPP
+#define PINK_HPP
+
+#include "../../core-coloram.hpp"
+
+CatalogColor<
+  RGBA(255, 192, 203, 1), 
+  HSV(350, 0.20, 1), 
+  Hex(*"pink", *"#FFC0CB", 0xFFC0CB)
+> pink;
+
+#endif // PINK_HPP

917bb0e03f38a1a5d7e4e5982487dfd9fa871ce2
Humba01 <humbandroid@gmail.com>
2024-02-25 22:44:05 -0300

    **Construindo biblioteca Colorama - IV**

---
+++ api/libs/colorama/componentes/html/pink/lightpink.hpp
@@ -0,0 +1,16 @@
+/**
+ * @author Humbanew Project ©️ 2024. Todos os direitos reservados.
+ */  
+
+#ifndef LIGHTPINK_HPP
+#define LIGHTPINK_HPP
+
+#include "../../core-coloram.hpp"
+
+CatalogColor<
+  RGBA(255, 182, 193, 1), 
+  HSV(350, 0.09, 1), 
+  Hex(*"lightpink", *"#FFB6C1", 0xFFB6C1)
+> lightpink;
+
+#endif // LIGHTPINK_HPP

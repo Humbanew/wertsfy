d5cb859b891cdf6de33b98391860b54dd682f975
Humba01 <humbandroid@gmail.com>
2024-03-02 17:49:02 -0300

    **v2.0.0** - Atualizando pasta `nxtftr`

--- api/nxtftr/@enqueue/brain.js/classifier.js/jest.config.ts
+++
@@ -1,9 +0,0 @@
-import type { Config } from 'jest'
-
-const config: Config = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
-  modulePathIgnorePatterns: ['<rootDir>/dist/'],
-}
-
-export default config
--- api/nxtftr/@enqueue/classifier.js/src/lib/index.ts
+++
@@ -1,4 +0,0 @@
-export * from './math'
-export * from './parser'
-export * from './utils'
-export * from './similarity'

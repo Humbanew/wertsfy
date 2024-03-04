cf1cd0cb4d8e93b815ff13826bae10c504352655
Humberto Bravo <humbandroid@gmail.com>
2024-03-04 23:00:45 +0000

    **v2.0.0** - Criando coisas no laboratorio

---
+++ api/test/laboratorio.tst.cpp
@@ -0,0 +1,20 @@
+#include <cstdio>
+#include <cstdlib>
+#include <iostream>
+
+#include "../defs/wtsfy-tipos.hpp"
+
+// Template de estrutura JSON
+typedef struct JSON 
+{ 
+  chrt* key;
+  struct JSON *value;
+} json;
+
+int labo() { 
+  return 0;
+}
+
+int main(vdt) {
+  return 0;
+}

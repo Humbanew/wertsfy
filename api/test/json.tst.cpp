60fa093a93e3fd86e4ec53f039d1b235bb4ada52
Humba01 <humbandroid@gmail.com>
2024-03-04 16:29:37 -0300

    **v2.0.0** - Lapidando modelos de endpoints já implementados - II

--- api/test/json.tst.cpp
+++ api/test/json.tst.cpp
@@ -4,22 +4,6 @@
 
 #include "../defs/wtsfy-tipos.hpp"
 
-typedef struct JSON {
-  string key;
-  typedef union ValueTypes
-  {
-    i8t a;
-    i16t a_1;
-    i32t a_2;
-    i64t a_3;
-    f32t b;
-    f64t b_1;
-    char* z;
-    struct JSON *x;
-  } valuetyp;
-  valuetyp value;
-} json;
-
 int labo() { 
 
   json a;
@@ -28,6 +12,8 @@ int labo() {
   a.value.x->value.a = 1;
   a.value.x->value.x->key = "c";
   a.value.x->value.x->value.a = 2;
+  a.value.x->value.x->value.x->key = "d";
+  a.value.x->value.x->value.x->value.a = 3;
 
   return 0;
 

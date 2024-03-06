78bac18659e357c7cd84c589bcc36ee74e299eea
Humba01 <humbandroid@gmail.com>
2024-03-06 19:44:11 -0300

    **v2.0.0** - Modificando e melhorando arquitetura de software do projeto - IX

--- api/test/laboratorio.tst.cpp
+++ api/test/laboratorio.tst.cpp
@@ -14,15 +14,39 @@ struct Lista
   struct No *inicio;
 };
 
-vdt cria_no(i64t x) {
+struct No* cria_no(i64t x) {
   struct No *novo = (struct No*)malloc(sizeof(struct No));
   novo->valor = x;
 }
 
-vdt cria_lista() {
+struct Lista* cria_lista() {
   struct Lista *l = (struct Lista*)malloc(sizeof(struct Lista));
   l->quantidade = 0;
   l->inicio = NULL;
+}
+
+vdt insere(struct Lista *l, i64t x) {
+  struct No *novo = cria_no(x);
+  novo->prox = l->inicio;
+  l->inicio = novo;
+  l->quantidade++;
+}
+
+vdt imprime(struct Lista *l) {
+  struct No *p;
+  for (p = l->inicio; p != NULL; p = p->prox) {
+    printf("%lld\n", p->valor);
+  }
+}
+
+vdt libera(struct Lista *l) {
+  struct No *p = l->inicio;
+  while (p != NULL) {
+    struct No *t = p->prox;
+    free(p);
+    p = t;
+  }
+  free(l);
 }
 
 int main(vdt) 

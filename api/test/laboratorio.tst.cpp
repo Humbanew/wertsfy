4855533027330db2094a214216c6c54854976260
Humba01 <humbandroid@gmail.com>
2024-03-06 11:41:38 -0300

    **v2.0.0** - Modificando e melhorando arquitetura de software do projeto - VI

--- api/test/laboratorio.tst.cpp
+++ api/test/laboratorio.tst.cpp
@@ -1,23 +1,29 @@
-#include <cstdio>
-#include <cstdlib>
-#include <cassert>
-#include <concepts>
-#include <iostream>
-#include <memory>
 // Nova Arquitetura de Pastas
 #include "./../includes/core/wtsfy_tipos.hpp"
 
-// Template de estrutura JSON
-typedef struct JSON
-{ 
-  chrt* key;
-  struct JSON *value;
-} json;
+struct No
+{
+  i64t valor;
+  struct No *prox;
+};
 
-vdt labo() { }
+struct Lista
+{
+  i64t quantidade;
+  struct No *inicio;
+};
+
+vdt cria_no(i64t x) {
+  struct No *novo = (struct No*)malloc(sizeof(struct No));
+  novo->valor = x;
+}
+
+vdt cria_lista(struct Lista *l) {
+  l->quantidade = 0;
+  l->inicio = NULL;
+}
 
 int main(vdt) 
 {
-  labo();
   return 0;
 }

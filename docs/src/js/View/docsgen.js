0d04ef407e02057fefff69454d0b723084291cf5
Humba01 <humbandroid@gmail.com>
2024-03-11 16:50:47 -0300

    Wtsfy Docs - Construindo página

--- docs/src/js/View/docsgen.js
+++ docs/src/js/View/docsgen.js
@@ -1,3 +1,8 @@
 export let docsv1_0_x = document.getElementById('docs-v1-0-x')
   , docsv1_1_x = document.getElementById('docs-v1-1-x')
-  , docsv2_0_x = document.getElementById('docs-v2-0-x');
+  , docsv2_0_x = document.getElementById('docs-v2-0-x')
+  , docs_selection_v1_0_x = document.getElementById('v1-0-x')
+  , docs_selection_v1_1_x = document.getElementById('v1-1-x')
+  , docs_selection_v2_0_x = document.getElementById('v2-0-x');
+
+class DocsGen {};

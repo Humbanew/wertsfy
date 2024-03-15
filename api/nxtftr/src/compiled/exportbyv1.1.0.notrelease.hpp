0044f384dfc5e1d210202fcc22e0aaf95d41929b
Humba01 <humbandroid@gmail.com>
2024-03-15 11:54:33 -0300

    Arrumando pastas

--- api/nxtftr/src/compiled/exportbyv1.1.0.notrelease.hpp
+++ api/nxtftr/src/compiled/exportbyv1.1.0.notrelease.hpp
@@ -5,6 +5,18 @@
 // Nova Arquitetura de Pastas
 #include "includes/core/wtsfy_tipos.hpp"
 
-namespace Wtsfy { namespace Alpha { } }
+namespace Wtsfy 
+{ 
+  
+  namespace Alpha 
+  { 
+
+    i64t Absoluto(i64t x) {
+      return x < 0 ? -1 * x : x;
+    };
+
+  } 
+
+}
 
 #endif // EXPORTBYV1_1_0_NOTRELEASE_HPP

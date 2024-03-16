6fd3bd1d4ad326cc62b854aede55b3e011b0e86a
Humba01 <humbandroid@gmail.com>
2024-03-16 12:23:07 -0300

    Atualizando multiplas partes do projeto

--- docs/src/js/Controller/control.js
+++ docs/src/js/Controller/control.js
@@ -1,2 +1,27 @@
-import { docsv1_0_x, docsv1_1_x, docsv2_0_x } from '../View/docsgen.js';
-console.log(docsv1_0_x, docsv1_1_x, docsv2_0_x);
+import { listaVersoes, DescontinuosVersions } from '../View/docsgen.js';
+
+// Controle de geração de elementos na árvore DOM
+let criadorDeItens = new DescontinuosVersions();
+listaVersoes.appendChild(criadorDeItens.criaElementoLista("prereboot/v1.0.0-alpha"));
+listaVersoes.appendChild(criadorDeItens.criaElementoLista("prereboot/v1.0.0-beta-i"));
+listaVersoes.appendChild(criadorDeItens.criaElementoLista("prereboot/v1.0.0-beta-ii"));
+listaVersoes.appendChild(criadorDeItens.criaElementoLista("prereboot/v1.0.0"));
+listaVersoes.appendChild(criadorDeItens.criaElementoLista("prereboot/v1.1.0"));
+listaVersoes.appendChild(criadorDeItens.criaElementoLista("prereboot/v1.1.1"));
+listaVersoes.appendChild(criadorDeItens.criaElementoLista("prereboot/v1.1.2"));
+listaVersoes.appendChild(criadorDeItens.criaElementoLista("prereboot/v1.1.3"));
+listaVersoes.appendChild(criadorDeItens.criaElementoLista("prereboot/v1.2.0-1"));
+listaVersoes.appendChild(criadorDeItens.criaElementoLista("prereboot/v1.2.0-2"));
+listaVersoes.appendChild(criadorDeItens.criaElementoLista("prereboot/v1.2.0-3"));
+listaVersoes.appendChild(criadorDeItens.criaElementoLista("prereboot/v1.2.0-4"));
+listaVersoes.appendChild(criadorDeItens.criaElementoLista("prereboot/v1.2.0-5"));
+listaVersoes.appendChild(criadorDeItens.criaElementoLista("prereboot/v1.2.0"));
+listaVersoes.appendChild(criadorDeItens.criaElementoLista("prereboot/v2.0.0-R"));
+listaVersoes.appendChild(criadorDeItens.criaElementoLista("prereboot/v2.0.0-RM"));
+listaVersoes.appendChild(criadorDeItens.criaElementoLista("prereboot/v2.0.0-outsider-alpha"));
+listaVersoes.appendChild(criadorDeItens.criaElementoLista("prereboot/v2.0.0-outsider-beta"));
+listaVersoes.appendChild(criadorDeItens.criaElementoLista("prereboot/v2.0.0-outsider-development"));
+listaVersoes.appendChild(criadorDeItens.criaElementoLista("prereboot/v2.0.0"));
+listaVersoes.appendChild(criadorDeItens.criaElementoLista("v0.0.1"));
+listaVersoes.appendChild(criadorDeItens.criaElementoLista("v0.1.0-rc"));
+listaVersoes.appendChild(criadorDeItens.criaElementoLista("v0.2.0"));
--- docs/src/js/View/docsgen.js
+++ docs/src/js/View/docsgen.js
@@ -1,8 +1,19 @@
-export let docsv1_0_x = document.getElementById('docs-v1-0-x')
-  , docsv1_1_x = document.getElementById('docs-v1-1-x')
-  , docsv2_0_x = document.getElementById('docs-v2-0-x')
-  , docs_selection_v1_0_x = document.getElementById('v1-0-x')
-  , docs_selection_v1_1_x = document.getElementById('v1-1-x')
-  , docs_selection_v2_0_x = document.getElementById('v2-0-x');
+export let listaVersoes = document.getElementById("lista_versoes");
 
-class DocsGen {};
+export class DescontinuosVersions {
+
+  criaElementoLista(versao="") {
+    let li = document.createElement("li");
+    li.innerHTML = versao;
+    return li;
+  }
+
+};
+
+class DocsGen {
+
+  constructor(elementoSuper=HTMLElement) {
+    this.elementoSuper = elementoSuper;
+  }
+
+};

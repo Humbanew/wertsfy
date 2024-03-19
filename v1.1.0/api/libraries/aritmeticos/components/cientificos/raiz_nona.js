fcd71f0527b2a5bcd5c1050d67be0b5a42acbcf7
Humberto Bravo <humbandroid@gmail.com>
2024-03-19 00:20:23 +0000

    Atualizando multiplas partes do projeto - XVI; Desenvolvendo HN Website junto com Wtsfy Docs e Website - III

--- v1.1.0/api/libraries/aritmeticos/components/cientificos/raiz_nona.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.RaizNona = void 0;
-var RaizNona = function (x) {
-    return Math.pow(x, (1 / 9));
-};
-exports.RaizNona = RaizNona;
--- v1.1.0/api/libraries/aritmeticos/components/cientificos/raiz_oitava.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.RaizOitava = void 0;
-var RaizOitava = function (x) {
-    return Math.pow(x, (1 / 8));
-};
-exports.RaizOitava = RaizOitava;

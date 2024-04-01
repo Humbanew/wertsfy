ec8c8251375f6966850ed4f8f82f49495517ab6a
Humba01 <humbandroid@gmail.com>
2024-04-01 17:45:40 -0300

    Arrumando pastas - II

---
+++ v1.1.0/api/build.sh
@@ -0,0 +1,2 @@
+echo -e "\n\t\033[36;1mExecutando modelo de rotina de build do projeto\033[0m\n"
+bash tsc.sh && bash cpypackagejson.sh
---
+++ v1.1.0/api/cpypackagejson.sh
@@ -0,0 +1,2 @@
+echo -e "\n\t\033[32;1mExecutando a copia de arquivo package.json\033[0m\n"
+cp package.json dist/package.json

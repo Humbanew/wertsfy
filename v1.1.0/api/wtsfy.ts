3deccb7ef586ddb8bcd5f56ba56705bf49682ec5
Humberto Bravo <humbandroid@gmail.com>
2024-04-01 23:42:07 +0000

    Construindo v1.1.0-beta

--- v1.1.0/api/wtsfy.ts
+++ v1.1.0/api/wtsfy.ts
@@ -1,1 +1,24 @@
 export const wtsfy = {};
+
+type TTamMemoria = 3|4|5|6;
+enum ETipagem {'standard', 'scientific', 'programmer', 'extreme'};
+type TTipagem = ETipagem;
+
+interface IDocumentObjectModel {}
+interface ICLI {}
+
+abstract class Calculator {
+    protected tipagem: TTipagem;
+    protected memoria: Array<number>;
+    protected analisador: string|RegExp = '/^((\d+)([\+\-\*\/])(\d+))+$/gi';
+
+    constructor(tipagem: TTipagem, tam_memoria: TTamMemoria) {
+        this.tipagem = tipagem;
+        this.memoria = new Array(tam_memoria);
+    }
+}
+
+class Aritmeticos {}
+class Conversores {}
+class Karzok {}
+class Azusa {}

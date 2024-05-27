57ec7a5be622228d146e2577c19c72f8fa4f107f
Humba01 <humbandroid@gmail.com>
2024-05-27 17:50:18 -0300

    Arrumando pastas e projetos

--- source/library/Aritmeticos.class.ts
+++ source/library/Aritmeticos.class.ts
@@ -1,5 +1,11 @@
 export default class Aritmeticos {
 
+  /**
+   * Módulo de Soma Numérica
+   * @param a valor 1
+   * @param b valor 2
+   * @returns o valor da soma de dois números inteiros ou decimais.
+   */
   public soma(
     a: number, 
     b: number
@@ -7,6 +13,12 @@ export default class Aritmeticos {
     return a+b;
   }
 
+  /**
+   * Módulo de Subtração Numérica
+   * @param a valor 1
+   * @param b valor 2
+   * @returns o valor da subtração de dois números inteiros ou decimais.
+   */
   public subtracao(
     a: number, 
     b: number
@@ -14,6 +26,12 @@ export default class Aritmeticos {
     return a-b;
   }
 
+  /**
+   * Módulo de Multiplicação Numérica
+   * @param a valor 1
+   * @param b valor 2
+   * @returns o valor da multiplicação de dois números inteiros ou decimais.
+   */
   public multiplicacao(
     a: number, 
     b: number
@@ -21,6 +39,12 @@ export default class Aritmeticos {
     return a*b;
   }
 
+  /**
+   * Módulo de Divisão Numérica
+   * @param a valor 1
+   * @param b valor 2
+   * @returns o valor da divisão de dois números inteiros ou decimais.
+   */
   public divisao(
     a: number, 
     b: number
@@ -28,6 +52,12 @@ export default class Aritmeticos {
     return a/b;
   }
 
+  /**
+   * Módulo do Resto Numérico
+   * @param a valor 1
+   * @param b valor 2
+   * @returns o valor do resto de dois números inteiros ou decimais.
+   */
   public resto(
     a: number, 
     b: number
@@ -35,17 +65,37 @@ export default class Aritmeticos {
     return a%b;
   }
 
+  /**
+   * Módulo da Potência Numérica
+   * @param a valor 1 - base
+   * @param b valor 2 - expoente
+   * @param M1 habilita a potência usando mais 1 no resultado
+   * @param m1 habilita a potência usando menos 1 no resultado
+   * @returns retorna o valor da potência de um número.
+   * @see Não usar ``m1`` e ``M1`` ao mesmo tempo, retornará um erro caso ocorra.
+   */
   public potencia(
     a: number, 
     b: number,
     M1?: boolean,
     m1?: boolean
   ): number {
+    if(M1 == true && m1 == true) 
+      throw new Error('Não é possível habilitar ambos ao mesmo tempo [M1] e [m1]');
     if(M1 == true) return (a**b)+1;
     if(m1 == true) return (a**b)-1;
     return a**b;
   }
 
+  /**
+   * Módulo da Potência de Potência Numérica
+   * @param a valor 1 - base
+   * @param b valor 2 - sequência de expoentes
+   * @param M1 habilita a potência usando mais 1 no resultado
+   * @param m1 habilita a potência usando menos 1 no resultado
+   * @returns retorna o valor da potência de um número.
+   * @see Não usar ``m1`` e ``M1`` ao mesmo tempo, retornará um erro caso ocorra.
+   */
   public potencia_de_potencia(
     a: number=1,
     b: number[],
@@ -54,6 +104,8 @@ export default class Aritmeticos {
   ): number {
     let i: number=0, r: number=0;
     while(i < b.length) {
+      if(M1 == true && m1 == true) 
+        throw new Error('Não é possível habilitar ambos ao mesmo tempo [M1] e [m1]');  
       if(M1 == true) r = (a**b[i])+1; i++;
       if(m1 == true) r = (a**b[i])-1; i++;
       r = a**b[i]; i++;
@@ -61,22 +113,105 @@ export default class Aritmeticos {
     return r;
   }
 
+  /**
+   * Módulo da Raiz Quadrada Numérica
+   * @param a valor
+   * @returns retorna o valor da raiz quadrada numérica.
+   */
   public raiz_quadrada(
     a: number
   ): number {
     return a**(1/2);
   }
 
+  /**
+   * Módulo da Raiz Cúbica Numérica
+   * @param a valor
+   * @returns retorna o valor da raiz cúbica numérica.
+   */
   public raiz_cubica(
     a: number
   ): number {
     return a**(1/3);
   }
 
+  /**
+   * Módulo da Raiz Enésima Numérica
+   * @param a valor
+   * @param n valor do índice da raiz
+   * @returns retorna o valor da raiz enésima numérica.
+   */
   public raiz_enesima(
     a: number, 
     n: number
   ): number {
     return a**(1/n);
   }
+
+  // possiveis novos modulos base
+  protected porcentagem() {}
+  protected porcentagem1000() {}
+  protected porcentagem10000() {}
+  protected porcentagem100000() {}
+  // classe da versao de 2020 dos aritmeticos
+  private Aritmeticos2020 = class Aritmeticos2020 {
+    /**
+     * módulos presentes nessa época:
+     * - arcocosecante
+     * - arcocosecante hiperbolica
+     * - arcocosseno
+     * - arcocosseno hiperbolico
+     * - arcocotangente
+     * - arcocotangente hiperbolica
+     * - arcosecante
+     * - arcosecante hiperbolica
+     * - arcoseno
+     * - arcoseno hiperbolico
+     * - arcotangente
+     * - arcotangente hiperbolica
+     * - cosecante
+     * - cosecante hiperbolica
+     * - cosseno
+     * - cosseno hiperbolico
+     * - cotangente
+     * - cotangente hiperbolica
+     * - divisao
+     * - divisao decimal
+     * - exponencial
+     * - exponencial decimal
+     * - logaritmo
+     * - logaritmo decimal
+     * - logaritmo base 2
+     * - logaritmo base 2 decimal
+     * - logaritmo base 10
+     * - logaritmo base 10 decimal
+     * - media aritmetica
+     * - media aritmetica decimal
+     * - multiplicacao
+     * - multiplicacao decimal
+     * - potenciacao
+     * - potenciacao decimal
+     * - potenciacao por 2
+     * - potenciacao por 2 decimal
+     * - potenciacao por 10
+     * - potenciacao por 10 decimal
+     * - potencia de potencia 
+     * - potencia de potencia decimal
+     * - radiciacao por 2
+     * - radiciacao por 2 decimal
+     * - radiciacao por 3
+     * - radiciacao por 3 decimal
+     * - secante
+     * - secante hiperbolica
+     * - seno
+     * - seno hiperbolico
+     * - soma
+     * - soma decimal
+     * - subtracao
+     * - subtracao decimal
+     * - tangente
+     * - tangente hiperbolica
+     */
+  }
+
 }
--- source/library/Calculadora.class.ts
+++ source/library/Calculadora.class.ts
@@ -1,13 +1,91 @@
+import Aritmeticos from "./Aritmeticos.class";
+
 export default class Calculadora {
+
+  protected slots_memoria: number[];
 
   protected modulos = {
     lista: [
       {
-        name: 'soma',
-        
+        nome: 'soma',
+        categoria: 'base',
+        ativo: false,
+        componente: 
+          Aritmeticos.prototype.soma
+      },
+      {
+        nome: 'subtração',
+        categoria: 'base',
+        ativo: false,
+        componente: 
+          Aritmeticos.prototype.subtracao
+      },
+      {
+        nome: 'multiplicação',
+        categoria: 'base',
+        ativo: false,
+        componente: 
+          Aritmeticos.prototype.multiplicacao
+      },
+      {
+        nome: 'divisão',
+        categoria: 'base',
+        ativo: false,
+        componente: 
+          Aritmeticos.prototype.divisao
+      },
+      {
+        nome: 'resto',
+        categoria: 'base',
+        ativo: false,
+        componente: 
+          Aritmeticos.prototype.resto
+      },
+      {
+        nome: 'potência',
+        categoria: 'base',
+        ativo: false,
+        componente:
+          Aritmeticos.prototype.potencia
+      },
+      {
+        nome: 'potência de potência',
+        categoria: 'base',
+        ativo: false,
+        componente:
+          Aritmeticos.prototype.potencia_de_potencia
+      },
+      {
+        nome: 'raiz quadrada',
+        categoria: 'base',
+        ativo: false,
+        componente:
+          Aritmeticos.prototype.raiz_quadrada
+      },
+      {
+        nome: 'raiz cúbica',
+        categoria: 'base',
+        ativo: false,
+        componente:
+          Aritmeticos.prototype.raiz_cubica
+      },
+      {
+        nome: 'raiz enésima',
+        categoria: 'base',
+        ativo: false,
+        componente:
+          Aritmeticos.prototype.raiz_enesima
       }
     ]
   }
 
+  // por padrão sempre habilitará as 10 funções elementares, 
+  // soma, subtração, multiplicação, divisão, resto, variacoes de potencia e raizes.
+  public constructor(memoria: number) {
+    this.slots_memoria.length = memoria;
+    for(let i=0; i < 10; i++) {
+      this.modulos.lista[i].ativo = true;
+    }
+  }
 
 }

4f3bb31e96a49c1b3d2b9b22703b7d4ff8bc1ab2
Humberto Bravo <humbandroid@gmail.com>
2024-05-27 15:29:06 +0000

    Tentando entender a nova arquitetura

---
+++ source/index.ts
@@ -0,0 +1,11 @@
+import Aritmeticos from "./library/Aritmeticos.class";
+
+export const Wertsfy = {
+  especificacoes: {
+    versao: '1.1.0',
+    patch: 0,
+  },
+  modulos: {
+    "#aritmeticos": Aritmeticos,
+  }
+}
---
+++ source/library/Aritmeticos.class.ts
@@ -0,0 +1,8 @@
+export default class Aritmeticos {
+
+    // componente soma.module.ts
+    public soma(a: number, b: number): number {
+        return a + b;
+    }
+
+}

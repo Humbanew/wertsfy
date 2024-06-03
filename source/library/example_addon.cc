e893f046780e8bcec04d9dbf3cc64b2433f89f4a
Humba01 <humbandroid@gmail.com>
2024-06-03 16:33:20 -0300

    Adicionando exemplo de addon nodejs

---
+++ source/library/example_addon.cc
@@ -0,0 +1,25 @@
+// nodejs addon
+#include <node.h>
+#include <v8.h>
+
+using namespace v8;
+
+Handle<Value> Add(const Arguments& args) {
+  HandleScope scope;
+  if (args.Length() < 2) {
+    ThrowException(Exception::TypeError(String::New("Wrong number of arguments")));
+    return scope.Close(Undefined());
+  }
+  if (!args[0]->IsNumber() || !args[1]->IsNumber()) {
+    ThrowException(Exception::TypeError(String::New("Wrong arguments")));
+    return scope.Close(Undefined());
+  }
+  Local<Number> num = Number::New(args[0]->NumberValue() + args[1]->NumberValue());
+  return scope.Close(num);
+}
+
+void Init(Handle<Object> target) {
+  NODE_SET_METHOD(target, "add", Add);
+}
+
+NODE_MODULE(addon, Init)
